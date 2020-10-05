7f997eef04c56a6ae5a3674b3505218e06ae674b
Roger Mähler <roger.mahler@hotmail.com>
2020-10-05 20:33:42 +0200

    Add exports to topic_modelling

--- penelope/topic_modelling/__init__.py
+++ penelope/topic_modelling/__init__.py
@@ -1,1 +1,40 @@
-from gensim.models import coherencemodel
+from .compute import (
+    compute_model,
+    load_model,
+    store_model
+)
+from .coherence import (
+    compute_score,
+    compute_scores
+)
+from .compiled_data import (
+    CompiledData,
+    compile_data,
+    get_topics_unstacked,
+    get_topic_tokens,
+    get_topic_title,
+    get_topic_titles,
+    extend_with_document_column
+)
+from .compute_options import (
+    engine_options,
+    DEFAULT_VECTORIZE_PARAMS
+)
+
+from .saliency import (
+    compute_topic_metrics,
+    compute_term_info,
+    compute_term_frequency,
+    compute_KL_divergence
+)
+
+from .utility import (
+    compute_topic_proportions,
+    malletmodel2ldamodel,
+    find_models,
+    display_termite_plot,
+    METHODS,
+    plot_topic,
+    compute_means,
+    normalize_weights
+)
