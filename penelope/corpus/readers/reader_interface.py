95281966291d4580ef3f6c7d3795088d40200e91
Roger Mähler <roger.mahler@hotmail.com>
2020-10-08 16:12:10 +0200

    Added reader inerface draft

---
+++ penelope/corpus/readers/reader_interface.py
@@ -0,0 +1,20 @@
+
+from abc import ABCMeta, abstractmethod
+
+class ITextReader(metaclass=ABCMeta):
+
+
+    @abstractmethod
+    def __iter__(self):
+        while False:
+            yield None
+
+    @property
+    @abstractmethod
+    def metadata(self):
+        pass
+
+    @property
+    @abstractmethod
+    def filenames(self):
+        pass
