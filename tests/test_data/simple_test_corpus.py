20c435293d969ec395eb839e08f57ab46892bb01
Roger Mähler <roger.mahler@hotmail.com>
2020-10-17 13:46:48 +0200

    Refactor: SimpleNamespace -> dict

--- tests/test_data/simple_test_corpus.py
+++ tests/test_data/simple_test_corpus.py
@@ -1,4 +1,3 @@
-import types
 from typing import List
 
 import pandas as pd
@@ -8,21 +7,21 @@ from penelope.corpus.tokenized_corpus import ReIterableTerms
 
 
 class SimpleTestCorpus:
-    def __init__(self, filename, meta_fields: List[str] = None):
+    def __init__(self, filename: str, meta_fields: List[str] = None):
 
         with open(filename, 'r') as f:
             lines = f.readlines()
 
         self.corpus_data = [
-            types.SimpleNamespace(
+            dict(
                 filename=data[0], title=data[1], text=data[2], tokens=[x.lower() for x in data[2].split() if len(x) > 0]
             )
             for data in [line.split(' # ') for line in lines]
         ]
-        self.filenames = [x.filename for x in self.corpus_data]
+        self.filenames = [x['filename'] for x in self.corpus_data]
         self.iterator = None
 
-        meta_data = {'filename': self.filenames, 'title': [x.title for x in self.corpus_data]}
+        meta_data = {'filename': self.filenames, 'title': [x['title'] for x in self.corpus_data]}
 
         if meta_fields is not None:
 
@@ -30,7 +29,7 @@ class SimpleTestCorpus:
             filename_data = [
                 utility.extract_filename_fields(filename, **filename_fields) for filename in self.filenames
             ]
-            meta_data = {**meta_data, **{k: [getattr(v, k) for v in filename_data] for k in filename_fields.keys()}}
+            meta_data = {**meta_data, **utility.list_of_dicts_to_dict_of_lists(filename_data)}
 
         self.documents = pd.DataFrame(data=meta_data)
         if 'document_id' not in self.documents.columns:
@@ -41,7 +40,7 @@ class SimpleTestCorpus:
         return ReIterableTerms(self)
 
     def _create_iterator(self):
-        return ((x.filename, x.tokens) for x in self.corpus_data)
+        return ((x['filename'], x['tokens']) for x in self.corpus_data)
 
     def __iter__(self):
         return self
