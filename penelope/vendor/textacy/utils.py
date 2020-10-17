841bf19a37927d002305a19e034ca3762b16f32b
Roger Mähler <roger.mahler@hotmail.com>
2020-10-17 10:57:35 +0200

    Added typings

--- penelope/vendor/textacy/utils.py
+++ penelope/vendor/textacy/utils.py
@@ -2,8 +2,11 @@ import collections
 import itertools
 import os
 import re
+from typing import Any, Callable, Dict, Iterable, List, Sequence, Tuple
 
 import pandas as pd
+import spacy.tokens
+import textacy
 from spacy import attrs
 from textacy.vsm import Vectorizer
 
@@ -12,8 +15,8 @@ import penelope.utility as utility
 logger = utility.getLogger()
 
 
-def generate_word_count_score(corpus, normalize, count):
-    wc = corpus.word_counts(normalize=normalize, weighting='count', as_strings=True)
+def generate_word_count_score(corpus: textacy.Corpus, normalize: str, count: int, weighting: str = 'count'):
+    wc = corpus.word_counts(normalize=normalize, weighting=weighting, as_strings=True)
     d = {i: set([]) for i in range(1, count + 1)}
     for k, v in wc.items():
         if v <= count:
@@ -21,7 +24,7 @@ def generate_word_count_score(corpus, normalize, count):
     return d
 
 
-def generate_word_document_count_score(corpus, normalize, threshold=75):
+def generate_word_document_count_score(corpus: textacy.Corpus, normalize: str, threshold: int = 75):
     wc = corpus.word_doc_counts(normalize=normalize, weighting='freq', smooth_idf=True, as_strings=True)
     d = {i: set([]) for i in range(threshold, 101)}
     for k, v in wc.items():
@@ -31,32 +34,37 @@ def generate_word_document_count_score(corpus, normalize, threshold=75):
     return d
 
 
-def count_documents_by_pivot(corpus, attribute):
+def count_documents_by_pivot(corpus: textacy.Corpus, attribute: str) -> List[int]:
     """Return a list of document counts per group defined by attribute
     Assumes documents are sorted by attribute!
     """
-    fx_key = lambda doc: doc._.meta[attribute]
+
+    def fx_key(doc: spacy.tokens.Doc):
+        return doc._.meta[attribute]
+
     return [len(list(g)) for _, g in itertools.groupby(corpus, fx_key)]
 
 
-def count_documents_in_index_by_pivot(documents, attribute):
+def count_documents_in_index_by_pivot(documents: pd.DataFrame, attribute: str) -> List[int]:
     """Return a list of document counts per group defined by attribute
     Assumes documents are sorted by attribute!
-    Same as count_documents_by_pivot but uses document index instead of (textacy) corpus
+    Same as count_documents_by_pivot but uses document index instead of (spaCy) corpus
     """
     assert documents[attribute].is_monotonic_increasing, 'Documents *MUST* be sorted by TIME-SLICE attribute!'
     # TODO: Either sort documents (and corpus or term stream!) prior to this call - OR force sortorder by filename (i.e add year-prefix)
     return list(documents.groupby(attribute).size().values)
 
 
-def get_document_by_id(corpus, document_id, field_name='document_id'):
+def get_document_by_id(
+    corpus: textacy.Corpus, document_id: Tuple[int, str], field_name: str = 'document_id'
+) -> spacy.tokens.Doc:
     for doc in corpus.get(lambda x: x._.meta[field_name] == document_id, limit=1):
         return doc
     assert False, 'Document {} not found in corpus'.format(document_id)
     return None
 
 
-def get_disabled_pipes_from_filename(filename):
+def get_disabled_pipes_from_filename(filename: str):
     re_pipes = r'^.*disable\((.*)\).*$'
     x = re.match(re_pipes, filename).groups(0)
     if len(x or []) > 0:
@@ -64,7 +72,13 @@ def get_disabled_pipes_from_filename(filename):
     return None
 
 
-def infrequent_words(corpus, normalize='lemma', weighting='count', threshold=0, as_strings=False):
+def infrequent_words(
+    corpus: textacy.Corpus,
+    normalize: str = 'lemma',
+    weighting: str = 'count',
+    threshold: int = 0,
+    as_strings: bool = False
+):
     '''Returns set of infrequent words i.e. words having total count less than given threshold'''
 
     if weighting == 'count' and threshold <= 1:
@@ -77,7 +91,11 @@ def infrequent_words(corpus, normalize='lemma', weighting='count', threshold=0, 
 
 
 def frequent_document_words(
-    corpus, normalize='lemma', weighting='freq', dfs_threshold=80, as_strings=True
+    corpus: textacy.Corpus,
+    normalize: str = 'lemma',
+    weighting: str = 'freq',
+    dfs_threshold: int = 80,
+    as_strings: bool = True
 ):  # pylint: disable=unused-argument
     '''Returns set of words that occurrs freuently in many documents, candidate stopwords'''
     document_freqs = corpus.word_doc_counts(normalize=normalize, weighting=weighting, smooth_idf=True, as_strings=True)
@@ -85,7 +103,13 @@ def frequent_document_words(
     return result
 
 
-def get_most_frequent_words(corpus, n_top, normalize='lemma', include_pos=None, weighting='count'):
+def get_most_frequent_words(
+    corpus: textacy.Corpus,
+    n_top: int,
+    normalize: str = 'lemma',
+    include_pos: Sequence[str] = None,
+    weighting: str = 'count'
+):
     include_pos = include_pos or ['VERB', 'NOUN', 'PROPN']
     include = lambda x: x.pos_ in include_pos
     token_counter = collections.Counter()
@@ -100,7 +124,14 @@ def get_most_frequent_words(corpus, n_top, normalize='lemma', include_pos=None, 
     return token_counter.most_common(n_top)
 
 
-def doc_to_bow(doc, target='lemma', weighting='count', as_strings=False, include=None, n_min_count=2):
+def doc_to_bow(
+    doc: spacy.tokens.Doc,
+    target: str = 'lemma',
+    weighting: str = 'count',
+    as_strings: bool = False,
+    include: Callable = None,
+    n_min_count: int = 2
+):
 
     weighing_keys = {'count', 'freq'}
     target_keys = {'lemma': attrs.LEMMA, 'lower': attrs.LOWER, 'orth': attrs.ORTH}
@@ -145,27 +176,29 @@ POS_TO_COUNT = {
 POS_NAMES = list(sorted(POS_TO_COUNT.keys()))
 
 
-def get_pos_statistics(doc):
+def get_pos_statistics(doc: spacy.tokens.Doc):
     pos_iter = (x.pos_ for x in doc if x.pos_ not in ['NUM', 'PUNCT', 'SPACE'])
     pos_counts = dict(collections.Counter(pos_iter))
     stats = utility.extend(dict(POS_TO_COUNT), pos_counts)
     return stats
 
 
-def get_corpus_data(corpus, document_index, title, columns_of_interest=None):
+def get_corpus_data(
+    corpus: textacy.Corpus, documents: pd.DataFrame, title: str, columns_of_interest: List[str] = None
+) -> pd.DataFrame:
     metadata = [
         utility.extend({}, dict(document_id=doc._.meta['document_id']), get_pos_statistics(doc)) for doc in corpus
     ]
     df = pd.DataFrame(metadata)[['document_id'] + POS_NAMES]
     if columns_of_interest is not None:
-        document_index = document_index[columns_of_interest]
-    df = pd.merge(df, document_index, left_on='document_id', right_index=True, how='inner')
+        documents = documents[columns_of_interest]
+    df = pd.merge(df, documents, left_on='document_id', right_index=True, how='inner')
     df['title'] = df[title]
     df['words'] = df[POS_NAMES].apply(sum, axis=1)
     return df
 
 
-def load_term_substitutions(filepath, default_term='_gpe_', delim=';', vocab=None):
+def load_term_substitutions(filepath: str, default_term: str = '_gpe_', delim: str = ';', vocab=None):
     substitutions = {}
     if not os.path.isfile(filepath):
         return {}
@@ -173,7 +206,7 @@ def load_term_substitutions(filepath, default_term='_gpe_', delim=';', vocab=Non
     with open(filepath) as f:
         substitutions = {
             x[0].strip(): x[1].strip()
-            for x in (tuple(line.lower().split(delim)) + (default_term,) for line in f.readlines())
+            for x in (tuple(line.lower().split(delim)) + (default_term, ) for line in f.readlines())
             if x[0].strip() != ''
         }
 
@@ -190,21 +223,21 @@ def load_term_substitutions(filepath, default_term='_gpe_', delim=';', vocab=Non
     return substitutions
 
 
-def term_substitutions(data_folder, filename='term_substitutions.txt', vocab=None):
+def term_substitutions(data_folder: str, filename: str = 'term_substitutions.txt', vocab=None):
     path = os.path.join(data_folder, filename)
     logger.info('Loading term substitution mappings...')
     data = load_term_substitutions(path, default_term='_masked_', delim=';', vocab=vocab)
     return data
 
 
-def vectorize_terms(terms, vectorizer_args):
+def vectorize_terms(terms, vectorizer_args: Dict):
     vectorizer = Vectorizer(**vectorizer_args)
     doc_term_matrix = vectorizer.fit_transform(terms)
     id2word = vectorizer.id_to_term
     return doc_term_matrix, id2word
 
 
-def _doc_token_stream(doc):
+def _doc_token_stream(doc: spacy.tokens.Doc) -> Iterable[Dict[str, Any]]:
     return (
         dict(
             i=t.i,
@@ -213,12 +246,11 @@ def _doc_token_stream(doc):
             pos=t.pos_,
             year=doc._.meta['year'],
             document_id=doc._.meta['document_id'],
-        )
-        for t in doc
+        ) for t in doc
     )
 
 
-def store_tokens(corpus, filename: str):
+def store_tokens(corpus: textacy.Corpus, filename: str):
 
     tokens = pd.DataFrame(list(itertools.chain.from_iterable(_doc_token_stream(d) for d in corpus)))
 
