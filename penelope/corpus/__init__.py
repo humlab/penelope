82c4ca12ef1590ee276ca9361ea6f5862cfe21e4
Roger Mähler <roger.mahler@hotmail.com>
2020-10-17 13:35:29 +0200

    Explicit module exports

--- penelope/corpus/__init__.py
+++ penelope/corpus/__init__.py
@@ -1,20 +1,21 @@
 from .interfaces import ICorpus, ITokenizedCorpus
-from .segmented_text_corpus import (SegmentedTextCorpus, SentenceSegmenter, DocumentSegmenter, ChunkSegmenter)
-from .sparv_corpus import (
-    SparvTokenizedXmlCorpus, SparvTokenizedCsvCorpus, sparv_csv_extract_and_store, sparv_xml_extract_and_store
-)
-from .store_corpus import (store_tokenized_corpus_as_archive)
+from .segmented_text_corpus import (ChunkSegmenter, DocumentSegmenter,
+                                    SegmentedTextCorpus, SentenceSegmenter)
+from .sparv_corpus import (SparvTokenizedCsvCorpus, SparvTokenizedXmlCorpus,
+                           sparv_csv_extract_and_store,
+                           sparv_xml_extract_and_store)
+from .store_corpus import store_tokenized_corpus_as_archive
 from .text_lines_corpus import SimpleTextLinesCorpus
-from .text_transformer import TextTransformer, TRANSFORMS
-from .tokenized_corpus import (TokenizedCorpus, ReIterableTerms)
-
-from .transforms import (
-    remove_empty_filter, remove_hyphens, has_alpha_filter, only_any_alphanumeric, only_alphabetic_filter,
-    remove_stopwords, min_chars_filter, max_chars_filter, lower_transform, upper_transform, remove_numerals,
-    remove_symbols, remove_accents
-)
-
-from .vectorized_corpus import (load_cached_normalized_vectorized_corpus, load_corpus, VectorizedCorpus)
-
-from .vectorizer import (CorpusVectorizer, generate_corpus as vectorize_stored_corpus)
-from .windowed_corpus import (concept_windows, corpus_concept_windows)
+from .text_transformer import TRANSFORMS, TextTransformer
+from .tokenized_corpus import ReIterableTerms, TokenizedCorpus
+from .transforms import (has_alpha_filter, lower_transform, max_chars_filter,
+                         min_chars_filter, only_alphabetic_filter,
+                         only_any_alphanumeric, remove_accents,
+                         remove_empty_filter, remove_hyphens, remove_numerals,
+                         remove_stopwords, remove_symbols, upper_transform)
+from .vectorized_corpus import (VectorizedCorpus,
+                                load_cached_normalized_vectorized_corpus,
+                                load_corpus)
+from .vectorizer import CorpusVectorizer
+from .vectorizer import generate_corpus as vectorize_stored_corpus
+from .windowed_corpus import concept_windows, corpus_concept_windows
