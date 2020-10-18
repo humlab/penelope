eb84bdb04b009496db283bb0e7b363643cd95d3c
Roger Mähler <roger.mahler@hotmail.com>
2020-10-18 12:40:18 +0200

    dox

--- penelope/corpus/sparv_corpus.py
+++ penelope/corpus/sparv_corpus.py
@@ -33,7 +33,33 @@ class SparvTokenizedXmlCorpus(tokenized_corpus.TokenizedCorpus):
         pos_excludes : str, optional
             [description], by default "|MAD|MID|PAD|"
         lemmatize : bool, optional
-            [description], by default True
+            If True then use token baseform, by default True
+        tokens_transform_opts : Dict[str, Any], optional
+            Passed to TokensTransformer:
+                only_alphabetic: bool = False,
+                only_any_alphanumeric: bool = False,
+                to_lower: bool = False,
+                to_upper: bool = False,
+                min_len: int = None,
+                max_len: int = None,
+                remove_accents: bool = False,
+                remove_stopwords: bool = False,
+                stopwords: Any = None,
+                extra_stopwords: List[str] = None,
+                language: str = "swedish",
+                keep_numerals: bool = True,
+                keep_symbols: bool = True,
+        tokenizer_opts : Dict[str, Any], optional
+            Passed to source reader:
+                transforms: List[Callable] = None,
+                chunk_size: int = None,
+                filename_pattern: str = None,
+                filename_filter: Union[Callable, List[str]] = None,
+                filename_fields=None,
+                N/A: tokenize: Callable = None,
+                N/A: fix_whitespaces: bool = False,
+                N/A: fix_hyphenation: bool = False,
+                as_binary: bool = False,
         """
 
         if isinstance(source, readers.SparvXmlTokenizer):
@@ -58,7 +84,7 @@ class SparvTokenizedCsvCorpus(tokenized_corpus.TokenizedCorpus):
     Parameters
     ----------
     tokenized_corpus : [type]
-        [description]
+
     """
 
     def __init__(
@@ -72,24 +98,7 @@ class SparvTokenizedCsvCorpus(tokenized_corpus.TokenizedCorpus):
         tokens_transform_opts: Dict[str, Any] = None,
         tokenizer_opts: Dict[str, Any] = None,
     ):
-        """[summary]
-
-        Parameters
-        ----------
-        source : [type]
-            [description]
-        pos_includes : str, optional
-            [description], by default None
-        pos_excludes : str, optional
-            [description], by default "|MAD|MID|PAD|"
-        lemmatize : bool, optional
-            [description], by default True
-        chunk_size : int, optional
-            [description], by default None
-        append_pos : bool, optional
-            [description], by default False
-        """
-
+        # TODO How use parents docstring?
         if isinstance(source, readers.SparvCsvTokenizer):
             tokenizer = source
         else:
@@ -124,6 +133,38 @@ def sparv_xml_extract_and_store(
         [description]
     version : int
         [description]
+    pos_includes : [type], optional
+        [description], by default None
+    pos_excludes : str, optional
+        [description], by default "|MAD|MID|PAD|"
+    lemmatize : bool, optional
+        If True then use token baseform, by default True
+    tokens_transform_opts : Dict[str, Any], optional
+        Passed to TokensTransformer:
+            only_alphabetic: bool = False,
+            only_any_alphanumeric: bool = False,
+            to_lower: bool = False,
+            to_upper: bool = False,
+            min_len: int = None,
+            max_len: int = None,
+            remove_accents: bool = False,
+            remove_stopwords: bool = False,
+            stopwords: Any = None,
+            extra_stopwords: List[str] = None,
+            language: str = "swedish",
+            keep_numerals: bool = True,
+            keep_symbols: bool = True,
+    tokenizer_opts : Dict[str, Any], optional
+        Passed to source reader:
+            transforms: List[Callable] = None,
+            chunk_size: int = None,
+            filename_pattern: str = None,
+            filename_filter: Union[Callable, List[str]] = None,
+            filename_fields=None,
+            N/A: tokenize: Callable = None,
+            N/A: fix_whitespaces: bool = False,
+            N/A: fix_hyphenation: bool = False,
+            as_binary: bool = False,
     """
     corpus = SparvTokenizedXmlCorpus(
         source,
@@ -154,11 +195,41 @@ def sparv_csv_extract_and_store(
     Parameters
     ----------
     source : str
-        Corpus source reader
+        [description]
     target : str
         [description]
-    version : int
-        [description]
+    pos_includes : [type], optional
+        [description], by default None
+    pos_excludes : str, optional
+        [description], by default "|MAD|MID|PAD|"
+    lemmatize : bool, optional
+        If True then use token baseform, by default True
+    tokens_transform_opts : Dict[str, Any], optional
+        Passed to TokensTransformer:
+            only_alphabetic: bool = False,
+            only_any_alphanumeric: bool = False,
+            to_lower: bool = False,
+            to_upper: bool = False,
+            min_len: int = None,
+            max_len: int = None,
+            remove_accents: bool = False,
+            remove_stopwords: bool = False,
+            stopwords: Any = None,
+            extra_stopwords: List[str] = None,
+            language: str = "swedish",
+            keep_numerals: bool = True,
+            keep_symbols: bool = True,
+    tokenizer_opts : Dict[str, Any], optional
+        Passed to source reader:
+            transforms: List[Callable] = None,
+            chunk_size: int = None,
+            filename_pattern: str = None,
+            filename_filter: Union[Callable, List[str]] = None,
+            filename_fields=None,
+            N/A: tokenize: Callable = None,
+            N/A: fix_whitespaces: bool = False,
+            N/A: fix_hyphenation: bool = False,
+            as_binary: bool = False,
     """
     corpus = SparvTokenizedCsvCorpus(
         source,
