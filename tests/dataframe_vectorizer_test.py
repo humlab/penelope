827850ff49249b68311c2e0eb8fe0834eb535da0
Roger Mähler <roger.mahler@hotmail.com>
2020-10-21 15:14:36 +0200

    Fixed failing tests

--- tests/dataframe_vectorizer_test.py
+++ tests/dataframe_vectorizer_test.py
@@ -1,14 +1,24 @@
 import unittest
-import pandas as pd
-import numpy as np
-import scipy
 
-from penelope.corpus import vectorizer as corpus_vectorizer
-import penelope.corpus.tokenized_corpus as corpora
+import pandas as pd
+
 import penelope.corpus.readers as readers
+import penelope.corpus.tokenized_corpus as corpora
+from penelope.cooccurrence.term_term_matrix import to_dataframe
+from penelope.corpus import vectorizer as corpus_vectorizer
+
+DEFAULT_TOKENS_TRANSFORM_OPTS = dict(
+    only_any_alphanumeric=False,
+    to_lower=False,
+    remove_accents=False,
+    min_len=1,
+    max_len=None,
+    keep_numerals=False
+)
 
 
 class Test_DataFrameVectorize(unittest.TestCase):
+
     def setUp(self):
         pass
 
@@ -20,15 +30,7 @@ class Test_DataFrameVectorize(unittest.TestCase):
     def create_corpus(self):
         df = self.create_test_dataframe()
         reader = readers.DataFrameTextTokenizer(df)
-        kwargs = dict(
-            only_any_alphanumeric=False,
-            to_lower=False,
-            remove_accents=False,
-            min_len=0,
-            max_len=None,
-            keep_numerals=False,
-        )
-        corpus = corpora.TokenizedCorpus(reader, **kwargs)
+        corpus = corpora.TokenizedCorpus(reader, **DEFAULT_TOKENS_TRANSFORM_OPTS)
         return corpus
 
     def test_corpus_token_stream(self):
@@ -49,15 +51,7 @@ class Test_DataFrameVectorize(unittest.TestCase):
     def test_processed_corpus_token_stream(self):
         df = self.create_test_dataframe()
         reader = readers.DataFrameTextTokenizer(df)
-        kwargs = dict(
-            only_any_alphanumeric=False,
-            to_lower=False,
-            remove_accents=False,
-            min_len=0,
-            max_len=None,
-            keep_numerals=False,
-        )
-        corpus = corpora.TokenizedCorpus(reader, **kwargs)
+        corpus = corpora.TokenizedCorpus(reader, **DEFAULT_TOKENS_TRANSFORM_OPTS)
         result = [x for x in corpus]
         expected = [
             ('0', ['A', 'B', 'C']),
@@ -156,75 +150,56 @@ class Test_DataFrameVectorize(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_fit_transform_gives_document_term_matrix(self):
-        reader = readers.DataFrameTextTokenizer(self.create_test_dataframe())
-        kwargs = dict(to_lower=False, remove_accents=False, min_len=1, max_len=None, keep_numerals=False)
-        corpus = corpora.TokenizedCorpus(reader, only_any_alphanumeric=False, **kwargs)
-        vectorizer = corpus_vectorizer.CorpusVectorizer()
-        v_corpus = vectorizer.fit_transform(corpus)
-        expected = np.asarray(
-            [
-                [1, 1, 1, 0, 0, 0],
-                [0, 1, 1, 1, 0, 0],
-                [0, 1, 1, 0, 0, 0],
-                [1, 1, 0, 0, 0, 1],
-                [0, 1, 0, 0, 1, 0],
-                [0, 0, 0, 0, 2, 1],
-            ]
+        # Arrange
+        reader = readers.DataFrameTextTokenizer(
+            self.create_test_dataframe()
         )
-        self.assertTrue((expected == v_corpus.bag_term_matrix).all())
-        results = v_corpus.token2id
-        expected = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'F': 5, 'E': 4}
-        self.assertEqual(expected, results)
+        corpus = corpora.TokenizedCorpus(
+            reader,
+            only_any_alphanumeric=False,
+            to_lower=False,
+            remove_accents=False,
+            min_len=1,
+            max_len=None,
+            keep_numerals=False
+        )
+        v_corpus = corpus_vectorizer\
+            .CorpusVectorizer()\
+            .fit_transform(corpus)
 
-    def test_AxAt_of_document_term_matrix_gives_term_term_matrix(self):
+        term_term_matrix = v_corpus.cooccurrence_matrix()
+        token2id = v_corpus.token2id
+
+        assert 2 == term_term_matrix.todense()[token2id['A'], token2id['B']]
+        assert 0 == term_term_matrix.todense()[token2id['C'], token2id['F']]
+
+    def test_to_dataframe_of_term_matrix_gives_expected_result(self):
 
         # Arrange
-        reader = readers.DataFrameTextTokenizer(self.create_test_dataframe())
-        kwargs = dict(to_lower=False, remove_accents=False, min_len=1, max_len=None, keep_numerals=False)
-        corpus = corpora.TokenizedCorpus(reader, only_any_alphanumeric=False, **kwargs)
-        vectorizer = corpus_vectorizer.CorpusVectorizer()
-        v_corpus = vectorizer.fit_transform(corpus)
+        reader = readers.DataFrameTextTokenizer(
+            self.create_test_dataframe()
+        )
+        corpus = corpora.TokenizedCorpus(
+            reader,
+            only_any_alphanumeric=False,
+            to_lower=False,
+            remove_accents=False,
+            min_len=1,
+            max_len=None,
+            keep_numerals=False
+        )
+
+        term_term_matrix = corpus_vectorizer\
+            .CorpusVectorizer()\
+            .fit_transform(corpus)\
+            .cooccurrence_matrix()
 
         # Act
-        term_term_matrix = np.dot(v_corpus.bag_term_matrix.T, v_corpus.bag_term_matrix)
+        coo_df = to_dataframe(term_term_matrix, corpus.id2token, corpus.documents)
 
         # Assert
-        expected = np.asarray(
-            [
-                [2, 2, 1, 0, 0, 1],
-                [2, 5, 3, 1, 1, 1],
-                [1, 3, 3, 1, 0, 0],
-                [0, 1, 1, 1, 0, 0],
-                [0, 1, 0, 0, 5, 2],
-                [1, 1, 0, 0, 2, 2],
-            ]
-        )
-        self.assertTrue((expected == term_term_matrix).all())
-
-        term_term_matrix = scipy.sparse.triu(term_term_matrix, 1)
-        expected = np.asarray(
-            [
-                [0, 2, 1, 0, 0, 1],
-                [0, 0, 3, 1, 1, 1],
-                [0, 0, 0, 1, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 2],
-                [0, 0, 0, 0, 0, 0],
-            ]
-        )
-
-        # print(term_term_matrix.todense())
-        # print(term_term_matrix)
-        coo = term_term_matrix
-        id2token = {i: t for t, i in v_corpus.token2id.items()}
-        cdf = (
-            pd.DataFrame({'w1_id': coo.row, 'w2_id': coo.col, 'value': coo.data})[['w1_id', 'w2_id', 'value']]
-            .sort_values(['w1_id', 'w2_id'])
-            .reset_index(drop=True)
-        )
-        cdf['w1'] = cdf.w1_id.apply(lambda x: id2token[x])
-        cdf['w2'] = cdf.w2_id.apply(lambda x: id2token[x])
-        print(cdf[['w1', 'w2', 'value']])
+        assert 2 == int(coo_df[((coo_df.w1 == 'A') & (coo_df.w2 == 'B'))].value)
+        assert 0 == len(coo_df[((coo_df.w1 == 'C') & (coo_df.w2 == 'F'))])
 
     def test_tokenized_document_token_counts_is_empty_if_enumerable_not_exhausted(self):
         corpus = self.create_simple_test_corpus(
