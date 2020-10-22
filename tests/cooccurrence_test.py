cd2207d2ec0ce2b40da576db4a2ac65c496a2aad
Roger Mähler <roger.mahler@hotmail.com>
2020-10-22 10:13:51 +0200

    Added tests for bug fixes

--- tests/cooccurrence_test.py
+++ tests/cooccurrence_test.py
@@ -1,9 +1,11 @@
 import os
 import random
 
-from penelope.cooccurrence import (
-    WindowsCorpus, cooccurrence_by_partition, corpus_concept_windows, to_coocurrence_matrix, to_dataframe
-)
+import pytest
+
+from penelope.cooccurrence import (WindowsCorpus, cooccurrence_by_partition,
+                                   corpus_concept_windows,
+                                   to_coocurrence_matrix, to_dataframe)
 from penelope.corpus import CorpusVectorizer, TokenizedCorpus
 from penelope.corpus.readers import InMemoryReader
 from penelope.corpus.sparv_corpus import SparvTokenizedCsvCorpus
@@ -169,3 +171,35 @@ def test_cooccurrence_using_cli_succeeds():
     compute_and_store_cooccerrence(**options)
 
     assert os.path.isfile(output_filename)
+
+@pytest.skip("long running, used for bug fixes")
+def test_cooccurrence_of_windowed_corpus_returns_correct_result3():
+
+    concept = {'jag'}
+    n_lr_tokens = 2
+    corpus = SparvTokenizedCsvCorpus(
+        './tests/test_data/riksdagens-protokoll.1920-2019.test.2files.zip',
+        tokenizer_opts=dict(filename_fields="year:_:1", ),
+        pos_includes='|NN|VB|',
+        lemmatize=False,
+    )
+    coo_df = cooccurrence_by_partition(corpus, concept, n_lr_tokens)
+
+    assert coo_df is not None
+
+
+@pytest.skip("long running, used for bug fixes")
+def test_cooccurrence_of_windowed_corpus_returns_correct_result4():
+
+    concept = {'jag'}
+    n_lr_tokens = 2
+    corpus = SparvTokenizedCsvCorpus(
+        './tests/test_data/riksdagens-protokoll.1920-2019.test.zip',
+        tokenizer_opts=dict(filename_fields="year:_:1", ),
+        pos_includes='|NN|VB|',
+        lemmatize=False,
+    )
+    coo_df = cooccurrence_by_partition(corpus, concept, n_lr_tokens)
+
+    assert coo_df is not None
+    assert len(coo_df) > 0
