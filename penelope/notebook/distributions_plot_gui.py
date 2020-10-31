d6c14c8937c2c75717a5c35a4b21c4dd50a781d8
Roger Mähler <roger.mahler@hotmail.com>
2020-10-31 12:37:09 +0100

    Moved from westac project

---
+++ penelope/notebook/distributions_plot_gui.py
@@ -0,0 +1,103 @@
+import types
+from typing import Iterable, List, Sequence
+
+import ipywidgets
+import penelope.plot.distribution_plot as plotter
+from bokeh.plotting import show
+from IPython.display import display
+from penelope.corpus import VectorizedCorpus
+
+
+def display_gui(x_corpus: VectorizedCorpus, tokens: Iterable[str], n_columns: int = 3):
+
+    tokens = sorted(list(tokens))
+    # tokens_map = {token: index for index, token in enumerate(tokens)}
+    gui = types.SimpleNamespace(
+        progress=ipywidgets.IntProgress(
+            description="",
+            min=0,
+            max=10,
+            step=1,
+            value=0,
+            continuous_update=False,
+            layout=ipywidgets.Layout(width="98%"),
+        ),
+        n_count=ipywidgets.IntSlider(
+            description="Count",
+            min=0,
+            max=100,
+            step=1,
+            value=3,
+            continuous_update=False,
+            layout=ipywidgets.Layout(width="300px"),
+        ),
+        forward=ipywidgets.Button(
+            description=">>",
+            button_style="Success",
+            layout=ipywidgets.Layout(width="40px", color="green"),
+        ),
+        back=ipywidgets.Button(
+            description="<<",
+            button_style="Success",
+            layout=ipywidgets.Layout(width="40px", color="green"),
+        ),
+        split=ipywidgets.ToggleButton(
+            description="Split", layout=ipywidgets.Layout(width="80px", color="green")
+        ),
+        output=ipywidgets.Output(layout=ipywidgets.Layout(width="99%")),
+        wtokens=ipywidgets.SelectMultiple(options=tokens, value=[], rows=30),
+    )
+
+    def update_plot(*_):
+
+        gui.output.clear_output()
+
+        selected_tokens: Sequence[str] = gui.wtokens.value
+
+        if len(selected_tokens) == 0:
+            selected_tokens = tokens[: gui.n_count.value]
+
+        indices: List[int] = [x_corpus.token2id[token] for token in selected_tokens]
+
+        with gui.output:
+            x_columns: int = n_columns if gui.split.value else None
+            p = plotter.plot_distributions(x_corpus, indices, n_columns=x_columns)
+            show(p)
+
+    def stepper_clicked(b):
+
+        current_token = gui.wtokens.value[0] if len(gui.wtokens.value) > 0 else tokens[0]
+        current_index = tokens.index(current_token)
+
+        if b.description == "<<":
+            current_index = max(current_index - gui.n_count.value, 0)
+
+        if b.description == ">>":
+            current_index = min(
+                current_index + gui.n_count.value, len(tokens) - gui.n_count.value + 1
+            )
+
+        gui.wtokens.value = tokens[current_index : current_index + gui.n_count.value]
+
+    def split_changed(*_):
+        update_plot()
+
+    def token_select_changed(*_):
+        update_plot()
+
+    gui.n_count.observe(update_plot, "value")
+    gui.split.observe(split_changed, "value")
+    gui.wtokens.observe(token_select_changed, "value")
+    gui.forward.on_click(stepper_clicked)
+    gui.back.on_click(stepper_clicked)
+
+    display(
+        ipywidgets.VBox(
+            [
+                gui.progress,
+                ipywidgets.HBox([gui.back, gui.forward, gui.n_count, gui.split]),
+                ipywidgets.HBox([gui.wtokens, gui.output]),
+            ]
+        )
+    )
+    update_plot()
---
+++ penelope/plot/distribution_plot.py
@@ -0,0 +1,183 @@
+import itertools
+import math
+from typing import Any, Callable, List, Sequence, Tuple, Union
+
+import bokeh
+import numpy as np
+from bokeh.plotting import Figure
+
+import penelope.common.curve_fit as cf
+from penelope.corpus import VectorizedCorpus
+
+SmootherFunction = Callable[[Any, Any], Tuple[Any, Any]]
+
+
+def plot_distribution(
+    xs: Union[str, Sequence[float]],
+    ys: Union[str, Sequence[float]],
+    *,
+    plot: Figure = None,
+    title: str = '',
+    color: str = 'navy',
+    ticker_labels=None,
+    smoothers: Sequence[SmootherFunction] = None,
+    **kwargs,
+) -> Figure:
+    """Plots a distribution defined by coordinate vectors `xs` and `ys`.
+
+    Parameters
+    ----------
+    xs : Union[str, Sequence[float]]
+        X-coordinates
+    ys : Union[str, Sequence[float]]
+        Y-coordinates
+    plot : Figure, optional
+        Figure to use, if None then a new Figure is created, by default None
+    title : str, optional
+        Plot title, by default ''
+    color : str, optional
+        Color for plot, by default 'navy'
+    ticker_labels : [type], optional
+        Ticker labels to use, by default None
+    smoothers : Sequence[SmootherFunction], optional
+        List of functions to apply on coordinates before plot, by default None
+
+    Returns
+    -------
+    Figure
+    """
+    if plot is None:
+
+        p: Figure = bokeh.plotting.figure(
+            plot_width=kwargs.get('plot_width', 400), plot_height=kwargs.get('plot_height', 200)
+        )
+        p.y_range.start = 0
+        # p.y_range.end = 0.5
+        # p.title.text = title.upper()
+        p.yaxis.axis_label = 'Frequency'
+        p.toolbar.autohide = True
+        if ticker_labels is not None:
+            p.xaxis.ticker = ticker_labels
+        p.xaxis.major_label_orientation = math.pi / 2
+        p.xgrid.grid_line_color = None
+        p.ygrid.grid_line_color = None
+
+    else:
+        p = plot
+
+    _ = p.scatter(xs, ys, size=2, color=color, alpha=1.0, legend_label=title)
+
+    # p.line(xs, ys , line_width=2, color=color, alpha=0.5, legend_label=title)
+    for smoother in smoothers or []:
+        xs, ys = smoother(xs, ys)
+
+    _ = p.line(xs, ys, line_width=2, color=color, alpha=0.5, legend_label=title)
+
+    # p.vbar(x=xs, top=ys, width=0.5, color=color, alpha=0.1)
+    # p.step(xs, ys, line_width=2, color=color, alpha=0.5)
+    # _, _, _, lx, ly = gof.fit_ordinary_least_square(ys, xs)
+    # p.line(x=lx, y=ly, line_width=1, color=color, alpha=0.6, legend_label=title)
+
+    p.legend.location = "top_left"
+    p.legend.click_policy = "hide"
+    p.legend.background_fill_alpha = 0.0
+
+    return p
+
+
+def plot_distributions(
+    x_corpus: VectorizedCorpus,
+    indices: List[int],
+    n_columns: int = 3,
+    *,
+    width: int = 1000,
+    height: int = 600,
+    smoothers: SmootherFunction = None,
+) -> Figure:
+    """Plots word distributions (columns) over time for words at indices `indices` in `x_corpus`' (i.e. the document-term-matrix.)
+
+    The `x-axis` is defined by the range of years that the corpus spans.
+
+    If `n_columns` is None then all distributions are plotted on the same plot. If `n_columns` is specified, then
+    the distributions are plotted on separate plot in a grid with 'n_columns' plots per row.,
+
+    Parameters
+    ----------
+    x_corpus : VectorizedCorpus
+    indices : List[int]
+        List of indices (token ids) to plot.
+    n_columns : int, optional
+        Number of plots per row, if `None` then all in one plot, by default 3
+    width : int, optional
+        Total plot width, by default 1000
+    height : int, optional
+        Total plot height, by default 600
+    smoothers : SmootherFunction, optional
+        List of functions  to use to smooth the lines, by default None
+
+    Returns
+    -------
+    Figure
+        bokeh Figure
+    """
+    x_corpus = x_corpus.todense()
+
+    smoothers = smoothers or [cf.rolling_average_smoother('nearest', 3), cf.pchip_spline]
+
+    colors = itertools.cycle(bokeh.palettes.Category10[10])
+    x_range = x_corpus.year_range()
+    xs = np.arange(x_range[0], x_range[1] + 1, 1)
+
+    plots = []
+    p = None
+
+    for token_id in indices:
+        try:
+
+            ys = x_corpus.data[:, token_id]
+
+            p = plot_distribution(
+                xs,
+                ys,
+                plot=p if n_columns is None else None,
+                title=x_corpus.id2token[token_id].upper(),
+                color=next(colors),
+                plot_width=width if n_columns is None else max(int(width / n_columns), 400),
+                plot_height=height if n_columns is None else max(int(height / n_columns), 300),
+                ticker_labels=xs if n_columns is None else None,
+                smoothers=smoothers,
+            )
+            plots.append(p)
+
+        except Exception as ex:  # pylint: disable=bare-except
+
+            print(ex)
+
+    if n_columns is not None:
+
+        p = bokeh.layouts.gridplot([plots[u : u + n_columns] for u in range(0, len(indices), n_columns)])
+
+    return p
+
+
+# def plot_distribution2(ys: Union[str, Sequence[float]], window_size, mode='nearest'):
+
+#     xs = np.arange(0, len(ys), 1)
+#     yw = scipy.ndimage.filters.uniform_filter1d(ys, size=window_size, mode=mode)
+#     xw = np.arange(0, len(yw), 1)
+
+#     # xp, yp = cf.fit_curve(cf.fit_polynomial3, xs, ys, step=0.1)
+#     xp, yp = cf.fit_polynomial4(xs, ys)
+#     p = bokeh.plotting.figure(
+#         plot_width=800,
+#         plot_height=600,
+#     )
+#     p.scatter(xs, ys, size=6, color='red', alpha=0.6, legend_label='actual')
+#     p.line(x=xs, y=ys, line_width=1, color='red', alpha=0.6, legend_label='actual')
+#     p.line(x=xp, y=yp, line_width=1, color='green', alpha=0.6, legend_label='poly')
+#     p.xgrid.grid_line_color = None
+#     p.ygrid.grid_line_color = None
+
+#     p.line(x=xw, y=yw, line_width=1, color='green', alpha=1, legend_label='rolling')
+
+#     return p
