11f2faf079555ec8636ab5898a90ac3c151233c1
Roger Mähler <roger.mahler@hotmail.com>
2020-11-11 07:41:34 +0100

    Export method

--- penelope/co_occurrence/__init__.py
+++ penelope/co_occurrence/__init__.py
@@ -2,12 +2,12 @@ from .compute_hal_or_glove import compute
 from .concept_co_occurrence import (
     ConceptContextOpts,
     corpus_concept_windows,
+    filter_co_coccurrences_by_global_threshold,
     load_co_occurrences,
     partitioned_corpus_concept_co_occurrence,
     store_co_occurrences,
     to_vectorized_corpus,
     tokens_concept_windows,
-    filter_co_coccurrences_by_global_threshold,
 )
 
 # from .compute_partitioned import (compute_for_column_group, load_text_windows,
