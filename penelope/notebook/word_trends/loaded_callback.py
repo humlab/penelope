36c8cd5ccb9f941919d3e78a265c703bf790a3ab
Roger Mähler <roger.mahler@hotmail.com>
2020-11-14 23:28:06 +0100

    Removed global state Removed state parameter Renamed state to  data Renamed State to WordTrendData Add read of options from file Moved dataclass to GUI

--- penelope/notebook/word_trends/loaded_callback.py
+++ penelope/notebook/word_trends/loaded_callback.py
@@ -1,66 +1,49 @@
 import os
-from dataclasses import dataclass
-from typing import Dict
 
 import ipywidgets
-import pandas as pd
 import penelope.common.goodness_of_fit as gof
 import penelope.notebook.utility as notebook_utility
-import penelope.notebook.word_trends.word_trends_gui as xxx_word_trends_gui
-from penelope.common.goodness_of_fit import GoodnessOfFitComputeError
 from penelope.corpus import VectorizedCorpus
 from penelope.notebook.ipyaggrid_utility import display_grid
 from penelope.utility import getLogger
 
+from .word_trends_gui import WordTrendData, display_gui
+
 logger = getLogger("penelope")
 
 
-@dataclass
-class State:
-    corpus: VectorizedCorpus = None
-    corpus_folder: str = None
-    corpus_tag: str = None
-    compute_options: Dict = None
-    goodness_of_fit: pd.DataFrame = None
-    most_deviating_overview: pd.DataFrame = None
-    most_deviating: pd.DataFrame = None
-
-
-def update_state(
-    state: State,  # pylint: disable=redefined-outer-name
+def update_trend_data(
     *,
     corpus: VectorizedCorpus = None,
     corpus_folder: str = None,
     corpus_tag: str = None,
-    n_count: int = 10000,
-    **_,
-):
-
-    state.corpus = corpus
-    state.corpus_folder = corpus_folder
-    state.corpus_tag = corpus_tag
-
-    state.goodness_of_fit = gof.compute_goddness_of_fits_to_uniform(state.corpus, None, verbose=False)
-    state.most_deviating_overview = gof.compile_most_deviating_words(state.goodness_of_fit, n_count=n_count)
-    state.most_deviating = gof.get_most_deviating_words(
-        state.goodness_of_fit, 'l2_norm', n_count=n_count, ascending=False, abs_value=True
+    n_count: int = 25000,
+) -> WordTrendData:
+    data = WordTrendData()
+    data.corpus = corpus
+    data.corpus_folder = corpus_folder
+    data.corpus_tag = corpus_tag
+    data.compute_options = VectorizedCorpus.load_options(tag=corpus_tag, folder=corpus_folder)
+    data.goodness_of_fit = gof.compute_goddness_of_fits_to_uniform(data.corpus, None, verbose=False)
+    data.most_deviating_overview = gof.compile_most_deviating_words(data.goodness_of_fit, n_count=n_count)
+    data.most_deviating = gof.get_most_deviating_words(
+        data.goodness_of_fit, 'l2_norm', n_count=n_count, ascending=False, abs_value=True
     )
-
-    return state
+    return data
 
 
 def build_layout(
-    state: State,  # pylint: disable=redefined-outer-name
+    data: WordTrendData,  # pylint: disable=redefined-outer-name
 ):
-    tab_trends = xxx_word_trends_gui.display_gui(state=state, display_widgets=False)
+    tab_trends = display_gui(trend_data=data, display_widgets=False)
 
     tab_gof = (
         notebook_utility.OutputsTabExt(["GoF", "GoF (abs)", "Plots", "Slopes"])
-        .display_fx_result(0, display_grid, state.goodness_of_fit)
-        .display_fx_result(1, display_grid, state.most_deviating_overview[['l2_norm_token', 'l2_norm', 'abs_l2_norm']])
-        .display_fx_result(2, gof.plot_metrics, state.goodness_of_fit, plot=False, lazy=True)
+        .display_fx_result(0, display_grid, data.goodness_of_fit)
+        .display_fx_result(1, display_grid, data.most_deviating_overview[['l2_norm_token', 'l2_norm', 'abs_l2_norm']])
+        .display_fx_result(2, gof.plot_metrics, data.goodness_of_fit, plot=False, lazy=True)
         .display_fx_result(
-            3, gof.plot_slopes, state.corpus, state.most_deviating, "l2_norm", 600, 600, plot=False, lazy=True
+            3, gof.plot_slopes, data.corpus, data.most_deviating, "l2_norm", 600, 600, plot=False, lazy=True
         )
     )
 
@@ -74,19 +57,14 @@ def build_layout(
     return layout
 
 
-state = State()
-
-
 def loaded_callback(
-    output: ipywidgets.Output,
     *,
     corpus: VectorizedCorpus = None,
     corpus_folder: str = None,
     corpus_tag: str = None,
-    n_count: int = 10000,
+    output: ipywidgets.Output = None,
     **kwargs,
 ):
-    global state
 
     with output:
         try:
@@ -99,23 +77,26 @@ def loaded_callback(
 
             if corpus is None:
                 logger.info("Please wait, loading corpus...")
-                # FIXME IS GROUP BY YEAR NEEDED?
-                corpus = VectorizedCorpus.load(tag=corpus_tag, folder=corpus_folder).group_by_year()
+                corpus = VectorizedCorpus.load(tag=corpus_tag, folder=corpus_folder)
+
+            corpus = corpus.group_by_year()
 
             output.clear_output()
 
-            _ = update_state(
-                state,
+            logger.info("Please wait, compiling data...")
+
+            data = update_trend_data(
                 corpus=corpus,
                 corpus_folder=corpus_folder,
                 corpus_tag=corpus_tag,
-                n_count=n_count,
+                n_count=kwargs.get('n_count', 25000),
                 **kwargs,
             )
 
-            build_layout(state=state).display()
+            logger.info("Done!")
+            build_layout(data=data).display()
 
-        except GoodnessOfFitComputeError as ex:
+        except gof.GoodnessOfFitComputeError as ex:
             logger.info(f"Unable to compute GoF: {str(ex)}")
         except Exception as ex:
             logger.exception(ex)
