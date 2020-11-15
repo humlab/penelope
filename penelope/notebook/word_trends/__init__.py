97eb4b45d25d6f27ac9b492d25f0b64ee08c2c3f
Roger Mähler <roger.mahler@hotmail.com>
2020-11-15 20:39:40 +0100

    Tidy

--- penelope/notebook/word_trends/__init__.py
+++ penelope/notebook/word_trends/__init__.py
@@ -1,5 +1,5 @@
+from .display_word_trends import build_layout, display_word_trends, update_trend_data
 from .displayers import DISPLAYERS as WORD_TREND_DISPLAYERS
 from .displayers import BarDisplayer, ITrendDisplayer, LineDisplayer, TableDisplayer, WordTrendData
-from .display_word_trends import display_word_trends, update_trend_data
 from .word_trends_gui import word_trend_gui
 from .word_trends_pick_gui import word_trends_pick_gui
