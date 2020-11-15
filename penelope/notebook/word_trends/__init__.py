65b1e607fae51844bfb10a205f416d8d8a7ef998
Roger Mähler <roger.mahler@hotmail.com>
2020-11-15 12:34:24 +0100

    Renamed file

--- penelope/notebook/word_trends/__init__.py
+++ penelope/notebook/word_trends/__init__.py
@@ -1,5 +1,5 @@
 from .displayers import DISPLAYERS as WORD_TREND_DISPLAYERS
 from .displayers import BarDisplayer, ITrendDisplayer, LineDisplayer, TableDisplayer, WordTrendData
-from .loaded_callback import display_word_trends
+from .display_word_trends import display_word_trends
 from .word_trends_gui import word_trend_gui
 from .word_trends_pick_gui import word_trends_pick_gui
