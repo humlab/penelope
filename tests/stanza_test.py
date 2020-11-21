fe6c18989a6d7161a0b326a2fe992237195b59e8
Roger Mähler <roger.mahler@hotmail.com>
2020-11-21 22:45:33 +0100

    Added stanza dependency

---
+++ tests/stanza_test.py
@@ -0,0 +1,11 @@
+import stanza
+from penelope.vendor.stanza.utility import download_model
+
+#stanza.download('en')
+
+def test_can_create_stanza_en_pipeline():
+
+    download_model('en')
+    _ = stanza.Pipeline('en')
+
+    #en_nlp = stanza.Pipeline('en', processors='tokenize,lemma,pos,depparse', verbose=False, use_gpu=False)
