3b54ba95badc5216dc30f0959c4246bc12891564
Roger Mähler <roger.mahler@hotmail.com>
2020-11-23 20:27:30 +0100

    Stanza placeholder

--- penelope/vendor/stanza/utility.py
+++ penelope/vendor/stanza/utility.py
@@ -1,3 +1,5 @@
+import os
+
 import stanza
 
 
@@ -7,3 +9,18 @@ def download_model(lang: str):
         del nlp
     except Exception:
         stanza.download(lang)
+
+
+def howto_install_corenlp(folder: str = "/data/vendor/corenlp"):
+    stanza.install_corenlp(dir=folder)
+
+
+def howto_download_corenlp_models(model: str, version: str, folder: str = None):
+    """Example how models can be fownloaded: """
+    if not folder:
+        if "CORENLP_HOME" in os.environ:
+            folder = os.environ["CORENLP_HOME"]
+        else:
+            FileNotFoundError("Either supply target folder or set CORENLP_HOME environ variable")
+
+    stanza.download_corenlp_models(model=model, version=version, dir=folder)
