17e26c351a3233b182ef0f0f0e4f46011d07f4b4
Roger Mähler <roger.mahler@hotmail.com>
2020-11-23 20:27:08 +0100

    Test placeholder

--- tests/stanza_test.py
+++ tests/stanza_test.py
@@ -10,3 +10,11 @@ def test_can_create_stanza_en_pipeline():
     _ = stanza.Pipeline('en')
 
     # en_nlp = stanza.Pipeline('en', processors='tokenize,lemma,pos,depparse', verbose=False, use_gpu=False)
+
+
+# def test_can_create_stanza_en_pipeline():
+
+
+#     stanza.install_corenlp(dir="/content/corenlp")
+#     os.environ["CORENLP_HOME"] = "./corenlp"
+#     stanza.download_corenlp_models(model='german', version='4.1.0', dir="/content/corenlp")
