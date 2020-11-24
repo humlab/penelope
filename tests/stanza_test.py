a0d6a9c86773f310e0c77cc5033bd72d8aad3b6a
Roger Mähler <roger.mahler@hotmail.com>
2020-11-24 08:15:30 +0100

    Removed stanza

--- tests/stanza_test.py
+++ tests/stanza_test.py
@@ -1,20 +1,20 @@
-import stanza
-from penelope.vendor.stanza.utility import download_model
+# import stanza
+# from penelope.vendor.stanza.utility import download_model
 
-# stanza.download('en')
-
-
-def test_can_create_stanza_en_pipeline():
-
-    download_model('en')
-    _ = stanza.Pipeline('en')
-
-    # en_nlp = stanza.Pipeline('en', processors='tokenize,lemma,pos,depparse', verbose=False, use_gpu=False)
+# # stanza.download('en')
 
 
 # def test_can_create_stanza_en_pipeline():
 
+#     download_model('en')
+#     _ = stanza.Pipeline('en')
 
-#     stanza.install_corenlp(dir="/content/corenlp")
-#     os.environ["CORENLP_HOME"] = "./corenlp"
-#     stanza.download_corenlp_models(model='german', version='4.1.0', dir="/content/corenlp")
+#     # en_nlp = stanza.Pipeline('en', processors='tokenize,lemma,pos,depparse', verbose=False, use_gpu=False)
+
+
+# # def test_can_create_stanza_en_pipeline():
+
+
+# #     stanza.install_corenlp(dir="/content/corenlp")
+# #     os.environ["CORENLP_HOME"] = "./corenlp"
+# #     stanza.download_corenlp_models(model='german', version='4.1.0', dir="/content/corenlp")
