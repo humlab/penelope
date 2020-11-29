e728e4944bb4e2f0b03b0c2b77788ade97836df1
Roger Mähler <roger.mahler@hotmail.com>
2020-11-29 04:30:04 +0100

    Fixed exports (lint)

--- penelope/vendor/spacy/__init__.py
+++ penelope/vendor/spacy/__init__.py
@@ -1,4 +1,18 @@
 from ._utils import load_document_index, store_document_index
+from .config import CorpusConfig
+from .convert import (
+    CHECKPOINT_SERIALIZERS,
+    CheckpointData,
+    ContentSerializeOpts,
+    ContentSerializer,
+    dataframe_to_tokens,
+    load_checkpoint,
+    spacy_doc_to_annotated_dataframe,
+    store_checkpoint,
+    text_to_annotated_dataframe,
+    texts_to_annotated_dataframes,
+    to_vectorized_corpus,
+)
 from .interfaces import ContentType, DocumentPayload, ITask, PipelineError, PipelinePayload
 from .pipeline import CorpusPipeline, SpacyPipeline
 from .tasks_mixin import PipelineShortcutMixIn, SpacyPipelineShortcutMixIn
