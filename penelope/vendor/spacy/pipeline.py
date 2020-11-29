37aed471059ef7efe7b649be72d8936563e5c847
Roger Mähler <roger.mahler@hotmail.com>
2020-11-29 04:25:17 +0100

    Made re-resolvable

--- penelope/vendor/spacy/pipeline.py
+++ penelope/vendor/spacy/pipeline.py
@@ -1,6 +1,8 @@
 import collections
 from typing import Any, Iterator, List, Sequence, Union
 
+from penelope.vendor.spacy.interfaces import ContentType
+
 from . import interfaces, tasks_mixin
 
 
@@ -15,7 +17,7 @@ class CorpusPipeline(tasks_mixin.PipelineShortcutMixIn, tasks_mixin.SpacyPipelin
         self._payload = payload
         self._tasks: List[interfaces.ITask] = []
         self.add(tasks or [])
-        self.resolved = False
+        # self.resolved = False
 
     @property
     def payload(self) -> interfaces.PipelinePayload:
@@ -31,6 +33,12 @@ class CorpusPipeline(tasks_mixin.PipelineShortcutMixIn, tasks_mixin.SpacyPipelin
             return self.tasks[index - 1]
         return None
 
+    def get_prior_content_type(self, task: interfaces.ITask) -> ContentType:
+        prior_task = self.get_prior_to(task)
+        if prior_task is None:
+            return ContentType.NONE
+        return prior_task.out_content_type
+
     def setup(self):
         for task in self.tasks:
             task.chain()
@@ -39,14 +47,14 @@ class CorpusPipeline(tasks_mixin.PipelineShortcutMixIn, tasks_mixin.SpacyPipelin
 
     def resolve(self) -> Iterator[interfaces.DocumentPayload]:
         """Resolves the pipeline by calling outstream on last task"""
-        if not self.resolved:
-            self.setup()
-            self.resolved = True
+        # if not self.resolved:
+        self.setup()
+        #    self.resolved = True
         return self.tasks[-1].outstream()
 
     def exhaust(self) -> "CorpusPipeline":
-        if self.resolved:
-            raise interfaces.PipelineError("cannot exhaust an already resolved pipeline")
+        # if self.resolved:
+        #     raise interfaces.PipelineError("cannot exhaust an already resolved pipeline")
         collections.deque(self.resolve(), maxlen=0)
         return self
 
