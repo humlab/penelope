dfc05539c31cd8fccca326a0635866bfaac9cc95
Roger Mähler <roger.mahler@hotmail.com>
2020-12-03 15:14:32 +0100

    added with except

--- penelope/utility/decorators.py
+++ penelope/utility/decorators.py
@@ -20,3 +20,16 @@ def try_catch(func, exceptions=None, suppress=False, nice=False):
                 raise
 
     return wrapper
+
+class ExpectException:
+    def __init__(self, exception_class):
+        self.exception_class = exception_class
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None and not isinstance(exc_val,
+                                                   self.exception_class):
+            raise self.exception_class(str(exc_val)) from exc_val
+        return False
