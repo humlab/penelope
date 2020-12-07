a368b9960b24920001f9d1a1c8e1226957c60167
Roger Mähler <roger.mahler@hotmail.com>
2020-12-07 17:15:05 +0100

    Fixed exports

--- penelope/notebook/word_trends/__init__.py
+++ penelope/notebook/word_trends/__init__.py
@@ -1,5 +1,5 @@
-from .display_word_trends import display_word_trends
 from .displayers import WORD_TREND_DISPLAYERS, BarDisplayer, ITrendDisplayer, LineDisplayer, TableDisplayer
 from .word_trend_data import WordTrendData
-from .word_trends_gui import word_trend_gui
+from .word_trends_gui import WordTrendsGUI, create_gui
 from .word_trends_pick_gui import word_trends_pick_gui
+from .word_trends_tabs_gui import TabsGUI, create_tabs_gui
