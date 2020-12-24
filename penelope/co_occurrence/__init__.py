9b4a466efd02acb746ee04b1a22a4dcccf0a499f
Roger Mähler <roger.mahler@hotmail.com>
2020-12-24 23:33:12 +0100

    Updated exports

--- penelope/co_occurrence/__init__.py
+++ penelope/co_occurrence/__init__.py
@@ -1,19 +1,22 @@
 # type: ignore
 from .co_occurrence import corpus_co_occurrence, corpus_to_windows, tokens_to_windows
 from .convert import (
+    CO_OCCURRENCE_FILENAME_PATTERN,
+    CO_OCCURRENCE_FILENAME_POSTFIX,
+    Bundle,
+    create_options_bundle,
+    filename_to_folder_and_tag,
+    folder_and_tag_to_filename,
+    load_bundle,
     load_co_occurrences,
     store_bundle,
     store_co_occurrences,
     to_co_occurrence_matrix,
     to_dataframe,
+    to_trends_data,
     to_vectorized_corpus,
 )
 from .hal_or_glove import GloveVectorizer, HyperspaceAnalogueToLanguageVectorizer, compute_hal_or_glove_co_occurrences
-from .interface import CO_OCCURRENCE_FILENAME_POSTFIX, ContextOpts, CoOccurrenceError
-from .partitioned import partitioned_corpus_co_occurrence
+from .interface import ContextOpts, CoOccurrenceError
+from .partitioned import ComputeResult, partitioned_corpus_co_occurrence
 from .windows_corpus import WindowsCorpus, WindowsStream
-
-# tests/co_occurrence_test: from penelope.co_occurrence import WindowsCorpus, to_co_occurrence_matrix, to_dataframe
-# penelope/co_occurrence/__init__.py: from .co_occurrence import ContextOpts, corpus_co_occurrence, corpus_to_windows, tokens_to_windows
-# penelope/co_occurrence/co_occurrence.py: from .convert import to_dataframe
-# penelope/co_occurrence/convert.py: from .co_occurrence import ContextOpts
