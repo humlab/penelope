bb3a3798a0062e71ef3cc53214941ab4c613db36
Roger Mähler <roger.mahler@hotmail.com>
2020-12-24 22:43:35 +0100

    Simplified GUI, removed duplicate functions

--- penelope/notebook/co_occurrence/explore_co_occurrence_gui.py
+++ penelope/notebook/co_occurrence/explore_co_occurrence_gui.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-import penelope.common.goodness_of_fit as gof
 import penelope.notebook.utility as notebook_utility
 import penelope.notebook.word_trends as word_trends
 from penelope.notebook.ipyaggrid_utility import display_grid
@@ -11,48 +10,37 @@ logger = getLogger()
 
 
 @dataclass
-class ExploreCoOccurrencesGUI:
+class ExploreGUI:
 
-    trends_data: TrendsData
+    trends_data: TrendsData = None
+
+    tab_main: notebook_utility.OutputsTabExt = None
+
+    trends_gui: word_trends.TrendsGUI = None
+    gofs_gui: word_trends.GoFsGUI = None
+
+    def setup(self) -> "ExploreGUI":
+
+        self.tab_main = notebook_utility.OutputsTabExt(["Data", "Trends", "Options", "GoF"], layout={'width': '98%'})
+        self.trends_gui = word_trends.TrendsGUI().setup()
+        self.gofs_gui = word_trends.GoFsGUI().setup()
+
+        return self
+
+    def display(self, trends_data: TrendsData) -> "ExploreGUI":
+
+        self.trends_data = trends_data
+
+        self.trends_gui.display(trends_data=trends_data)
+        self.gofs_gui.display(trends_data=trends_data)
+
+        self.tab_main.display_fx_result(0, display_grid, trends_data.memory.get('co_occurrences'), clear=True)
+        self.tab_main.display_as_yaml(2, trends_data.compute_options, clear=True, width='800px', height='600px')
+
+        return self
 
     def layout(self):
-
-        tab_gof = (
-            notebook_utility.OutputsTabExt(["GoF", "GoF (abs)", "Plots", "Slopes"], layout={'width': '100%'})
-            .display_fx_result(0, display_grid, self.trends_data.goodness_of_fit)
-            .display_fx_result(
-                1, display_grid, self.trends_data.most_deviating_overview[['l2_norm_token', 'l2_norm', 'abs_l2_norm']]
-            )
-            .display_fx_result(2, gof.plot_metrics, self.trends_data.goodness_of_fit, plot=False, lazy=True)
-            .display_fx_result(
-                3,
-                gof.plot_slopes,
-                self.trends_data.corpus,
-                self.trends_data.most_deviating,
-                "l2_norm",
-                600,
-                600,
-                plot=False,
-                lazy=True,
-            )
+        layout = self.tab_main.display_content(1, what=self.trends_gui.layout(), clear=True).display_content(
+            3, self.gofs_gui.layout(), clear=True
         )
-
-        gof_and_trends_gui = word_trends.GofTrendsGUI(
-            gofs_gui=word_trends.GoFsGUI().setup(),
-            trends_gui=word_trends.TrendsGUI().setup(),
-        )
-
-        trends_with_pick_gui = word_trends.TrendsWithPickTokensGUI.create(
-            self.trends_data.corpus, tokens=self.trends_data.most_deviating
-        )
-
-        layout = (
-            notebook_utility.OutputsTabExt(["Data", "Trends", "Explore", "Options", "GoF"], layout={'width': '100%'})
-            .display_fx_result(0, display_grid, self.trends_data.memory.get('co_occurrences'))
-            .display_content(1, what=gof_and_trends_gui.layout(), clear=True)
-            .display_content(2, what=trends_with_pick_gui.layout(), clear=True)
-            .display_as_yaml(3, self.trends_data.compute_options, clear=True, width='800px', height='600px')
-            .display_content(4, tab_gof, clear=True)
-        )
-
         return layout
