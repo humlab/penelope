32abe1f5944b8b0ba65395978166add659088ff5
Roger Mähler <roger.mahler@hotmail.com>
2020-12-24 22:40:40 +0100

    Deprecated class

--- penelope/notebook/word_trends/gof_and_trends_gui.py
+++ penelope/notebook/word_trends/gof_and_trends_gui.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import penelope.notebook.utility as notebook_utility
+from gensim.utils import deprecated
 from penelope.utility import getLogger
 
 from .gofs_gui import GoFsGUI
@@ -13,6 +14,7 @@ logger = getLogger("penelope")
 # display(debug_view)
 
 
+@deprecated
 @dataclass
 class GofTrendsGUI:
     """GUI component for combined display of tokens distributions and goodness of fit to uniform distribution."""
@@ -20,15 +22,19 @@ class GofTrendsGUI:
     trends_gui: TrendsGUI
     gofs_gui: GoFsGUI
 
+    def setup(self) -> "GofTrendsGUI":
+        self.trends_gui = TrendsGUI().setup()
+        self.gofs_gui = GoFsGUI().setup()
+        return self
+
     def layout(self) -> notebook_utility.OutputsTabExt:
         _layout = (
-            notebook_utility.OutputsTabExt(["Trends", "GoF"], layout={'width': '100%'})
+            notebook_utility.OutputsTabExt(["Trends", "GoF"], layout={'width': '98%'})
             .display_content(0, what=self.trends_gui.layout(), clear=True)
             .display_content(1, what=self.gofs_gui.layout(), clear=True)
         )
         return _layout
 
     def display(self, trends_data: TrendsData):
-        # self.trends_data = trends_data
         self.gofs_gui.display(trends_data=trends_data)
         self.trends_gui.display(trends_data=trends_data)
