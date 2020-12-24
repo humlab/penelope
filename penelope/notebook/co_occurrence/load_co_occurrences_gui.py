555978388ddb08ddc149aef4f13aef300aa2f7fd
Roger Mähler <roger.mahler@hotmail.com>
2020-12-24 23:34:02 +0100

    Reduced complexity

--- penelope/notebook/co_occurrence/load_co_occurrences_gui.py
+++ penelope/notebook/co_occurrence/load_co_occurrences_gui.py
@@ -2,23 +2,21 @@ import os
 from dataclasses import dataclass, field
 from typing import Callable
 
-from ipywidgets import Button, HBox, Label, Layout, VBox  # , Output
-from penelope.co_occurrence import load_co_occurrences, to_vectorized_corpus
-from penelope.co_occurrence.interface import CO_OCCURRENCE_FILENAME_POSTFIX
-from penelope.corpus import VectorizedCorpus
+import penelope.co_occurrence as co_occurrence
+from ipywidgets import Button, HBox, Label, Layout, Output, VBox
 from penelope.notebook.utility import FileChooserExt2
-from penelope.utility import default_data_folder, getLogger, read_json, replace_extension, right_chop
+from penelope.utility import default_data_folder, getLogger
 
 logger = getLogger('penelope')
 
 # pylint: disable=attribute-defined-outside-init, too-many-instance-attributes
 
 
-# view = Output(layout={"border": "1px solid black"})
+debug_view = Output(layout={"border": "1px solid black"})
 
 
 @dataclass
-class GUI:
+class LoadGUI:
 
     default_path: str = field(default=None)
     default_filename: str = field(default='')
@@ -39,7 +37,7 @@ class GUI:
 
         self._load_button.disabled = True
         try:
-            self.load_callback(self)
+            self.load_callback(self.filename)
         except (ValueError, FileNotFoundError) as ex:
             print(ex)
         except Exception as ex:
@@ -51,7 +49,7 @@ class GUI:
     def file_selected(self, *_):
         self._load_button.disabled = not self._filename.selected
 
-    def setup(self, filename_pattern: str, load_callback: str) -> "GUI":  # pylint: disable=redefined-outer-name)
+    def setup(self, filename_pattern: str, load_callback: str) -> "LoadGUI":  # pylint: disable=redefined-outer-name)
 
         self.filename_pattern = filename_pattern
 
@@ -83,71 +81,46 @@ class GUI:
                         ),
                     ]
                 ),
-                # view,
+                debug_view,
             ]
         )
 
     @property
     def filename(self):
-        return self.filename.selected
+        return self._filename.selected
 
 
-# @view.capture(clear_output=True)
-def load_callback(
-    args: GUI,
-    loaded_callback: Callable,
-):
+@debug_view.capture(clear_output=True)
+def create_load_gui(
+    data_folder: str,
+    filename_pattern: str = co_occurrence.CO_OCCURRENCE_FILENAME_PATTERN,
+    loaded_callback: Callable[[co_occurrence.Bundle], None] = None,
+) -> "LoadGUI":
+    @debug_view.capture(clear_output=True)
+    def load_callback(filename: str):
+        load_co_occurrence_bundle(filename, loaded_callback)
 
+    gui: LoadGUI = LoadGUI(default_path=data_folder).setup(
+        filename_pattern=filename_pattern, load_callback=load_callback
+    )
+    return gui
+
+
+@debug_view.capture(clear_output=True)
+def load_co_occurrence_bundle(filename: str, loaded_callback: Callable[[co_occurrence.Bundle], None]):
     try:
 
-        if not args.filename or not os.path.isfile(args.filename):
+        if not filename or not os.path.isfile(filename):
             raise ValueError("Please select co-occurrence file")
 
-        co_occurrences = load_co_occurrences(args.filename)
-
-        options_filename = replace_extension(args.filename, 'json')
-        if os.path.isfile(options_filename):
-            compute_options = read_json(options_filename)
-        else:
-            logger.warning(f"options file {options_filename} not found")
-            compute_options = {'not_found': options_filename}
-
-        corpus_folder = os.path.split(args.filename)
-        corpus_tag = right_chop(args.filename, CO_OCCURRENCE_FILENAME_POSTFIX)
-
-        corpus = (
-            VectorizedCorpus.load(folder=corpus_folder, tag=corpus_tag)
-            if VectorizedCorpus.dump_exists(folder=corpus_folder, tag=corpus_tag)
-            else to_vectorized_corpus(
-                co_occurrences=co_occurrences,
-                value_column='value_n_t',
-            ).group_by_year()
-        )
+        bundle = co_occurrence.load_bundle(filename)
 
         if loaded_callback is not None:
-            loaded_callback(
-                corpus_folder=corpus_folder,
-                corpus_tag=corpus_tag,
-                co_occurrences=co_occurrences,
-                corpus=corpus,
-                compute_options=compute_options,
-            )
+            loaded_callback(bundle)
 
     except (ValueError, FileNotFoundError, PermissionError) as ex:
         logger.error(ex)
+        raise
     except Exception as ex:
         logger.error(ex)
-
-
-def create_gui(
-    data_folder: str,
-    filename_pattern: str = "*.co_occurrence.csv.zip",
-    loaded_callback: Callable[..., None] = None,
-):
-
-    gui = GUI(default_path=data_folder).setup(
-        filename_pattern=filename_pattern,
-        load_callback=lambda args: load_callback(args, loaded_callback),
-    )
-
-    return gui
+        raise
