513c2a731d49b57eb1b888c5b8d1856d1f36c40a
Roger Mähler <roger.mahler@hotmail.com>
2021-01-07 20:01:53 +0100

    Adapted to tng transform opts

--- penelope/corpus/readers/dataframe_text_tokenizer.py
+++ penelope/corpus/readers/dataframe_text_tokenizer.py
@@ -2,13 +2,19 @@ import pandas as pd
 from penelope.vendor.nltk import word_tokenize
 
 from . import tng
-from .text_transformer import TextTransformer
 
 
 class DataFrameTextTokenizer(tng.CorpusReader):
     def __init__(self, data: pd.DataFrame, text_column='txt', **column_filters):
+        text_transformer = tng.TextTransformer(
+            transform_opts=tng.TextTransformOpts().clear().add([
+                tng.KnownTransformType.fix_unicode,
+                tng.KnownTransformType.fix_whitespaces,
+                tng.KnownTransformType.fix_hyphenation
+            ])
+        )
         super().__init__(
             source=tng.PandasSource(data, text_column=text_column, filename_column='filename', **column_filters),
-            transformer=TextTransformer().fix_unicode().fix_whitespaces().fix_hyphenation(),
+            transformer=text_transformer,
             tokenizer=word_tokenize,
         )
