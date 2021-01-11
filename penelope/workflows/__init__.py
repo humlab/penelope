1f22eabccb413a6855614d8fabb889ec1eea5dfd
Roger Mähler <roger.mahler@hotmail.com>
2021-01-11 12:09:03 +0100

    Supressed pylance warnings

--- penelope/workflows/__init__.py
+++ penelope/workflows/__init__.py
@@ -1,3 +1,4 @@
+# type: ignore
 from ._co_occurrence import execute_workflow as co_occurrence_workflow
 from ._vectorize_tokenized_corpus import execute_workflow as vectorize_corpus_workflow
 from .utils import WorkflowException
