dece41703e36a29c96c70080a339d3995cdae478
Roger Mähler <roger.mahler@hotmail.com>
2021-01-26 08:47:54 +0100

    Made GUI more testable Deprecated old bugfix

--- penelope/notebook/mdw/main_gui.py
+++ penelope/notebook/mdw/main_gui.py
@@ -15,14 +15,14 @@ def display_mdw(corpus: dtm.VectorizedCorpus, df_mdw):  # pylint: disable=unused
 
 
 @view_gui.capture(clear_output=True)
-def loaded_callback(
+def default_loaded_callback(
     corpus: dtm.VectorizedCorpus, corpus_folder: str, corpus_tag: str
 ):  # pylint: disable=unused-argument
     mdw_gui = create_mdw_gui(corpus, done_callback=display_mdw)
     display(mdw_gui.layout())
 
 
-def create_main_gui(corpus_folder: str, loaded_callback=loaded_callback) -> VBox:
+def create_main_gui(corpus_folder: str, loaded_callback=default_loaded_callback) -> VBox:
 
     gui = load_dtm_gui.create_load_gui(corpus_folder=corpus_folder, loaded_callback=loaded_callback)
     return VBox([gui.layout(), view_gui, view_display])
