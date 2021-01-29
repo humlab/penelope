7b88d09643274ef6318d91e63bd71da8c38806ae
Roger Mähler <roger.mahler@hotmail.com>
2021-01-29 08:01:18 +0100

    Resolves #122 Added type hints.

--- penelope/notebook/topic_modelling/display_topic_topic_network.py
+++ penelope/notebook/topic_modelling/display_topic_topic_network.py
@@ -1,8 +1,9 @@
 # Visualize topic co-occurrence
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple, Union
 
 import bokeh
 import bokeh.plotting
+import pandas as pd
 import penelope.network.plot_utility as plot_utility
 import penelope.network.utility as network_utility
 import penelope.utility as utility
@@ -26,11 +27,43 @@ def get_topic_titles(topic_token_weights, topic_id=None, n_words=100):
     return df
 
 
+def get_filtered_network_data(
+    inferred_topics: InferredTopicsData,
+    filters: Dict[str, Any],
+    threshold: float,
+    ignores: List[int],
+    period: Union[int, Tuple[int,int]],
+    n_docs: int,
+) -> pd.DataFrame:
+
+    df = filter_document_topic_weights(inferred_topics.document_topic_weights, filters=filters, threshold=threshold)
+
+    if ignores is not None:
+        df = df[~df.topic_id.isin(ignores)]
+
+    if len(period or []) == 2:
+        df = df[(df.year >= period[0]) & (df.year <= period[1])]
+
+    if isinstance(period, int):
+        df = df[df.year == period]
+
+    df = df.merge(df, how='inner', left_on='document_id', right_on='document_id')
+    df = df[(df.topic_id_x < df.topic_id_y)]
+
+    df = df.groupby([df.topic_id_x, df.topic_id_y]).size().reset_index()
+
+    df.columns = ['source', 'target', 'n_docs']
+
+    if n_docs > 1:
+        df = df[df.n_docs >= n_docs]
+    return df
+
+
 # pylint: disable=too-many-arguments, too-many-locals
 def display_topic_topic_network(
     inferred_topics: InferredTopicsData,
-    filters,
-    period=None,
+    filters: Dict[str, Any],
+    period: Union[int, Tuple[int,int]]=None,
     ignores: List[int] = None,
     threshold: float = 0.10,
     layout: str = 'Fruchterman-Reingold',
@@ -50,26 +83,7 @@ def display_topic_topic_network(
         if 'document_id' not in document_index.columns:
             raise ValueError("Supplied document index has no document_id")
 
-        df = filter_document_topic_weights(inferred_topics.document_topic_weights, filters=filters, threshold=threshold)
-
-        if ignores is not None:
-            df = df[~df.topic_id.isin(ignores)]
-
-        if len(period or []) == 2:
-            df = df[(df.year >= period[0]) & (df.year <= period[1])]
-
-        if isinstance(period, int):
-            df = df[df.year == period]
-
-        df = df.merge(df, how='inner', left_on='document_id', right_on='document_id')
-        df = df[(df.topic_id_x < df.topic_id_y)]
-
-        df = df.groupby([df.topic_id_x, df.topic_id_y]).size().reset_index()
-
-        df.columns = ['source', 'target', 'n_docs']
-
-        if n_docs > 1:
-            df = df[df.n_docs >= n_docs]
+        df = get_filtered_network_data(inferred_topics, filters, threshold, ignores, period, n_docs)
 
         if len(df) == 0:
             print('No data. Please change selections.')
@@ -83,7 +97,7 @@ def display_topic_topic_network(
                 scale=scale,
                 threshold=0.0,
                 node_description=titles,
-                node_proportions=(topic_proportions or 10),
+                node_proportions=topic_proportions,
                 weight_scale=1.0,
                 normalize_weights=False,
                 element_id=text_id,
@@ -105,6 +119,6 @@ def display_topic_topic_network(
                 df.to_csv(filename, sep='\t')
                 print('Data stored in file {}'.format(filename))
 
-    except:  # pylint: disable=bare-except
+    except Exception as ex:  # pylint: disable=bare-except
         print("No data: please adjust filters")
-        # raise
+        logger.info(ex)
