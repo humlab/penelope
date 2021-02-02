06e4ba8fcc22df1a9457c193c5201414fa7d02cd
Roger Mähler <roger.mahler@hotmail.com>
2021-02-02 20:48:15 +0100

    Removed guard

--- penelope/notebook/ipyaggrid_utility.py
+++ penelope/notebook/ipyaggrid_utility.py
@@ -57,9 +57,9 @@ def display_grid(
     else:
         raise ValueError(f"Data must be dict or pandas.DataFrame not {type(data)}")
 
-    if os.environ.get('VSCODE_LOG_STACK', None) is not None:
-        logging.warning("bug-check: vscode detected, aborting plot...")
-        return df
+    # if os.environ.get('VSCODE_LOG_STACK', None) is not None:
+    #     logging.warning("bug-check: vscode detected, aborting plot...")
+    #     return df
 
     column_defs = default_column_defs(df) if column_defs is None else column_defs
 
