3cda64b93b2394059287a708204642b4d0e25b30
Roger Mähler <roger.mahler@hotmail.com>
2021-04-08 13:13:20 +0200

    Added inline test data

--- tests/pipeline/checkpoint_test.py
+++ tests/pipeline/checkpoint_test.py
@@ -1,4 +1,7 @@
 import os
+from io import StringIO
+
+import pandas as pd
 
 import penelope.pipeline.checkpoint as checkpoint
 from penelope.corpus.readers.interfaces import ExtractTaggedTokensOpts
@@ -23,92 +26,47 @@ def test_spaCy_load_tagged_frame_checkpoint():
 def test_phrased_tagged_frame():
 
     os.makedirs('./tests/output', exist_ok=True)
-
-    checkpoint_filename: str = "./tests/test_data/legal_instrument_five_docs_test_pos_csv.zip"
-    checkpoint_opts: checkpoint.CheckpointOpts = None  # CheckpointOpts()
-    data = checkpoint.load_checkpoint(
-        source_name=checkpoint_filename, checkpoint_opts=checkpoint_opts, reader_opts=None
+    opts = dict(
+        extract_opts=ExtractTaggedTokensOpts(lemmatize=False),
+        filter_opts = None,
+        text_column= 'text',
+        lemma_column = 'lemma_',
+        pos_column = 'pos_',
+        ignore_case = False,
+        verbose = True
     )
-    payload = next(data.payload_stream)
+    data_str: str = """	text	lemma_	pos_	is_punct	is_stop
+0	Constitution	constitution	NOUN	False	False
+1	of	of	ADP	False	True
+2	the	the	DET	False	True
+3	United	United	PROPN	False	False
+4	Nations	Nations	PROPN	False	False
+5	Educational	Educational	PROPN	False	False
+6	,	,	PUNCT	True	False
+7	Scientific	Scientific	PROPN	False	False
+8	and	and	CCONJ	False	True
+9	Cultural	Cultural	PROPN	False	False
+10	Organization	Organization	PROPN	False	False"""
 
-    tokens = tagged_frame_to_tokens(
-        payload.content,
-        ExtractTaggedTokensOpts(lemmatize=False),
-    )
+    tagged_frame: pd.date_range1 = pd.read_csv(StringIO(data_str), sep='\t', index_col=0)
+
+    tokens = tagged_frame_to_tokens(tagged_frame, **opts)
     assert tokens is not None
-    phrases = {'United Nations': 'United_Nations', 'United': 'United'}
-    phrased_tokens = tagged_frame_to_tokens(
-        payload.content,
-        ExtractTaggedTokensOpts(lemmatize=False),
-        phrases=phrases,
-    )
-    assert phrased_tokens[:9] == [
-        'Constitution',
-        'of',
-        'the',
-        'United_Nations',
-        'Educational',
-        ',',
-        'Scientific',
-        'and',
-        'Cultural',
-    ]
 
-    phrases = {'United': 'United', 'United Nations': 'United_Nations'}
-    phrased_tokens = tagged_frame_to_tokens(
-        payload.content,
-        ExtractTaggedTokensOpts(lemmatize=False),
-        phrases=phrases,
-    )
-    assert phrased_tokens[:9] == [
-        'Constitution',
-        'of',
-        'the',
-        'United_Nations',
-        'Educational',
-        ',',
-        'Scientific',
-        'and',
-        'Cultural',
-    ]
+    phrases = {'United Nations': 'United_Nations', 'United': 'United'}
+    phrased_tokens = tagged_frame_to_tokens(tagged_frame, **opts, phrases=phrases)
+    assert phrased_tokens[:9] == 'Constitution of the United_Nations Educational , Scientific and Cultural'.split(' ')
 
     phrases = {'United Nations': 'United_Nations', 'the United Nations': 'the_United_Nations'}
-
-    phrased_tokens = tagged_frame_to_tokens(
-        payload.content,
-        ExtractTaggedTokensOpts(lemmatize=False),
-        phrases=phrases,
-    )
-    assert phrased_tokens[:8] == [
-        'Constitution',
-        'of',
-        'the_United_Nations',
-        'Educational',
-        ',',
-        'Scientific',
-        'and',
-        'Cultural',
-    ]
+    phrased_tokens = tagged_frame_to_tokens(tagged_frame, **opts, phrases=phrases)
+    assert phrased_tokens[:8] == 'Constitution of the_United_Nations Educational , Scientific and Cultural'.split(' ')
 
     phrases = {'united nations': 'United_Nations'}
 
     phrased_tokens = tagged_frame_to_tokens(
-        payload.content,
-        ExtractTaggedTokensOpts(lemmatize=False),
-        phrases=phrases,
-        ignore_case=True,
+        tagged_frame, phrases=phrases, **{**opts, **{'ignore_case': True}}
     )
-    assert phrased_tokens[:9] == [
-        'Constitution',
-        'of',
-        'the',
-        'United_Nations',
-        'Educational',
-        ',',
-        'Scientific',
-        'and',
-        'Cultural',
-    ]
+    assert phrased_tokens[:9] == 'Constitution of the United_Nations Educational , Scientific and Cultural'.split(' ')
 
 
 def test_python_list_merge():
