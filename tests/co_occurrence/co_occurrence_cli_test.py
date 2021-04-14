82d8e40e82010e8149ca5fc1c088e3e8ee99b30a
Roger Mähler <roger.mahler@hotmail.com>
2021-04-14 08:52:02 +0200

    CLI test case

---
+++ tests/co_occurrence/co_occurrence_cli_test.py
@@ -0,0 +1,28 @@
+from penelope.scripts.co_occurrence import process_co_ocurrence
+
+def test_process_co_ocurrence():
+
+    args: dict = dict(
+        corpus_config="./tests/test_data/",
+        input_filename="./tests/test_data/riksdagens-protokoll.1920-2019.test.zip",
+        output_filename="./tests/output/APA",
+        concept="medium",
+        no_concept=False,
+        context_width=6,
+        partition_key="year",
+        create_subfolder=True,
+        pos_includes="|NN|PM|UO|",
+        pos_excludes="|MAD|MID|PAD|",
+        to_lowercase=True,
+        lemmatize=True,
+        remove_stopwords='swedish',
+        min_word_length=1,
+        max_word_length=None,
+        keep_symbols=True,
+        keep_numerals=True,
+        only_any_alphanumeric=False,
+        only_alphabetic=False,
+        count_threshold=10,
+    )
+
+    process_co_ocurrence(**dict)
