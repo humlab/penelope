9fb46da836129ef8006e0564aa3f5d705f7ac7d3
Roger Mähler <roger.mahler@hotmail.com>
2021-04-21 21:23:28 +0200

    Test update

--- tests/notebook/token_counts/create_token_count_gui_test.py
+++ tests/notebook/token_counts/create_token_count_gui_test.py
@@ -66,11 +66,11 @@ def test_create_token_count_gui_create_succeeds():
     assert load_corpus_config_callback_is_called
 
 
-@pytest.mark.skip(reason="long running")
+#@pytest.mark.skip(reason="long running")
 def test_load_document_index(config: corpus_config.CorpusConfig):
 
-    config.pipeline_payload.source = 'riksdagens-protokoll.1920-2019.sparv4.csv.zip'
-    config.pipeline_payload.folders('/data/westac/data')
+    config.pipeline_payload.source = 'riksdagens-protokoll.1920-2019.9files.sparv4.csv.zip'
+    config.pipeline_payload.folders('./tests/test_data')
 
     document_index: pd.DataFrame = tokens_count_gui.load_document_index(config)
 
