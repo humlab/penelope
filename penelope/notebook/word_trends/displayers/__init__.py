0b2fdfadd2c8d160bddb93f7a3d7ba22b35c9597
Roger Mähler <roger.mahler@hotmail.com>
2021-04-28 11:41:27 +0200

    Silenced matplotlib

--- penelope/notebook/word_trends/displayers/__init__.py
+++ penelope/notebook/word_trends/displayers/__init__.py
@@ -1,10 +1,18 @@
 # type: ignore
+# isort: skip_file
+
+import logging
+
+logging.getLogger("matplotlib").setLevel(level=logging.ERROR)
+
 from ._compile_mixins import CategoryDataMixin, LinesDataMixin, PenelopeBugCheck
 from ._displayer import ITrendDisplayer
 from .display_bar import BarDisplayer
 from .display_line import LineDisplayer
+from .display_network import NetworkDisplayer, create_network
 from .display_table import TableDisplayer, UnnestedExplodeTableDisplayer, UnnestedTableDisplayer
 from .display_top_table import TopTokensDisplayer
+
 
 DEFAULT_WORD_TREND_DISPLAYERS = [
     TableDisplayer,
@@ -12,4 +20,5 @@ DEFAULT_WORD_TREND_DISPLAYERS = [
     LineDisplayer,
     BarDisplayer,
     TopTokensDisplayer,
+    # NetworkDisplayer,
 ]
