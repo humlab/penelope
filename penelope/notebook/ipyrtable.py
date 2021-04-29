0f4ad38d75cd7f3616400f483554a846788c1fd8
Roger Mähler <roger.mahler@hotmail.com>
2021-04-29 12:11:09 +0200

    JupyterLab 3.0

--- penelope/notebook/ipyrtable.py
+++ penelope/notebook/ipyrtable.py
@@ -1,17 +1,17 @@
-from typing import Union
+# from typing import Union
 
-import ipyregulartable as irt
-import pandas as pd
+# import ipyregulartable as irt
+# import pandas as pd
 
 
-def display_table(data: Union[dict, pd.DataFrame]) -> irt.RegularTableWidget:
+# def display_table(data: Union[dict, pd.DataFrame]) -> irt.RegularTableWidget:
 
-    if isinstance(data, dict):
-        df = pd.DataFrame(data=data)  # .set_index('year')
-    elif isinstance(data, pd.DataFrame):
-        df = data
-    else:
-        raise ValueError(f"Data must be dict or pandas.DataFrame not {type(data)}")
+#     if isinstance(data, dict):
+#         df = pd.DataFrame(data=data)  # .set_index('year')
+#     elif isinstance(data, pd.DataFrame):
+#         df = data
+#     else:
+#         raise ValueError(f"Data must be dict or pandas.DataFrame not {type(data)}")
 
-    w = irt.RegularTableWidget(df)
-    return w
+#     w = irt.RegularTableWidget(df)
+#     return w
