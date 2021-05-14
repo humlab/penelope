c4113d9ecfd39ec0a1b48eda7be447cb7d58f809
Roger Mähler <roger.mahler@hotmail.com>
2021-05-14 16:19:29 +0200

    Minor speed improvement

--- penelope/corpus/utils.py
+++ penelope/corpus/utils.py
@@ -9,11 +9,10 @@ def generate_token2id(terms: Iterator[Iterator[str]], n_docs: int = None) -> dic
 
     token2id = defaultdict()
     token2id.default_factory = token2id.__len__
-    tokens_iter = tqdm(terms, desc="Vocab", total=n_docs, position=0, leave=True) if n_docs > 0 else terms
+    tokens_iter = tqdm(terms, desc="Vocab", total=n_docs, position=0, mininterval=1.0, leave=True) #if n_docs > 0 else terms
     for tokens in tokens_iter:
         for token in tokens:
             _ = token2id[token]
-        tokens_iter.set_description(f"Vocab #{len(token2id)}")
     return dict(token2id)
 
 
