13517bf731f9d65cb5872a5bdfb7ca3314d8d958
Roger Mähler <roger.mahler@hotmail.com>
2021-05-16 09:52:35 +0200

    Added token type alias

--- penelope/co_occurrence/interface.py
+++ penelope/co_occurrence/interface.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Set
+from typing import Set, Union
 
 import pandas as pd
 from penelope.corpus import DocumentIndex, Token2Id
@@ -7,6 +7,8 @@ from penelope.utility import getLogger
 from prometheus_client import Counter
 
 logger = getLogger('penelope')
+
+Token = Union[int, str]
 
 
 class CoOccurrenceError(ValueError):
@@ -30,7 +32,7 @@ class ComputeResult:
 class ContextOpts:
 
     context_width: int = 2
-    concept: Set[str] = field(default_factory=set)
+    concept: Set[Token] = field(default_factory=set)
     ignore_concept: bool = False
 
     @property
