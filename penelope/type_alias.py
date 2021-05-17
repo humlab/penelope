59d16ff1fa40c193e1c2abc04a45801a2c5b9363
Roger Mähler <roger.mahler@hotmail.com>
2021-05-17 19:01:49 +0200

    Added type aliases

--- penelope/type_alias.py
+++ penelope/type_alias.py
@@ -6,3 +6,4 @@ PartitionKeys = Union[str, List[str], Callable]
 FilenameTokensTuple = Tuple[str, Iterable[str]]
 FilenameTokensTuples = Iterable[FilenameTokensTuple]
 CoOccurrenceDataFrame = pd.DataFrame
+DocumentIndex = pd.DataFrame
