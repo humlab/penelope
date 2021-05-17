d68745115abd613a4f32a5c0deaef4625de70226
Roger Mähler <roger.mahler@hotmail.com>
2021-05-17 13:55:43 +0200

    Implemented co-occurrence dataframe to DTM

--- penelope/co_occurrence/partition_by_document/convert.py
+++ penelope/co_occurrence/partition_by_document/convert.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import scipy
+from penelope.corpus import DocumentIndex, Token2Id, VectorizedCorpus
+from penelope.type_alias import CoOccurrenceDataFrame
+
+# from ..interface import PartitionKeyNotUniqueKey
 
 # def compute_normalized_count(co_occurrences: pd.DataFrame, document_index: DocumentIndex) -> pd.Series:
 
@@ -67,3 +71,51 @@ def co_occurrence_term_term_matrix_to_dataframe(
         co_occurrences = co_occurrences[co_occurrences.value >= threshold_count]
 
     return co_occurrences
+
+
+def co_occurrence_dataframe_to_vectorized_corpus(
+    *,
+    co_occurrences: CoOccurrenceDataFrame,
+    token2id: Token2Id,
+    document_index: DocumentIndex,
+    # partition_key: Union[int, str] = "document_id",
+) -> VectorizedCorpus:
+    """Creates a DTM corpus from a co-occurrence result set that was partitioned by `partition_column`."""
+
+    """Create distinct word-pair tokens and assign a token_id"""
+    to_token = token2id.id2token.get
+    token_pairs = co_occurrences[["w1_id", "w2_id"]].unique()
+    token_pairs["token"] = token_pairs.w1_id.apply(to_token) + "/" + token_pairs.w2_id.apply(to_token)
+    token_pairs["token_id"] = token_pairs.index
+
+    """Create a new vocabulary"""
+    vocabulary = token_pairs.set_index("token").to_dict()
+
+    """Merge and assign token_id to co-occurring pairs"""
+    token_ids: pd.Series = co_occurrences.merge(
+        token_pairs.set_index(['w1_id', 'w2_id']),
+        how='left',
+        left_on=['w1_id', 'w2_id'],
+        right_index=True,
+    ).token_id
+
+    """Set document_id as unique key for DTM document index """
+    document_index = document_index.set_index('document_id', drop=False).rename_axis('').sort_index()
+
+    """Make certain taht matrix gets right shape (to avoid offset errors)"""
+    shape = (len(document_index.index.size), len(vocabulary))
+    coo_matrix = scipy.sparse.coo_matrix(
+        (
+            co_occurrences.weight,
+            (
+                co_occurrences.document_id,
+                token_ids.astype(np.uint32),
+            ),
+        ),
+        shape=shape,
+    )
+
+    """Create the final corpus"""
+    corpus = VectorizedCorpus(coo_matrix, token2id=vocabulary, document_index=document_index)
+
+    return corpus
