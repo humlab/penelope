b1120c739ec334461bc0527dc615a961c61c01e7
Roger Mähler <roger.mahler@hotmail.com>
2021-05-17 12:26:25 +0200

    Added type alias

--- penelope/type_alias.py
+++ penelope/type_alias.py
@@ -1,5 +1,8 @@
 from typing import Callable, Iterable, List, Tuple, Union
 
+import pandas as pd
+
 PartitionKeys = Union[str, List[str], Callable]
 FilenameTokensTuple = Tuple[str, Iterable[str]]
 FilenameTokensTuples = Iterable[FilenameTokensTuple]
+CoOccurrenceDataFrame = pd.DataFrame
