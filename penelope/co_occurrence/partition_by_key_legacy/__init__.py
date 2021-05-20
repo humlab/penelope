3995ee1d616cde33cd65e04703a0769a714fc42b
Roger Mähler <roger.mahler@hotmail.com>
2021-05-20 10:57:47 +0200

    Deprecated code

--- penelope/co_occurrence/partition_by_key_legacy/__init__.py
+++
@@ -1,8 +0,0 @@
-# type: ignore
-
-from .compute import compute_co_occurrence, compute_corpus_co_occurrence
-from .convert import (
-    co_occurrences_to_co_occurrence_corpus,
-    term_term_matrix_to_co_occurrences,
-    to_vectorized_windows_corpus,
-)
