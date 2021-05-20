68a6555bf73517f7e8eef7df503087254a3dad58
Roger Mähler <roger.mahler@hotmail.com>
2021-05-20 16:37:12 +0200

    Added test

---
+++ tests/notebook/co_occurrence/main_gui_test.py
@@ -0,0 +1,15 @@
+from penelope import co_occurrence
+from penelope.notebook.co_occurrence import main_gui
+
+
+def test_to_trends_data():
+
+    folder, tag = './tests/test_data/VENUS', 'VENUS'
+
+    filename = co_occurrence.to_filename(folder=folder, tag=tag)
+
+    bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(filename, compute_corpus=False)
+
+    trends_data = main_gui.to_trends_data(bundle).update()
+
+    assert trends_data is not None
