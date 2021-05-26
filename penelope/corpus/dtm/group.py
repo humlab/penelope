01b05d24b59c6631adc954d3a8cd2c81d8118e18
Roger Mähler <roger.mahler@hotmail.com>
2021-05-26 10:22:53 +0200

    Added generic group_by_document_index

--- penelope/corpus/dtm/group.py
+++ penelope/corpus/dtm/group.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Sequence, TypeVar, Union
+from typing import List, Optional, Sequence, Tuple, TypeVar, Union
 
 import numpy as np
 import pandas as pd
@@ -63,7 +63,7 @@ class GroupByMixIn:
             bag_term_matrix,
             token2id=self.token2id,
             document_index=document_index,
-            token_counter=self.token_counter,
+            term_frequency_mapping=self.term_frequency_mapping,
         )
 
         return corpus
@@ -79,9 +79,22 @@ class GroupByMixIn:
         aggregate: str = 'sum',
         fill_gaps: bool = False,
     ) -> IVectorizedCorpus:
-        """Returns a new corpus where documents have been grouped and summed up by year groups.
-        Adds a new column named 'lustrum', 'decade' or 'periods' (if dict)"""
+        """[summary] Returns a new corpus where documents have been grouped and summed up by [period] + group_columns.
 
+        Adds a new column named 'lustrum', 'decade' or 'periods' (if dict)
+
+        Args:
+            self (IVectorizedCorpusProtocol): Vectorized corpus
+            period (Union[str, dict]): Periods to group by ("year", "lustrum", "decade" or user defined via dict)
+            aggregate (str, optional): Aggregate function. Defaults to 'sum'.
+            fill_gaps (bool, optional): Defaults to False.
+
+        Raises:
+            VectorizedCorpusError: [description]
+
+        Returns:
+            IVectorizedCorpus: [description]
+        """
         known_periods: dict = {'year': 1, 'lustrum': 5, 'decade': 10}
 
         if isinstance(period, str):
@@ -108,6 +121,90 @@ class GroupByMixIn:
         )
 
         return corpus
+
+    def group_by_document_index(
+        self: IVectorizedCorpusProtocol,
+        period_specifier: Union[str, dict],
+        aggregate: str = 'sum',
+        extra_grouping_columns: Optional[Union[str, List[str]]] = None,
+    ) -> IVectorizedCorpus:
+
+        DTM: scipy.sparse.spmatrix = self.bag_term_matrix
+
+        document_index, group_indicies = categorize_document_index(
+            self.document_index,
+            period_specifier,
+            extra_grouping_columns,
+        )
+
+        assert aggregate in {'sum', 'mean'}
+
+        shape = (len(document_index), DTM.shape[1])
+        dtype = np.uint32 if np.issubdtype(DTM.dtype, np.integer) and aggregate == 'sum' else np.float64
+
+        matrix: scipy.sparse.lil_matrix = scipy.sparse.lil_matrix(shape, dtype=dtype)
+
+        for document_id, category in document_index.category.to_dict().items():
+
+            indices = group_indicies.get(category, [])
+
+            if len(indices) == 0:
+                continue
+
+            matrix[document_id, :] = (
+                DTM[indices, :].mean(axis=0) if aggregate == 'mean' else DTM[indices, :].sum(axis=0)
+            )
+
+        grouped_corpus: IVectorizedCorpus = self.create(
+            matrix.tocsr(),
+            token2id=self.token2id,
+            document_index=document_index,
+            term_frequency_mapping=self.term_frequency_mapping,
+        )
+        return grouped_corpus
+
+
+def categorize_document_index(
+    document_index: pd.DataFrame,
+    period_specifier: Union[str, dict],
+    extra_grouping_columns: Optional[Union[str, List[str]]] = None,
+) -> Tuple[pd.DataFrame, dict]:
+
+    if extra_grouping_columns:
+        raise NotImplementedError("extra_grouping_columns not implemented")
+
+    known_periods: dict = {'year': 1, 'lustrum': 5, 'decade': 10}
+
+    if isinstance(period_specifier, str):
+
+        if period_specifier not in known_periods:
+            raise ValueError(f"{period_specifier} is not a known period specifier")
+
+        if period_specifier != 'year':
+            categorizer = lambda y: y - int(y % known_periods[period_specifier])
+
+    else:
+        year_group_mapping = dict_of_key_values_inverted_to_dict_of_value_key(period_specifier)
+        categorizer = lambda x: year_group_mapping.get(x, np.nan)
+
+    document_index['category'] = (
+        document_index.year if period_specifier == 'year' else document_index.year.apply(categorizer)
+    )
+
+    # if isinstance(extra_grouping_columns, str):
+    #     extra_grouping_columns = [extra_grouping_columns]
+
+    group_indicies = document_index.groupby('category').apply(lambda x: x.index.tolist()).to_dict()
+
+    grouped_document_index = (
+        DocumentIndexHelper(document_index)
+        .group_by_column('category', extra_grouping_columns=None)  # extra_grouping_columns
+        .document_index.set_index('document_id', drop=False)
+        .sort_index(axis=0)
+    )
+    grouped_document_index.columns = [name.replace('_sum', '') for name in grouped_document_index.columns]
+
+    return grouped_document_index, group_indicies
 
 
 def _get_unique_category_values(category_series: pd.Series, fill_gaps: bool = True, fill_steps: int = 1):
