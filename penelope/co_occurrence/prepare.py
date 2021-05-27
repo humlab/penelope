9ceda916b74f7d37d5ebf249d31f12754073f008
Roger Mähler <roger.mahler@hotmail.com>
2021-05-27 13:17:49 +0200

    Overhaul of class - Changed, less confusing API - Properties given more descriptive names - Explicit pivot key naming - Fixed str vs List[str] bugg - Implemented `largest`, `trunk_by_global_count`

--- penelope/co_occurrence/prepare.py
+++ penelope/co_occurrence/prepare.py
@@ -1,67 +1,74 @@
 from typing import List, Union
 
+import numpy as np
 import pandas as pd
 from penelope.corpus import DocumentIndex, Token2Id
-from penelope.type_alias import CoOccurrenceDataFrame
+from penelope.corpus.dtm.vectorized_corpus import VectorizedCorpus
 
 IntOrStr = Union[int, str]
+
+
+ID_COLUMNS: List[str] = ['token_id', 'w1_id', 'w2_id']
+VALUE_COLUMNS: List[str] = ['value', 'n_tokens', 'n_raw_tokens']
+TOKEN_COLUMNS: List[str] = ['w1', 'w2', 'token']
 
 
 class CoOccurrenceHelper:
     def __init__(
         self,
-        co_occurrences: CoOccurrenceDataFrame,
-        token2id: Token2Id,
-        document_index: DocumentIndex,
-        pivot_keys: List[str] = None,
+        *,
+        corpus: VectorizedCorpus,
+        source_token2id: Token2Id,
+        pivot_keys: Union[str, List[str]] = None,
     ):
+        self.corpus: VectorizedCorpus = corpus
+        self.source_token2id: Token2Id = source_token2id
+        self.corpus_pivot_keys: List[str] = [pivot_keys] if isinstance(pivot_keys, str) else pivot_keys
 
-        self.co_occurrences: CoOccurrenceDataFrame = co_occurrences
-        self.token2id: Token2Id = token2id
-        self.document_index: DocumentIndex = document_index
-        self.data: pd.DataFrame = self.co_occurrences  # .copy()
-        self.pivot_keys: List[str] = pivot_keys
+        self.co_occurrences: pd.DataFrame = self.corpus.to_co_occurrences(source_token2id)  # .copy()
+        self.data: pd.DataFrame = self.co_occurrences
+        self.data_pivot_keys: List[str] = self.corpus_pivot_keys
 
     def reset(self) -> "CoOccurrenceHelper":
         self.data: pd.DataFrame = self.co_occurrences  # .copy()
-        self.pivot_keys = None
+        self.data_pivot_keys = self.corpus_pivot_keys
         return self
 
-    def groupby(self, keys: Union[str, List[str]]) -> "CoOccurrenceHelper":
+    def groupby(self, pivot_keys: Union[str, List[str]], normalize_key: str = 'n_raw_tokens') -> "CoOccurrenceHelper":
+        """Groups co-occurrences data frame by given keys"""
 
-        if not keys:
-            raise ValueError("group keys is not specified")
+        if self.data_pivot_keys:
+            raise ValueError("Already grouped, please reset before calling again")
+
+        if not pivot_keys:
+            raise ValueError("pivot keys is not specified")
 
         data: pd.DataFrame = self.data
 
-        if isinstance(keys, str):
-            keys = [keys]
+        if isinstance(pivot_keys, str):
+            pivot_keys = [pivot_keys]
 
-        document_index: DocumentIndex = self.document_index.set_index('document_id')
+        pivot_keys = [g for g in pivot_keys if g in self.corpus.document_index.columns and g not in data.columns]
 
-        keys = [g for g in keys if g in document_index.columns and g not in data.columns]
-
-        if len(keys) == 0:
-            raise ValueError(f"group keys {' '.join(keys)} not found in document index")
-        # counter_columns = [c for c in ['n_tokens', 'n_raw_tokens'] if c in document_index.columns]
+        if len(pivot_keys) == 0:
+            raise ValueError("No keys to group by!")
 
         """Add grouping columns to data"""
-        data = data.merge(document_index[keys], left_on='document_id', right_index=True, how='inner')
+        data = data.merge(self.corpus.document_index[pivot_keys], left_on='document_id', right_index=True, how='inner')
 
         """Group and sum up data"""
-        data = data.groupby(keys + ['w1_id', 'w2_id'])['value'].sum().reset_index()
+        data = data.groupby(pivot_keys + ID_COLUMNS)['value'].sum().reset_index()
 
-        """Divide yearly window counts with yearly token counts"""
-        normalize_key = 'n_raw_tokens'
+        """Divide window counts with time-periods token counts"""
         data['value_n_t'] = data.value / pd.merge(
-            data[keys],
-            document_index.groupby(keys)[normalize_key].sum(),  # Yearly token counts
+            data[pivot_keys],
+            self.corpus.document_index.groupby(pivot_keys)[normalize_key].sum(),  # Yearly token counts
             left_on='year',
             right_index=True,
         )[normalize_key]
 
         self.data = data
-        self.pivot_keys = keys
+        self.data_pivot_keys = pivot_keys
 
         return self
 
@@ -70,10 +77,14 @@ class CoOccurrenceHelper:
         if 'w1' in self.data.columns:
             return self
 
-        fg = self.token2id.id2token.get
+        fg = self.source_token2id.id2token.get
         self.data["w1"] = self.data.w1_id.apply(fg)
         self.data["w2"] = self.data.w2_id.apply(fg)
-        self.data["token"] = self.data.w1 + '/' + self.data.w2
+
+        fg = self.corpus.id2token.get
+        self.data["token"] = self.data.token_id.apply(fg)
+        # faster:
+        # self.data["token"] = self.data.w1 + '/' + self.data.w2
 
         return self
 
@@ -82,9 +93,9 @@ class CoOccurrenceHelper:
         if threshold < 2:
             return self
 
-        global_tokens_counts: pd.Series = self.co_occurrences.groupby(['token'])['value'].sum()
-        threshold_tokens: pd.Index = global_tokens_counts[global_tokens_counts >= threshold].index
-        self.data = self.data.set_index('token').loc[threshold_tokens]  # [['year', 'value', 'value_n_t']]
+        low_frequency_ids: np.ndarray = self.corpus.term_frequencies_below_threshold(threshold)
+
+        self.data = self.data[~self.data.token_id.isin(low_frequency_ids)]
 
         return self
 
@@ -93,7 +104,7 @@ class CoOccurrenceHelper:
         data: pd.DataFrame = self.data
 
         if match_tokens:
-            include_ids = self.token2id.find(match_tokens)
+            include_ids = self.source_token2id.find(match_tokens)
             data = data[(data.w1_id.isin(include_ids)) | (data.w2_id.isin(include_ids))]
 
         self.data = data
@@ -108,7 +119,7 @@ class CoOccurrenceHelper:
         if isinstance(excludes, (int, str)):
             excludes = [excludes]
 
-        fg = self.token2id.get
+        fg = self.source_token2id.get
 
         exclude_ids = [x if isinstance(x, int) else fg(x) for x in excludes]
 
@@ -124,13 +135,10 @@ class CoOccurrenceHelper:
 
     def rank(self, n_top=10, column='value') -> "CoOccurrenceHelper":
 
-        value_columns: List[str] = ['value', 'n_tokens', 'n_raw_tokens']
-        token_columns: List[str] = ['w1', 'w2', 'token']
+        if column not in VALUE_COLUMNS:
+            raise ValueError(f"largets: expected any of {', '.join(VALUE_COLUMNS)} but found {column}")
 
-        if column not in value_columns:
-            raise ValueError(f"largets: expected any of {', '.join(value_columns)} but found {column}")
-
-        group_columns = [x for x in self.data.columns if x not in value_columns + token_columns]
+        group_columns = [x for x in self.data.columns if x not in VALUE_COLUMNS + TOKEN_COLUMNS]
 
         # self.data['rank'] = self.data.groupby(group_columns)[column].rank(ascending=False) #, method='first')
         # return self.data[self.data['rank'] <= n_top] # .drop(columns='rank')
@@ -140,12 +148,9 @@ class CoOccurrenceHelper:
         return self
 
     def largest(self, n_top=10, column='value') -> "CoOccurrenceHelper":
-
-        if self.pivot_keys is None:
-            return self.data.sort_values(by=column, ascending=True).head(n_top)
-
-        self.data = self.data.loc[self.data.groupby(self.pivot_keys)[column].nlargest(n_top).reset_index().level_1]
-
+        group_columns = list(set(self.data_pivot_keys or []).union(set(self.corpus_pivot_keys or [])))
+        largest_group = self.data.groupby(group_columns)[column].nlargest(n_top)
+        self.data = self.data.loc[largest_group.index.levels[1]]
         return self
 
     def head(self, n_head: int) -> "CoOccurrenceHelper":
@@ -162,4 +167,4 @@ class CoOccurrenceHelper:
 
     @property
     def value(self) -> pd.DataFrame:
-        return self.data
+        return self.decode().data
