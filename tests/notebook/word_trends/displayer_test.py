5cfc27f899965965d93e8c9d39319322b2486a1f
Roger Mähler <roger.mahler@hotmail.com>
2021-05-28 09:33:04 +0200

    Added displayer class tests

--- tests/notebook/word_trends/displayer_test.py
+++ tests/notebook/word_trends/displayer_test.py
@@ -4,9 +4,25 @@ import ipycytoscape
 import numpy as np
 import pandas as pd
 import pytest
+from penelope import co_occurrence
 from penelope.common.curve_fit import pchip_spline, rolling_average_smoother
-from penelope.notebook.word_trends import BarDisplayer, LineDisplayer, TableDisplayer
-from penelope.notebook.word_trends.displayers import CategoryDataMixin, LinesDataMixin, PenelopeBugCheck, create_network
+from penelope.corpus import VectorizedCorpus
+from penelope.notebook.co_occurrence import main_gui
+from penelope.notebook.word_trends import TrendsData
+from penelope.notebook.word_trends.displayers import (
+    BarDisplayer,
+    CategoryDataMixin,
+    ITrendDisplayer,
+    LineDisplayer,
+    LinesDataMixin,
+    NetworkDisplayer,
+    PenelopeBugCheck,
+    TableDisplayer,
+    TopTokensDisplayer,
+    UnnestedExplodeTableDisplayer,
+    UnnestedTableDisplayer,
+    create_network,
+)
 from tests.fixtures import create_smaller_vectorized_corpus
 
 BIGGER_CORPUS_FILENAME = './tests/test_data/riksdagens-protokoll.1950-1959.ak.sparv4.csv.zip'
@@ -74,9 +90,45 @@ def test_compile_year_token_vector_data_when_corpus_is_not_grouped_by_year_fails
         _ = CategoryDataMixin().compile(corpus, indices)
 
 
-def test_():
+@pytest.fixture(scope="module")
+def bundle() -> co_occurrence.Bundle:
+    folder, tag = './tests/test_data/VENUS', 'VENUS'
+    filename = co_occurrence.to_filename(folder=folder, tag=tag)
+    bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(filename, compute_frame=False)
+    return bundle
 
-    data_str = """;category;token;count;w1;w2
+
+@pytest.fixture(scope="module")
+def trends_data(bundle) -> TrendsData:
+    trends_data: TrendsData = main_gui.to_trends_data(bundle).update()
+    return trends_data
+
+
+@pytest.mark.parametrize(
+    'displayer_cls',
+    [
+        BarDisplayer,
+        LineDisplayer,
+        TableDisplayer,
+        TopTokensDisplayer,
+        UnnestedExplodeTableDisplayer,
+        UnnestedTableDisplayer,
+        NetworkDisplayer,
+    ],
+)
+def test_bar_displayer_compile_and_display(displayer_cls, bundle: co_occurrence.Bundle):
+
+    corpus: VectorizedCorpus = bundle.corpus.group_by_year(target_column_name='category')
+
+    displayer: ITrendDisplayer = displayer_cls()
+
+    plot_data: dict = displayer.compile(corpus=corpus, indices=[0, 1, 2])
+
+    assert plot_data is not None
+
+def test_network():
+
+    data_str = """;year;token;count;w1;w2
 0;1920;riksdag/ledamot;18;riksdag;ledamot
 1;1930;riksdag/ledamot;22;riksdag;ledamot
 2;1940;riksdag/ledamot;29;riksdag;ledamot
@@ -89,6 +141,6 @@ def test_():
 
     df = pd.read_csv(io.StringIO(data_str), sep=';', index_col=0)
 
-    w: ipycytoscape.CytoscapeWidget = create_network(df)
+    w: ipycytoscape.CytoscapeWidget = create_network(df, category_column_name='year')
 
     assert w is not None
