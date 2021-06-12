216d2b36ddc4a4d491957f5f26c9b4054a061aae
Roger Mähler <roger.mahler@hotmail.com>
2021-06-12 14:43:13 +0200

    Export mask label

--- penelope/corpus/readers/__init__.py
+++ penelope/corpus/readers/__init__.py
@@ -1,5 +1,11 @@
 # type: ignore
-from .interfaces import ExtractTaggedTokensOpts, ICorpusReader, PhraseSubstitutions, TextSource
+from .interfaces import (
+    GLOBAL_TF_THRESHOLD_MASK_TOKEN,
+    ExtractTaggedTokensOpts,
+    ICorpusReader,
+    PhraseSubstitutions,
+    TextSource,
+)
 from .pandas_reader import PandasCorpusReader
 from .sparv_csv_tokenizer import SparvCsvTokenizer
 from .sparv_xml_tokenizer import Sparv3XmlReader, SparvXmlReader
