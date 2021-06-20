75d37d826e4d05b36645f18b94986dd65a461216
Roger Mähler <roger.mahler@hotmail.com>
2021-06-20 11:54:12 +0200

    Rollbacked

--- penelope/corpus/dtm/slice.py
+++ penelope/corpus/dtm/slice.py
@@ -4,6 +4,7 @@ from heapq import nlargest
 from typing import Sequence, Union
 
 import numpy as np
+import textacy
 
 from .interface import IVectorizedCorpus, IVectorizedCorpusProtocol
 
@@ -90,7 +91,6 @@ class SliceMixIn:
         max_n_terms : in optional
             [description], by default None
         """
-        import textacy
 
         sliced_bag_term_matrix, token2id = textacy.vsm.matrix_utils.filter_terms_by_df(
             self.bag_term_matrix, self.token2id, max_df=max_df, min_df=min_df, max_n_terms=max_n_terms
