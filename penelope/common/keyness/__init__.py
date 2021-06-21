3db882b9e50e6ad2981d045652e0691075a0dfeb
Roger Mähler <roger.mahler@hotmail.com>
2021-06-21 12:50:30 +0200

    Added export

--- penelope/common/keyness/__init__.py
+++ penelope/common/keyness/__init__.py
@@ -1,4 +1,11 @@
 # type: ignore
 
 from .hal_cwr import compute_hal_cwr_score, compute_hal_score_by_co_occurrence_matrix
-from .significance import KeynessMetric, KeynessMetricSource, partitioned_significances, significance, significance_matrix
+from .significance import (
+    KeynessMetric,
+    KeynessMetricSource,
+    partitioned_significances,
+    significance,
+    significance_matrix,
+    significance_ratio,
+)
