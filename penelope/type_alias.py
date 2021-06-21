31277dec152ddcbffb68542bb74de3e511cb363f
Roger Mähler <roger.mahler@hotmail.com>
2021-06-21 11:03:01 +0200

    Added type alias

--- penelope/type_alias.py
+++ penelope/type_alias.py
@@ -1,16 +1,19 @@
-from typing import Callable, Iterable, Iterator, List, Tuple, Union
+from typing import Callable, Iterable, Iterator, List, Mapping, Optional, Tuple, Union
 
 import pandas as pd
 
 PartitionKeys = Union[str, List[str], Callable]
 FilenameTokensTuple = Tuple[str, Iterable[str]]
 FilenameTokensTuples = Iterable[FilenameTokensTuple]
-CoOccurrenceDataFrame = pd.DataFrame
+
 DocumentIndex = pd.DataFrame
 IntOrStr = Union[int, str]
 
 TaggedFrame = pd.core.api.DataFrame
 Token = Union[int, str]
 
-WindowsStream = Iterator[Tuple[str, int, Iterable[Token]]]
 DocumentIndex = pd.DataFrame
+
+WindowsStream = Iterator[Tuple[str, int, Iterable[Token]]]
+CoOccurrenceDataFrame = pd.DataFrame
+VocabularyMapping = Optional[Mapping[Tuple[int, int], int]]
