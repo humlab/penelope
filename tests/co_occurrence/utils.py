4f0b98e629893bbe257705f838d360c4c7b1bb1c
Roger Mähler <roger.mahler@hotmail.com>
2021-06-27 23:49:04 +0200

    Compress arg

--- tests/co_occurrence/utils.py
+++ tests/co_occurrence/utils.py
@@ -58,6 +58,7 @@ def create_simple_bundle_by_pipeline(
     context_opts: ContextOpts,
     tag: str = "TERRA",
     folder: str = None,
+    compress: bool = False
 ):
     folder = folder or OUTPUT_FOLDER
     if folder.startswith('./tests') and folder != OUTPUT_FOLDER:
@@ -73,7 +74,7 @@ def create_simple_bundle_by_pipeline(
         .load_corpus(data)
         .vocabulary(lemmatize=False)
         .to_document_co_occurrence(context_opts=context_opts, ingest_tokens=False)
-        .to_corpus_co_occurrence(context_opts=context_opts, global_threshold_count=1)
+        .to_corpus_co_occurrence(context_opts=context_opts, global_threshold_count=1, compress=compress)
         .single()
         .content
     )
