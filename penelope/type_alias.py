bf859c7fe4af464b448820e90a6c670fa0347c75
Roger Mähler <roger.mahler@hotmail.com>
2021-06-30 14:00:20 +0200

    Fixed alias

--- penelope/type_alias.py
+++ penelope/type_alias.py
@@ -1,7 +1,7 @@
 from typing import Callable, Iterable, Iterator, List, Mapping, Optional, Tuple, Union
 
 import pandas as pd
-import scipy
+import scipy.sparse as sp
 
 PartitionKeys = Union[str, List[str], Callable]
 FilenameTokensTuple = Tuple[str, Iterable[str]]
@@ -13,9 +13,7 @@ IntOrStr = Union[int, str]
 TaggedFrame = pd.core.api.DataFrame
 Token = Union[int, str]
 
-DocumentIndex = pd.DataFrame
-
 WindowsStream = Iterator[Tuple[str, int, Iterable[Token]]]
 CoOccurrenceDataFrame = pd.DataFrame
 VocabularyMapping = Optional[Mapping[Tuple[int, int], int]]
-SparseMatrix = scipy.sparse.spmatrix
+SparseMatrix = sp.spmatrix
