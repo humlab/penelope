a02651978a9921b252c920c77574e1a391925c26
Roger Mähler <roger.mahler@hotmail.com>
2021-07-03 21:20:25 +0200

    Test data updates

--- doit.sh
+++ doit.sh
@@ -5,9 +5,13 @@ set -e
 CORPUS_FOLDER=/data/westac/data
 TARGET_FOLDER=/data/westac/shared
 
-#CORPUS_FILENAME=riksdagens-protokoll.1920-2019.sparv4.csv.zip
-CORPUS_FILENAME=riksdagens-protokoll.1920-2019.test.sparv4.csv.zip
-CONFIG_FILENAME='./tests/test_data/riksdagens-protokoll.yml'
+CORPUS_FILENAME=riksdagens-protokoll.1920-2019.sparv4.csv.zip
+CONFIG_FILENAME='./doit.yml'
+#CORPUS_FILENAME=riksdagens-protokoll.1920-2019.test.sparv4.csv.zip
+#CONFIG_FILENAME='./tests/test_data/riksdagens-protokoll.yml'
+
+RUN_OPTS="--tf-threshold 10 --tf-threshold-mask  --lemmatize --to-lowercase --partition-key year --enable-checkpoint"
+# --force-checkpoint
 
 run()
 {
@@ -19,8 +23,7 @@ run()
     basename="NEW_${concept_word}_w${context_width}_${pos_includes//|}_${pos_paddings//|}_LEMMA_KEEPSTOPS"
     mkdir -p  ${TARGET_FOLDER}/$basename
 
-    command="poetry run co_occurrence --tf-threshold 20  --tf-threshold-mask --pos-includes "$pos_includes" --pos-paddings "$pos_paddings" --context-width $context_width \
-        --lemmatize --to-lowercase --partition-key year --concept $concept_word \
+    command="poetry run co_occurrence $RUN_OPTS --pos-includes "$pos_includes" --pos-paddings "$pos_paddings" --context-width $context_width --concept $concept_word \
          ${CONFIG_FILENAME} \
          ${CORPUS_FOLDER}/${CORPUS_FILENAME} \
          ${TARGET_FOLDER}/${basename}/${basename}"
