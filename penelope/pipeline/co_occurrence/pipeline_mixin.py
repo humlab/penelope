9be6e9eeb503ed114f6ce56cf608685cfb064f4c
Roger Mähler <roger.mahler@hotmail.com>
2021-07-03 21:17:30 +0200

    Arg removed

--- penelope/pipeline/co_occurrence/pipeline_mixin.py
+++ penelope/pipeline/co_occurrence/pipeline_mixin.py
@@ -14,18 +14,10 @@ class PipelineShortcutMixIn:
     """Shortcuts for specific tasks that can be injected to derived pipelines"""
 
     def to_document_co_occurrence(
-        self: pipelines.CorpusPipeline,
-        *,
-        context_opts: ContextOpts = None,
-        ingest_tokens: bool = True,
+        self: pipelines.CorpusPipeline, *, context_opts: ContextOpts = None
     ) -> pipelines.CorpusPipeline:
         """ TOKENS => CO_OCCURRENCE_DTM_DOCUMENT """
-        return self.add(
-            tasks.ToCoOccurrenceDTM(
-                context_opts=context_opts,
-                ingest_tokens=ingest_tokens,
-            )
-        )
+        return self.add(tasks.ToCoOccurrenceDTM(context_opts=context_opts))
 
     def to_corpus_co_occurrence(
         self: pipelines.CorpusPipeline,
