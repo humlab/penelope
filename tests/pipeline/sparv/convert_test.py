175c6760caf1e4836058e7b3b0f839753fc438dc
Roger Mähler <roger.mahler@hotmail.com>
2021-07-05 23:49:18 +0200

    Added test

--- tests/pipeline/sparv/convert_test.py
+++ tests/pipeline/sparv/convert_test.py
@@ -2,7 +2,8 @@ from typing import Type
 
 import pandas as pd
 from penelope import utility
-from penelope.pipeline import checkpoint, sparv
+from penelope.pipeline import CheckpointOpts, CsvContentSerializer, checkpoint, sparv
+from penelope.pipeline.sparv import deserialize_lemma_form
 
 
 def test_sparv_csv_serializer():
@@ -27,3 +28,42 @@ def test_sparv_csv_serializer():
     assert all(~tagged_frame.token.isna())
     assert all(~tagged_frame.baseform.isna())
     assert all(~tagged_frame.pos.isna())
+
+
+def test_sparv_csv_deserialize_lemma_form():
+
+    tagged_frame_str: str = """token	pos	baseform
+# text
+RIKSDAGEN	NN	|riksdag|
+1975	RG	|
+/	PAD	|
+76	RG	|
+Protokoll	NN	|protokoll|
+Nr	NN	|nr|
+1	RG	|
+—	MID	|
+15	RG	|
+15	RG	|
+oktober	NN	|oktober|
+—	MID	|
+4	RG	|
+november	NN	|november|
+Band	PM	|
+A	IN	|
+1	RG	|"""
+
+    serializer: CsvContentSerializer = CsvContentSerializer()
+    checkpoint_opts: CheckpointOpts = CheckpointOpts(
+        text_column='token',
+        lemma_column='baseform',
+        pos_column='pos',
+        index_column=None,
+        sep='\t',
+    )
+    tagged_frame: pd.DataFrame = serializer.deserialize(tagged_frame_str, checkpoint_opts)
+
+    assert tagged_frame is not None
+
+    lemma: pd.Series = deserialize_lemma_form(tagged_frame)
+
+    assert lemma is not None
