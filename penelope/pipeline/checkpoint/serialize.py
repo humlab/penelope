4eef7a6bf0a4761ad8e1e3f2d0a129820c7179be
Roger Mähler <roger.mahler@hotmail.com>
2021-07-06 07:37:16 +0200

    Performance improvement - pre-compute term frequency - deleyed open of zip-archive

--- penelope/pipeline/checkpoint/serialize.py
+++ penelope/pipeline/checkpoint/serialize.py
@@ -1,11 +1,14 @@
 import os
 import zipfile
+from collections import defaultdict
 from io import StringIO
 from multiprocessing import get_context
-from typing import Callable, Iterable, List, Sequence, Tuple, Union
+from typing import Callable, Iterable, List, Mapping, Sequence, Tuple, Union
 
 import pandas as pd
 from loguru import logger
+from penelope.utility.zip_utils import zipfile_or_filename
+from tqdm import tqdm
 
 from ..interfaces import ContentType, DocumentPayload
 from ..tagged_frame import TaggedFrame
@@ -62,22 +65,36 @@ def create_serializer(options: CheckpointOpts) -> "IContentSerializer":
 Serializer = Callable[[str, CheckpointOpts], pd.DataFrame]
 
 
+@zipfile_or_filename(mode='r')
 def read_and_deserialize_tagged_frame(
-    source: Union[str, zipfile.ZipFile], filename: str, checkpoint_opts: CheckpointOpts, serializer: Serializer
+    *,
+    zip_or_filename: Union[str, zipfile.ZipFile],
+    filename: str,
+    checkpoint_opts: CheckpointOpts,
+    serializer: Serializer,
 ) -> TaggedFrame:
-    content: str = source.read(filename).decode(encoding='utf-8') if isinstance(source, zipfile.ZipFile) else source
+    content: str = zip_or_filename.read(filename).decode(encoding='utf-8')
     tagged_frame: TaggedFrame = serializer.deserialize(content, checkpoint_opts)
     return tagged_frame
 
 
 def read_and_deserialize_tagged_frame_with_feather_cache(
-    zf: zipfile.ZipFile, filename: str, checkpoint_opts: CheckpointOpts, serializer: Serializer
+    *,
+    zip_or_filename: Union[str, zipfile.ZipFile],
+    filename: str,
+    checkpoint_opts: CheckpointOpts,
+    serializer: Serializer,
 ) -> TaggedFrame:
     feather_filename: str = checkpoint_opts.feather_filename(filename)
     if os.path.isfile(feather_filename):
         tagged_frame: pd.DataFrame = pd.read_feather(feather_filename)
     else:
-        tagged_frame = read_and_deserialize_tagged_frame(zf, filename, checkpoint_opts, serializer)
+        tagged_frame = read_and_deserialize_tagged_frame(
+            zip_or_filename=zip_or_filename,
+            filename=filename,
+            checkpoint_opts=checkpoint_opts,
+            serializer=serializer,
+        )
         tagged_frame.to_feather(feather_filename, compression="lz4")
     return tagged_frame
 
@@ -86,7 +103,10 @@ def read_and_deserialize_tagged_frame_with_feather_cache(
 
 
 def sequential_deserialized_payload_stream(
-    source_name: str, checkpoint_opts: CheckpointOpts, filenames: List[str]
+    *,
+    zip_or_filename: Union[str, zipfile.ZipFile],
+    checkpoint_opts: CheckpointOpts,
+    filenames: List[str],
 ) -> Iterable[DocumentPayload]:
     """Yields a deserialized payload stream read from given source"""
 
@@ -99,51 +119,101 @@ def sequential_deserialized_payload_stream(
     else:
         reader = read_and_deserialize_tagged_frame
 
-    with zipfile.ZipFile(source_name, mode="r") as zf:
-        for filename in filenames:
-            tagged_frame = reader(zf, filename, checkpoint_opts, serializer)
-            yield DocumentPayload(
-                content_type=checkpoint_opts.content_type,
-                content=tagged_frame,
-                filename=filename,
-            )
+    for filename in filenames:
+        tagged_frame = reader(
+            zip_or_filename=zip_or_filename,
+            filename=filename,
+            checkpoint_opts=checkpoint_opts,
+            serializer=serializer,
+        )
+        yield DocumentPayload(
+            content_type=checkpoint_opts.content_type,
+            content=tagged_frame,
+            filename=filename,
+        )
+
+
+def _count_column_frequencies(series: pd.DataFrame) -> Mapping[str, int]:
+    counts = defaultdict(int)
+    for v in series:
+        counts[v] += 1
+    return counts
 
 
 def open_read_and_deserialize_tagged_frame(args: Tuple) -> DocumentPayload:
-    filename, source, serializer, checkpoint_opts = args
-    with zipfile.ZipFile(source, mode="r") as zf:
-        tagged_frame = read_and_deserialize_tagged_frame(zf, filename, checkpoint_opts, serializer)
-    return DocumentPayload(content_type=checkpoint_opts.content_type, content=tagged_frame, filename=filename)
+    try:
+        filename, source, serializer, checkpoint_opts = args
+        tagged_frame = read_and_deserialize_tagged_frame(
+            zip_or_filename=source,
+            filename=filename,
+            checkpoint_opts=checkpoint_opts,
+            serializer=serializer,
+        )
+        payload = DocumentPayload(content_type=checkpoint_opts.content_type, content=tagged_frame, filename=filename)
+
+        if checkpoint_opts.frequency_column:
+            payload.remember(term_frequency=_count_column_frequencies(tagged_frame[checkpoint_opts.frequency_column]))
+            payload.remember(pos_frequency=_count_column_frequencies(tagged_frame[checkpoint_opts.pos_column]))
+
+        return payload
+    except Exception as ex:
+        logger.exception(ex)
+        raise ex
+        # return ex
 
 
 def open_read_and_deserialize_tagged_frame_with_feather_cache(args: Tuple) -> DocumentPayload:
     filename, source, serializer, checkpoint_opts = args
-    with zipfile.ZipFile(source, mode="r") as zf:
+    try:
         tagged_frame: pd.DataFrame = read_and_deserialize_tagged_frame_with_feather_cache(
-            zf, filename, checkpoint_opts, serializer
+            zip_or_filename=source,
+            filename=filename,
+            checkpoint_opts=checkpoint_opts,
+            serializer=serializer,
         )
-    return DocumentPayload(content_type=checkpoint_opts.content_type, content=tagged_frame, filename=filename)
+
+        payload = DocumentPayload(content_type=checkpoint_opts.content_type, content=tagged_frame, filename=filename)
+
+        if checkpoint_opts.frequency_column:
+            payload.remember(term_frequency=_count_column_frequencies(tagged_frame[checkpoint_opts.frequency_column]))
+            payload.remember(pos_frequency=_count_column_frequencies(tagged_frame[checkpoint_opts.pos_column]))
+
+        return payload
+    except Exception as ex:
+        logger.error(f"Filename: {filename}")
+        logger.exception(ex)
+        raise ex
+        # return ex
 
 
 def parallel_deserialized_payload_stream(
-    source_name: str,
+    *,
+    zip_or_filename: str,
     checkpoint_opts: CheckpointOpts,
     filenames: List[str],
 ) -> Iterable[DocumentPayload]:
 
-    logger.trace(f"Using parallel deserialization with {checkpoint_opts.deserialize_processes} processes.")
-    _process_document = open_read_and_deserialize_tagged_frame
-    if checkpoint_opts.feather_folder:
-        logger.trace(f"Using feather checkpoint folder {checkpoint_opts.feather_folder}.")
-        _process_document = open_read_and_deserialize_tagged_frame_with_feather_cache
+    try:
+        logger.trace(f"Using parallel deserialization with {checkpoint_opts.deserialize_processes} processes.")
+        _process_document = open_read_and_deserialize_tagged_frame
+        if checkpoint_opts.feather_folder:
+            logger.trace(f"Using feather checkpoint folder {checkpoint_opts.feather_folder}.")
+            _process_document = open_read_and_deserialize_tagged_frame_with_feather_cache
 
-    serializer: IContentSerializer = create_serializer(checkpoint_opts)
+        serializer: IContentSerializer = create_serializer(checkpoint_opts)
 
-    with get_context("spawn").Pool(processes=checkpoint_opts.deserialize_processes) as executor:
-        args: str = [(filename, source_name, serializer, checkpoint_opts) for filename in filenames]
-        payloads_futures: Iterable[DocumentPayload] = executor.imap(
-            _process_document, args, chunksize=checkpoint_opts.deserialize_chunksize
-        )
+        with get_context("spawn").Pool(processes=checkpoint_opts.deserialize_processes) as executor:
+            args: Iterable[Tuple] = tqdm(
+                [(filename, zip_or_filename, serializer, checkpoint_opts) for filename in filenames]
+            )
+            payloads_futures: Iterable[DocumentPayload] = executor.imap_unordered(
+                _process_document, args, chunksize=checkpoint_opts.deserialize_chunksize
+            )
 
-        for payload in payloads_futures:
-            yield payload
+            for payload in payloads_futures:
+                yield payload
+
+    except Exception as ex:
+        logger.exception(ex)
+        raise ex
+        # return ex
