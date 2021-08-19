bf5cb1eb16ca46bc3de0748c02b94a8645ac1b41
Roger Mähler <roger.mahler@hotmail.com>
2021-08-19 15:26:37 +0200

    Added explicit delete

--- penelope/pipeline/pipeline.py
+++ penelope/pipeline/pipeline.py
@@ -101,7 +101,8 @@ class CorpusPipelineBase(Generic[_T_self]):
 
     def exhaust(self, n_count=0) -> CorpusPipelineBase:
         """Exhaust the entire pipeline, disregarding items"""
-        for i, _ in enumerate(self.resolve()):
+        for i, p in enumerate(self.resolve()):
+            del p
             if 0 < n_count <= i:
                 break
         # collections.deque(self.resolve(), maxlen=n_count)
