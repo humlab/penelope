a3e8b26e07b2e69d7109bf1ac7ddd0dba0b96459
Roger Mähler <roger.mahler@hotmail.com>
2021-08-23 08:24:36 +0200

    Profiling update

--- tests/profiling/vocabulary.py
+++ tests/profiling/vocabulary.py
@@ -7,8 +7,8 @@ from penelope.pipeline.pipelines import CorpusPipeline
 DATA_FOLDER = "/data/westac/data"
 CONFIG_FILENAME = "/home/roger/source/penelope/doit.yml"
 OUTPUT_FOLDER = './tests/output'
-# CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1920-2019.sparv4.csv.zip")
-CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1970.sparv4.csv.zip")
+CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1920-2019.sparv4.csv.zip")
+# CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1970.sparv4.csv.zip")
 # CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1920-2019.test.sparv4.csv.zip")
 # CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1920-2019.9files.sparv4.csv.zip")
 
@@ -16,7 +16,7 @@ CORPUS_FILENAME = jj(DATA_FOLDER, "riksdagens-protokoll.1970.sparv4.csv.zip")
 def run_workflow():
     corpus_config = pipeline.CorpusConfig.load(CONFIG_FILENAME).folders(DATA_FOLDER)
     corpus_config.pipeline_payload.files(source=CORPUS_FILENAME, document_index_source=None)
-    corpus_config.checkpoint_opts.deserialize_processes = 4
+    corpus_config.checkpoint_opts.deserialize_processes = 3
 
     # transform_opts: corpora.TokensTransformOpts = corpora.TokensTransformOpts(
     #     to_lower=True,
@@ -33,15 +33,15 @@ def run_workflow():
     #     only_alphabetic=False,
     #     only_any_alphanumeric=False,
     # )
-    extract_opts: corpora.ExtractTaggedTokensOpts = corpora.ExtractTaggedTokensOpts(
-        pos_includes='NN|PM',
-        pos_excludes='MAD|MID|PAD',
-        pos_paddings='AB|DT|HA|HD|HP|HS|IE|IN|JJ|KN|PC|PL|PN|PP|PS|RG|RO|SN|UO|VB',
-        lemmatize=True,
-        append_pos=False,
-        global_tf_threshold=1,
-        global_tf_threshold_mask=False,
-    )
+    # extract_opts: corpora.ExtractTaggedTokensOpts = corpora.ExtractTaggedTokensOpts(
+    #     pos_includes='NN|PM',
+    #     pos_excludes='MAD|MID|PAD',
+    #     pos_paddings='AB|DT|HA|HD|HP|HS|IE|IN|JJ|KN|PC|PL|PN|PP|PS|RG|RO|SN|UO|VB',
+    #     lemmatize=True,
+    #     append_pos=False,
+    #     global_tf_threshold=1,
+    #     global_tf_threshold_mask=False,
+    # )
     # filter_opts: utility.PropertyValueMaskingOpts=utility.PropertyValueMaskingOpts()
 
     (
@@ -51,13 +51,13 @@ def run_workflow():
             checkpoint_opts=corpus_config.checkpoint_opts,
             extra_reader_opts=corpus_config.text_reader_opts,
         )
-        .vocabulary(
-            lemmatize=extract_opts.lemmatize,
-            progress=True,
-            tf_threshold=extract_opts.global_tf_threshold,
-            tf_keeps=set(),
-            close=True,
-        )
+        # .vocabulary(
+        #     lemmatize=extract_opts.lemmatize,
+        #     progress=True,
+        #     tf_threshold=extract_opts.global_tf_threshold,
+        #     tf_keeps=set(),
+        #     close=True,
+        # )
         .exhaust()
         # .tagged_frame_to_tokens(
         #     extract_opts=extract_opts,  # .clear_tf_threshold(),
