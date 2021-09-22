4d878de5446603a19ac799555333a0ed89fe9eca
Roger Mähler <roger.mahler@hotmail.com>
2021-09-22 12:42:43 +0200

    Renamed main function

--- penelope/scripts/topic_model.py
+++ penelope/scripts/topic_model.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from os.path import join as jj
-from tkinter import mainloop
 
 import click
 import penelope.corpus.readers.text_tokenizer as text_tokenizer
@@ -31,7 +30,7 @@ from penelope.corpus.readers.interfaces import TextReaderOpts
 @click.option('--filename-field', '-f', default=None, help='Field to extract from document name', multiple=True)
 @click.option('--store-corpus/--no-store-corpus', default=True, is_flag=True, help='')
 @click.option('--compressed/--no-compressed', default=True, is_flag=True, help='')
-def main(
+def click_main(
     name,
     n_topics,
     corpus_folder,
@@ -62,7 +61,7 @@ def main(
         if v is not None
     }
 
-    mainloop(
+    main(
         name=name,
         corpus_folder=corpus_folder,
         corpus_filename=corpus_filename,
@@ -74,7 +73,7 @@ def main(
     )
 
 
-def mainloop(
+def main(
     name: str = None,
     corpus_folder: str = None,
     corpus_filename: str = None,
