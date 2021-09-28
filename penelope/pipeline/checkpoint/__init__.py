44bb22c3e35e71a29a0c2c6e537b1d184c614dd7
Roger Mähler <roger.mahler@hotmail.com>
2021-09-28 16:30:16 +0200

    Add export

--- penelope/pipeline/checkpoint/__init__.py
+++ penelope/pipeline/checkpoint/__init__.py
@@ -3,6 +3,6 @@
 from . import feather
 from .checkpoint import CheckpointData, load_archive, store_archive
 from .interface import CheckpointOpts, IContentSerializer
-from .load import load_payloads_multiprocess, load_payloads_singleprocess
+from .load import load_payload, load_payloads_multiprocess, load_payloads_singleprocess
 from .serialize import CsvContentSerializer, TextContentSerializer, TokensContentSerializer, create_serializer
 from .utility import CorruptCheckpointError, EmptyCheckpointError, find_checkpoints, read_document_index
