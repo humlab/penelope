2c61f47f8a17bd51555f1e4508b823a4d07ec713
Roger Mähler <roger.mahler@hotmail.com>
2021-10-01 09:15:21 +0200

    Updated config test

--- tests/pipeline/config_test.py
+++ tests/pipeline/config_test.py
@@ -1,81 +1,56 @@
 import json
 import os
 
+import pytest
 import yaml
 from penelope.pipeline import CorpusConfig
 from penelope.pipeline.interfaces import PipelinePayload
 
-TEST_CONFIG = CorpusConfig.loads(
-    """
-corpus_name: ssi_unesco
-corpus_pattern: '*.zip'
-corpus_type: 1
-language: english
-pipelines:
-  tagged_frame_pipeline: penelope.pipeline.spacy.pipelines.to_tagged_frame_pipeline
-pipeline_payload:
-  document_index_sep: ;
-  document_index_source: legal_instrument_index.csv
-  filenames: null
-  memory_store:
-    lang: en
-    lemma_column: lemma_
-    pos_column: pos_
-    spacy_model: en_core_web_sm
-    tagger: spaCy
-    text_column: text
-  pos_schema_name: Universal
-  source: legal_instrument_corpus.zip
-filter_opts:
-  data:
-    is_alpha: null
-    is_digit: null
-    is_punct: false
-    is_stop: null
-text_reader_opts:
-  as_binary: false
-  filename_fields:
-  - unesco_id:_:2
-  - year:_:3
-  - city:\\w+\\_\\d+\\_\\d+\\_\\d+\\_(.*)\\.txt
-  filename_filter: null
-  filename_pattern: '*.txt'
-  index_field: null
-"""
-)
+
+@pytest.fixture
+def corpus_config() -> CorpusConfig:
+    return CorpusConfig.load('./tests/test_data/SSI.yml')
 
 
-def test_json_dumps_and_loads_of_corpus_config_succeeds():
+def test_json_dumps_and_loads_of_corpus_config_succeeds(corpus_config: CorpusConfig):
 
-    first_dump_str = json.dumps(TEST_CONFIG, default=vars)
+    first_dump_str = json.dumps(corpus_config, default=vars)
     deserialized_config = json.loads(first_dump_str)
     second_dump_str = json.dumps(deserialized_config, default=vars)
 
     assert first_dump_str == second_dump_str
 
 
-def test_yaml_dumps_and_loads_of_corpus_config_succeeds():
+def test_yaml_dumps_and_loads_of_corpus_config_succeeds(corpus_config: CorpusConfig):
 
-    json_dump_str = json.dumps(TEST_CONFIG, default=vars)
+    json_dump_str = json.dumps(corpus_config, default=vars)
     yaml_dump_str = yaml.dump(json_dump_str)
 
     _ = yaml.load(yaml_dump_str, Loader=yaml.FullLoader)
 
 
-def test_dump_and_load_of_corpus_config_succeeds():
+def test_dump_and_load_of_corpus_config_succeeds(corpus_config: CorpusConfig):
 
     os.makedirs('./tests/output', exist_ok=True)
     dump_filename = './tests/output/corpus_config_test.yml'
-    TEST_CONFIG.dump(dump_filename)
+    corpus_config.dump(dump_filename)
     deserialized_config = CorpusConfig.load(dump_filename)
 
-    assert json.dumps(TEST_CONFIG, default=vars) == json.dumps(deserialized_config, default=vars)
+    assert json.dumps(corpus_config, default=vars) == json.dumps(deserialized_config, default=vars)
 
 
-def test_find_config():
+def test_find_config(corpus_config: CorpusConfig):
 
     c = CorpusConfig.find("SSI.yml", './tests/test_data')
-    assert c is not None
+
+    assert json.dumps(c, default=vars) == json.dumps(corpus_config, default=vars)
+
+
+def test_text_transform_opts(corpus_config: CorpusConfig):
+
+    assert corpus_config.text_transform_opts is not None
+    assert corpus_config.text_transform_opts.fix_hyphenation
+    assert corpus_config.text_transform_opts.fix_whitespaces
 
 
 def test_corpus_config_set_folders():
