c8120ba9a5ee875c4f8bfa3488edd2a7354171d7
Roger Mähler <roger.mahler@hotmail.com>
2021-10-10 21:08:33 +0200

    Moved persistence into enttities Allow non-pickled store

--- penelope/topic_modelling/interfaces.py
+++ penelope/topic_modelling/interfaces.py
@@ -1,20 +1,28 @@
 from __future__ import annotations
 
 import abc
+import json
 import os
 import pickle
 import sys
 import types
+from dataclasses import dataclass
+from enum import Enum
 from os.path import join as jj
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Sequence, Tuple, Type
 
-import gensim
 import numpy as np
 import pandas as pd
-import scipy
+from gensim.matutils import Sparse2Corpus
+from gensim.models.tfidfmodel import TfidfModel
 from penelope import utility
-from penelope.corpus import DocumentIndex, DocumentIndexHelper, Token2Id, load_document_index
+from penelope.corpus import DocumentIndex, DocumentIndexHelper, Token2Id, VectorizedCorpus, load_document_index
+from penelope.vendor.gensim import terms_to_sparse_corpus
+from scipy.sparse import coo_matrix, csr_matrix, issparse
 from tqdm.auto import tqdm
+
+CORPUS_OPTIONS_FILENAME: str = "train_corpus_options.json"
+VECTORIZER_ARGS_FILENAME: str = "train_vectorizer_args.json"
 
 DEFAULT_VECTORIZE_PARAMS = dict(tf_type='linear', apply_idf=False, idf_type='smooth', norm='l2', min_df=1, max_df=0.95)
 
@@ -104,49 +112,129 @@ class ITopicModelEngine(abc.ABC):
         return df.set_index('topic_id')
 
 
+@dataclass
 class TrainingCorpus:
-    """A container for the corpus data used during learning/inference"""
+    """A container for the corpus data used during learning/inference
+    The corpus can be wither represented as a sequence of list of tokens or a docuement-term-matrix.
+    The corpus actually used in training is stored in `corpus` by the modelling engine
 
-    def __init__(
-        self,
-        terms: Iterable[Iterable[str]] = None,
-        document_index: DocumentIndex = None,
-        doc_term_matrix: scipy.sparse.csr_matrix = None,
-        id2word: Optional[Mapping[int, str]] = None,
-        vectorizer_args: Mapping[str, Any] = None,
-        corpus: gensim.matutils.Sparse2Corpus = None,
-        corpus_options: dict = None,
-    ):
-        """A container for the corpus data used during learning/inference
+    Parameters
+    ----------
+    terms : Iterable[Iterable[str]], optional
+        Document tokens stream, by default None
+    document_index : DocumentIndex, optional
+        Documents metadata, by default None
+    doc_term_matrix : scipy.sparse.csr_sparse, optional
+        DTM BoW, by default None
+    id2token : Dict[int, str], optional
+        ID to word mapping, by default None
+    vectorizer_args: Dict[str, Any]
+        Options to use when vectorizing `terms`, ony used if DTM is None,
+    """
 
-        The corpus can be wither represented as a sequence of list of tokens or a docuement-term-matrix.
+    terms: Iterable[Iterable[str]] = None
+    doc_term_matrix: csr_matrix = None
+    document_index: DocumentIndex = None
+    id2token: Optional[Mapping[int, str]] = None
 
-        The corpus actually used in training is stored in `corpus` by the modelling engine
+    vectorizer_args: Mapping[str, Any] = None
+    corpus: Sparse2Corpus = None
+    corpus_options: dict = None
 
-        Parameters
-        ----------
-        terms : Iterable[Iterable[str]], optional
-            Document tokens stream, by default None
-        document_index : DocumentIndex, optional
-            Documents metadata, by default None
-        doc_term_matrix : scipy.sparse.csr_sparse, optional
-            DTM BoW, by default None
-        id2word : Union[gensim.corpora.Dictionary, Dict[int, str]], optional
-            ID to word mapping, by default None
-        vectorizer_args: Dict[str, Any]
-            Options to use when vectorizing `terms`, ony used if DTM is None,
-        """
-        self.terms = terms
-        self.doc_term_matrix = doc_term_matrix
-        self.id2word = id2word
-        self.documents = document_index
-        self.vectorizer_args = {**DEFAULT_VECTORIZE_PARAMS, **(vectorizer_args or {})}
-        self.corpus = corpus
-        self.corpus_options = corpus_options
+    def __post_init__(self):
+        self.vectorizer_args = {**DEFAULT_VECTORIZE_PARAMS, **(self.vectorizer_args or {})}
 
     @property
-    def document_index(self):
-        return self.documents
+    def source(self) -> Any:
+        return self.doc_term_matrix or self.terms
+
+    def store(self, folder: str, store_compressed: bool = True, pickled: bool = True):
+        """Stores the corpus used in training. If not pickled, then stored as separate files"""
+
+        if pickled:
+
+            filename = jj(folder, f"training_corpus.pickle{'.pbz2' if store_compressed else ''}")
+
+            utility.pickle_to_file(
+                filename,
+                TrainingCorpus(
+                    doc_term_matrix=None,
+                    terms=None,
+                    corpus=self.corpus,
+                    document_index=self.document_index,
+                    id2token=self.id2token,
+                    vectorizer_args=self.vectorizer_args,
+                    corpus_options=self.corpus_options,
+                ),
+            )
+
+            utility.write_json(jj(folder, CORPUS_OPTIONS_FILENAME), data=self.corpus_options or {})
+
+        else:
+
+            utility.write_json(jj(folder, VECTORIZER_ARGS_FILENAME), data=self.vectorizer_args or {})
+            utility.write_json(jj(folder, CORPUS_OPTIONS_FILENAME), data=self.corpus_options or {})
+
+            corpus: VectorizedCorpus = VectorizedCorpus(
+                self.corpus.sparse,
+                token2id=self.id2token,
+                document_index=self.document_index,
+            )
+
+            corpus.dump(tag='train', folder=folder)
+
+    @staticmethod
+    def load(folder: str) -> TrainingCorpus:
+        """Loads an training corpus from pickled file."""
+        for filename in ["training_corpus.pickle.pbz2", "training_corpus.pickle"]:
+            if os.path.isfile(jj(folder, filename)):
+                return utility.unpickle_from_file(jj(folder, filename))
+
+        """Load from vectorized corpus if exists"""
+        if VectorizedCorpus.dump_exists(tag='train', folder=folder):
+
+            corpus: VectorizedCorpus = VectorizedCorpus.load(tag='train', folder=folder)
+            return TrainingCorpus(
+                corpus=None,  # Sparse2Corpus(corpus.data),
+                doc_term_matrix=corpus.data,
+                document_index=corpus.document_index,
+                id2token=corpus.id2token,
+                corpus_options=utility.read_json(jj(folder, CORPUS_OPTIONS_FILENAME), default={}),
+                vectorizer_args=utility.read_json(jj(folder, VECTORIZER_ARGS_FILENAME), default={}),
+            )
+        return None
+
+    def to_sparse_corpus(self) -> TrainingCorpus:
+        """Create a Gensim Sparse2Corpus from `source`. Store in `corpus`."""
+        if isinstance(self.source, Sparse2Corpus):
+
+            return self
+
+        if isinstance(self.source, VectorizedCorpus):
+
+            self.corpus = Sparse2Corpus(self.source, documents_columns=False)
+            self.id2token = self.source.id2token
+
+        elif issparse(self.source):
+
+            if self.id2token is None:
+                raise ValueError("expected valid token2id, found None")
+
+            self.corpus = Sparse2Corpus(self.source, documents_columns=False)
+
+        else:
+
+            self.corpus, self.id2token = terms_to_sparse_corpus(self.source)
+
+        return self
+
+    def to_tf_idf(self) -> TrainingCorpus:
+        # assert algorithm_name != 'MALLETLDA', 'MALLET training model cannot (currently) use TFIDF weighed corpus'
+        if self.corpus is None:
+            raise ValueError("no corpus")
+        tfidf_model = TfidfModel(self.corpus)
+        self.corpus = [tfidf_model[d] for d in self.corpus]
+        return self
 
 
 class InferredModel:
@@ -180,6 +268,50 @@ class InferredModel:
             tbar.close()
         return self._train_corpus
 
+    class StoreCorpusOptions(Enum):
+        NONE = 1
+        PICKLED = 2
+
+    def store_topic_model(self, folder: str, store_compressed: bool = True):
+        """Stores topic model in pickled format """
+        filename: str = jj(folder, f"topic_model.pickle{'.pbz2' if store_compressed else ''}")
+        os.makedirs(folder, exist_ok=True)
+        utility.pickle_to_file(filename, self.topic_model)
+
+    def store_model_options(self, folder: str):
+        filename: str = jj(folder, "model_options.json")
+        options: dict = {**dict(method=self.method), **self.options}
+        os.makedirs(folder, exist_ok=True)
+        with open(filename, 'w') as fp:
+            json.dump(options, fp, indent=4, default=lambda o: f"<<non-serializable: {type(o).__qualname__}>>")
+
+    def store(self, folder: str, store_corpus=True, store_compressed=True):
+        """Stores the inferred model on disk in folder `folder`"""
+        self.store_topic_model(folder, store_compressed=store_compressed)
+        if store_corpus:
+            self.train_corpus.store(folder, store_compressed=store_compressed)
+        self.store_model_options(folder)
+
+    @staticmethod
+    def load_topic_model(folder: str) -> Any:
+        """Loads an train corpus from av previously pickled file."""
+        return utility.unpickle_from_file(jj(folder, "topic_model.pickle.pbz2"))
+
+    @staticmethod
+    def load_model_options(folder: str) -> Dict[str, Any]:
+        filename = jj(folder, "model_options.json")
+        with open(filename, 'r') as f:
+            options = json.load(f)
+        return options
+
+    @staticmethod
+    def load(folder: str, lazy=True) -> InferredModel:
+        """Loads inferred model data from previously pickled files."""
+        topic_model = lambda: InferredModel.load_topic_model(folder) if lazy else InferredModel.load_topic_model(folder)
+        train_corpus = lambda: TrainingCorpus.load(folder) if lazy else TrainingCorpus.load(folder)
+        options = InferredModel.load_model_options(folder)
+        return InferredModel(topic_model=topic_model, train_corpus=train_corpus, **options)
+
 
 class InferredTopicsData:
     """The result of applying a topic model on a corpus.
@@ -188,33 +320,33 @@ class InferredTopicsData:
 
     def __init__(
         self,
-        document_index: DocumentIndex,  # document_index (training, shuould be predicted?)
-        dictionary: Any,  # dictionary
-        topic_token_weights: pd.DataFrame,  # model data
-        topic_token_overview: pd.DataFrame,  # model data
-        document_topic_weights: pd.DataFrame,  # predicted data
+        *,
+        dictionary: pd.DataFrame,
+        topic_token_weights: pd.DataFrame,
+        topic_token_overview: pd.DataFrame,
+        document_index: pd.DataFrame,
+        document_topic_weights: pd.DataFrame,
     ):
-        """A container for compiled data as generic pandas dataframes suitable for analysi and visualisation
-        Parameters
-        ----------
-        document_index : DocumentIndex
-            Corpus document index
-        dictionary : Any
-            Corpus dictionary
-        topic_token_weights : pd.DataFrame
-            Topic token weights
-        topic_token_overview : pd.DataFrame
-            Topic overview
-        document_topic_weights : pd.DataFrame
-            Document topic weights
+        """A container for compiled data as generic pandas dataframes suitable for analysis and visualisation
+
+        Model Args:
+            dictionary (pd.DataFrame): Vocabulary
+            topic_token_weights (pd.DataFrame): Topic token distributions
+            topic_token_overview (pd.DataFrame): Topic token overview
+
+        Predicted Args:
+            document_index (pd.DataFrame): Documents index for predicted corpus.
+            document_topic_weights (pd.DataFrame): Predicted weights
         """
-        self.dictionary: Any = dictionary
-        self.document_index: DocumentIndex = document_index
+
+        self.dictionary: pd.DataFrame = dictionary
+        self.document_index: pd.DataFrame = document_index
         self.topic_token_weights: pd.DataFrame = topic_token_weights
         self.document_topic_weights: pd.DataFrame = DocumentIndexHelper(document_index).overload(
             document_topic_weights, 'year'
         )
         self.topic_token_overview: pd.DataFrame = topic_token_overview
+
         self._id2token: dict = None
         self._token2id: Token2Id = None
 
@@ -235,17 +367,7 @@ class InferredTopicsData:
         return list(self.document_topic_weights.topic_id.unique())
 
     def store(self, target_folder: str, pickled: bool = False):
-        """Stores aggregate in `target_folder` as individual zipped files
-
-        Parameters
-        ----------
-        data_folder : str
-            target folder
-        model_name : str
-            Model name
-        pickled : bool, optional
-            if True then pickled (binary) format else  CSV, by default False
-        """
+        """Stores topics data in `target_folder` either as pickled file or individual zipped files """
 
         os.makedirs(target_folder, exist_ok=True)
 
@@ -361,7 +483,7 @@ class InferredTopicsData:
     @staticmethod
     def compute_topic_proportions2(document_topic_weights: pd.DataFrame, doc_length_series: np.ndarray):
         """Compute topic proportations the LDAvis way. Fast version"""
-        theta = scipy.sparse.coo_matrix(
+        theta = coo_matrix(
             (document_topic_weights.weight, (document_topic_weights.document_id, document_topic_weights.topic_id))
         )
         theta_mult_doc_length = theta.T.multiply(doc_length_series).T
