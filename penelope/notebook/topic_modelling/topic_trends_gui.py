2274db7941f3f783944600d9272d382d957dd2b2
Roger Mähler <roger.mahler@hotmail.com>
2021-10-25 21:04:23 +0200

    Refactored to class

--- penelope/notebook/topic_modelling/topic_trends_gui.py
+++ penelope/notebook/topic_modelling/topic_trends_gui.py
@@ -1,9 +1,9 @@
-import types
 import warnings
+from typing import Optional
 
-import ipywidgets as widgets
 import penelope.topic_modelling as topic_modelling
 from IPython.display import display
+from ipywidgets import Button, Dropdown, HBox, IntProgress, IntSlider, Output, ToggleButton, VBox
 
 from .. import widgets_utils
 from .display_topic_trends import display_topic_trends
@@ -12,91 +12,107 @@ from .model_container import TopicModelContainer
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+TEXT_ID = 'topic_share_plot'
 
-def display_gui(state: TopicModelContainer):
 
-    text_id = 'topic_share_plot'
+class GUI:  # pylint: disable=too-many-instance-attributes
+    def __init__(self):
 
-    weighings = [(x['description'], x['key']) for x in topic_modelling.YEARLY_MEAN_COMPUTE_METHODS]
+        self.state: TopicModelContainer = None
 
-    gui = types.SimpleNamespace(
-        n_topics=state.num_topics,
-        text_id=text_id,
-        text=widgets_utils.text_widget(text_id),
-        aggregate=widgets.Dropdown(
-            description='Aggregate', options=weighings, value='true_mean', layout=widgets.Layout(width="200px")
-        ),
-        normalize=widgets.ToggleButton(description='Normalize', value=True, layout=widgets.Layout(width="120px")),
-        topic_id=widgets.IntSlider(
-            description='Topic ID', min=0, max=state.num_topics - 1, step=1, value=0, continuous_update=False
-        ),
-        output_format=widgets.Dropdown(
-            description='Format', options=['Chart', 'Table'], value='Chart', layout=widgets.Layout(width="200px")
-        ),
-        progress=widgets.IntProgress(min=0, max=4, step=1, value=0),
-        output=widgets.Output(),
-        prev_topic_id=None,
-        next_topic_id=None,
-    )
+        self.text = widgets_utils.text_widget(TEXT_ID)
+        self.n_topics: Optional[int] = None
+        self.text_id = TEXT_ID
 
-    gui.prev_topic_id = widgets_utils.button_with_previous_callback(gui, 'topic_id', state.num_topics)
-    gui.next_topic_id = widgets_utils.button_with_next_callback(gui, 'topic_id', state.num_topics)
+        self.aggregate = Dropdown(
+            description='Aggregate',
+            options=[(x['description'], x['key']) for x in topic_modelling.YEARLY_MEAN_COMPUTE_METHODS],
+            value='true_mean',
+            layout=dict(width="200px"),
+        )
 
-    def on_topic_change_update_gui(topic_id):
+        self.normalize = ToggleButton(description='Normalize', value=True, layout=dict(width="120px"))
+        self.topic_id = IntSlider(description='Topic ID', min=0, max=999, step=1, value=0, continuous_update=False)
 
-        if gui.n_topics != state.num_topics:
-            gui.n_topics = state.num_topics
-            gui.topic_id.value = 0
-            gui.topic_id.max = state.num_topics - 1
+        self.output_format = Dropdown(
+            description='Format', options=['Chart', 'Table'], value='Chart', layout=dict(width="200px")
+        )
 
-        tokens = topic_modelling.get_topic_title(state.inferred_topics.topic_token_weights, topic_id, n_tokens=200)
+        self.progress = IntProgress(min=0, max=4, step=1, value=0)
+        self.output = Output()
 
-        gui.text.value = 'ID {}: {}'.format(topic_id, tokens)
+        self.prev_topic_id: Optional[Button] = None
+        self.next_topic_id: Optional[Button] = None
 
-    def update_handler(*_):
+    def layout(self):
+        return VBox(
+            children=[
+                HBox(
+                    children=[
+                        VBox(
+                            children=[
+                                HBox(children=[self.prev_topic_id, self.next_topic_id]),
+                                self.progress,
+                            ]
+                        ),
+                        VBox(children=[self.topic_id]),
+                        VBox(children=[self.aggregate, self.output_format]),
+                        VBox(children=[self.normalize]),
+                    ]
+                ),
+                self.text,
+                self.output,
+            ]
+        )
 
-        gui.output.clear_output()
+    def setup(self, state: TopicModelContainer) -> "GUI":
 
-        with gui.output:
+        self.state = state
+        self.topic_id.max = state.num_topics - 1
+        self.prev_topic_id = widgets_utils.button_with_previous_callback(self, 'topic_id', state.num_topics)
+        self.next_topic_id = widgets_utils.button_with_next_callback(self, 'topic_id', state.num_topics)
+        self.topic_id.observe(self.update_handler, names='value')
+        self.normalize.observe(self.update_handler, names='value')
+        self.aggregate.observe(self.update_handler, names='value')
+        self.output_format.observe(self.update_handler, names='value')
 
-            on_topic_change_update_gui(gui.topic_id.value)
+        return self
 
-            weights = topic_modelling.compute_topic_yearly_means(state.inferred_topics.document_topic_weights)
+    def on_topic_change_update_gui(self, topic_id: int):
+
+        if self.n_topics != self.state.num_topics:
+            self.n_topics = self.state.num_topics
+            self.topic_id.value = 0
+            self.topic_id.max = self.state.num_topics - 1
+
+        tokens = topic_modelling.get_topic_title(self.state.inferred_topics.topic_token_weights, topic_id, n_tokens=200)
+
+        self.text.value = 'ID {}: {}'.format(topic_id, tokens)
+
+    def update_handler(self, *_):
+
+        self.output.clear_output()
+
+        with self.output:
+
+            self.on_topic_change_update_gui(self.topic_id.value)
+
+            weights = topic_modelling.compute_topic_yearly_means(self.state.inferred_topics.document_topic_weights)
 
             display_topic_trends(
                 weight_over_time=weights,
-                topic_id=gui.topic_id.value,
-                year_range=state.inferred_topics.year_period,
-                aggregate=gui.aggregate.value,
-                normalize=gui.normalize.value,
-                output_format=gui.output_format.value,
+                topic_id=self.topic_id.value,
+                year_range=self.state.inferred_topics.year_period,
+                aggregate=self.aggregate.value,
+                normalize=self.normalize.value,
+                output_format=self.output_format.value,
             )
 
-    gui.topic_id.observe(update_handler, names='value')
-    gui.normalize.observe(update_handler, names='value')
-    gui.aggregate.observe(update_handler, names='value')
-    gui.output_format.observe(update_handler, names='value')
 
-    display(
-        widgets.VBox(
-            [
-                widgets.HBox(
-                    [
-                        widgets.VBox(
-                            [
-                                widgets.HBox([gui.prev_topic_id, gui.next_topic_id]),
-                                gui.progress,
-                            ]
-                        ),
-                        widgets.VBox([gui.topic_id]),
-                        widgets.VBox([gui.aggregate, gui.output_format]),
-                        widgets.VBox([gui.normalize]),
-                    ]
-                ),
-                gui.text,
-                gui.output,
-            ]
-        )
-    )
+def display_gui(state: TopicModelContainer):
 
-    update_handler()
+    gui = GUI().setup(state)
+
+    display(gui.layout())
+
+    gui.update_handler()
