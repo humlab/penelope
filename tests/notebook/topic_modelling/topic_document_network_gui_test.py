ca6872608a5e6b5bafe06e33ce79836269f18737
Roger Mähler <roger.mahler@hotmail.com>
2021-10-28 12:48:29 +0200

    Test update

--- tests/notebook/topic_modelling/topic_document_network_gui_test.py
+++ tests/notebook/topic_modelling/topic_document_network_gui_test.py
@@ -4,8 +4,7 @@ import ipywidgets as widgets  # type: ignore
 import pandas as pd
 import pytest
 from penelope.notebook.topic_modelling import topic_document_network_gui as tdn_gui
-from penelope.topic_modelling import InferredTopicsData
-from tests.utils import PERSISTED_INFERRED_MODEL_SOURCE_FOLDER
+from penelope.notebook.topic_modelling import TopicModelContainer
 
 
 # pylint: disable=protected-access, redefined-outer-name
@@ -13,26 +12,25 @@ def monkey_patch(*_, **__):
     ...
 
 
-# @pytest.fixture
-# def inferred_model() -> InferredModel:
-#     _inferred_model: InferredModel = InferredModel.load(PERSISTED_INFERRED_MODEL_SOURCE_FOLDER)
-#     return _inferred_model
+def test_create_gui(state: TopicModelContainer):
+
+    plot_mode = tdn_gui.PlotMode.Default
+
+    gui: tdn_gui.GUI = tdn_gui.GUI(plot_mode=plot_mode).setup(inferred_topics=state.inferred_topics)
+    assert isinstance(gui, tdn_gui.GUI)
+
+    layout = gui.layout()
+    assert isinstance(layout, widgets.VBox)
+
+    opts = gui.opts
+    assert isinstance(opts, tdn_gui.GUI.GUI_opts)
 
 
-@pytest.fixture
-def inferred_topics_data() -> InferredTopicsData:
-    filename_fields = ["year:_:1", "year_serial_id:_:2"]
-    _inferred_topics_data = InferredTopicsData.load(
-        folder=PERSISTED_INFERRED_MODEL_SOURCE_FOLDER, filename_fields=filename_fields
-    )
-    return _inferred_topics_data
-
-
-def test_compile_data(inferred_topics_data):
+def test_compile_data(state: TopicModelContainer):
 
     opts: tdn_gui.GUI.GUI_opts = tdn_gui.GUI.GUI_opts(
         plot_mode=tdn_gui.PlotMode.Default,
-        inferred_topics=inferred_topics_data,
+        inferred_topics=state.inferred_topics,
         threshold=0.5,
         period=(1900, 2100),
         topic_ids=None,
@@ -45,30 +43,16 @@ def test_compile_data(inferred_topics_data):
     assert data is not None
 
 
-def test_topic_document_network_gui(inferred_topics_data: InferredTopicsData):
-
-    plot_mode = tdn_gui.PlotMode.Default
-
-    gui: tdn_gui.GUI = tdn_gui.GUI(plot_mode=plot_mode).setup(inferred_topics=inferred_topics_data)
-    assert isinstance(gui, tdn_gui.GUI)
-
-    layout = gui.layout()
-    assert isinstance(layout, widgets.VBox)
-
-    opts = gui.opts
-    assert isinstance(opts, tdn_gui.GUI.GUI_opts)
-
-
 @pytest.mark.parametrize('plot_mode', [tdn_gui.PlotMode.Default, tdn_gui.PlotMode.FocusTopics])
 @mock.patch('IPython.display.display')  # , monkey_patch)
 @mock.patch('bokeh.plotting.show')  # , monkey_patch)
 def test_display_document_topic_network(
-    mock_show: mock.Mock, mock_display: mock.Mock, plot_mode, inferred_topics_data: InferredTopicsData
+    mock_show: mock.Mock, mock_display: mock.Mock, plot_mode, state: TopicModelContainer
 ):
 
     plot_mode = tdn_gui.PlotMode.Default
 
-    gui: tdn_gui.GUI = tdn_gui.GUI(plot_mode=plot_mode).setup(inferred_topics=inferred_topics_data)
+    gui: tdn_gui.GUI = tdn_gui.GUI(plot_mode=plot_mode).setup(inferred_topics=state.inferred_topics)
 
     _ = gui.layout()
 
@@ -85,10 +69,3 @@ def test_display_document_topic_network(
     opts.output_format = "network"
     tdn_gui.display_document_topic_network(opts)
     assert mock_show.called and not mock_display.called
-
-
-# # def display_gui(plot_mode:PlotMode.FocusTopics, state: TopicModelContainer):
-
-# #     gui: GUI = GUI().setup(plot_mode, state.inferred_topics, state.num_topics)
-# #     display(gui.layout())
-# #     gui.update_handler()
