d8673b406f042435e0fc6914723108068202dddc
Roger Mähler <roger.mahler@hotmail.com>
2021-11-24 09:12:00 +0100

    Fix setup/cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,8 +43,8 @@ def pytest_sessionstart(session):  # pylint: disable=unused-argument
     Called after the Session object has been created and
     before performing collection and entering the run test loop.
     """
-    shutil.rmtree('./tests/test_data/output', ignore_errors=True)
-    os.makedirs('./tests/test_data/output', exist_ok=True)
+    shutil.rmtree('./tests/output', ignore_errors=True)
+    os.makedirs('./tests/output', exist_ok=True)
 
 
 # def pytest_sessionfinish(session, exitstatus):
