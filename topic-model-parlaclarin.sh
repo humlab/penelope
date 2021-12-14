c02f2baed650c42408672d60146a6609b982e170
Roger Mähler <roger.mahler@hotmail.com>
2021-12-14 11:57:05 +0100

    Updated temporary scripts

--- topic-model-parlaclarin.sh
+++ topic-model-parlaclarin.sh
@@ -4,20 +4,22 @@
 
 N_TOPICS=200
 
+    # --lemmatize \
+    # --to-lower \
+    # --remove-stopwords swedish \
+    # --train-corpus-folder ./data/riksprot-speech-train-corpus \
+    # --min-word-length 1 \
+    # --only-any-alphanumeric \
+
 PYTHONPATH=. python penelope/scripts/topic_model.py \
+    --passthrough-column lemma \
     --n-topics 200 \
-    --lemmatize \
-    --to-lower \
-    --remove-stopwords swedish \
-    --min-word-length 1 \
-    --only-any-alphanumeric \
-    --engine gensim_lda-multicore \
+    --engine "gensim_lda-multicore" \
     --random-seed 42 \
     --alpha asymmetric \
     --max-iter 3000 \
     --store-corpus \
     --workers 4 \
     --target-folder ./data \
-    --train-corpus-folder ./data/riksprot-speech-train-corpus \
     ./riksprot-parlaclarin.yml \
     riksprot-parlaclarin-protokoll-200-lemma
