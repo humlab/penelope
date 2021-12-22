7908f6f6d7b8b3c46c3d215047f159d0c4590497
Roger Mähler <roger.mahler@hotmail.com>
2021-12-22 16:10:57 +0100

    Updated run config/script

--- topic-model-parlaclarin.sh
+++ topic-model-parlaclarin.sh
@@ -2,7 +2,7 @@
 
 # gensim_lda-multicore gensim_mallet-lda
 
-N_TOPICS=200
+N_TOPICS=500
 
     # --lemmatize \
     # --to-lower \
@@ -10,16 +10,16 @@ N_TOPICS=200
     # --train-corpus-folder ./data/riksprot-speech-train-corpus \
     # --min-word-length 1 \
     # --only-any-alphanumeric \
+    # --random-seed 42 \
 
 PYTHONPATH=. python penelope/scripts/topic_model.py \
     --passthrough-column lemma \
-    --n-topics 200 \
+    --n-topics 500 \
     --engine "gensim_lda-multicore" \
-    --random-seed 42 \
     --alpha asymmetric \
     --max-iter 3000 \
     --store-corpus \
     --workers 4 \
     --target-folder ./data \
-    ./riksprot-parlaclarin.yml \
-    riksprot-parlaclarin-protokoll-200-lemma
+    /data/riksdagen_corpus_data/tagged-speech-corpus.v0.3.0.id.1960/corpus.yml \
+    riksprot-parlaclarin-1960-protokoll-500-lemma
