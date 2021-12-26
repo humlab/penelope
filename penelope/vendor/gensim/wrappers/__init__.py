011ad920e89d7c6f144ec3b8e76fc6873b4eb1a9
Roger Mähler <roger.mahler@hotmail.com>
2021-12-26 12:18:28 +0100

    make tidy

--- penelope/vendor/gensim/wrappers/__init__.py
+++ penelope/vendor/gensim/wrappers/__init__.py
@@ -7,6 +7,7 @@ This package contains wrappers for other topic modeling programs.
 # from .dtmmodel import DtmModel  # noqa:F401
 # from .fasttext import FastText  # noqa:F401
 from .ldamallet import LdaMallet  # noqa:F401
+
 # from .ldavowpalwabbit import LdaVowpalWabbit  # noqa:F401
 # from .varembed import VarEmbed  # noqa:F401
 # from .wordrank import Wordrank  # noqa:F401
