a4af2933e40d8008834b21f14534b24372aad7e8
Roger Mähler <roger.mahler@hotmail.com>
2021-12-31 23:08:29 +0100

    Change to dict

--- penelope/pipeline/tasks_mixin.py
+++ penelope/pipeline/tasks_mixin.py
@@ -1,6 +1,6 @@
 import contextlib
 from dataclasses import dataclass, field
-from typing import Any, List, Mapping, Optional, Tuple
+from typing import Any, List, Mapping, Optional, Set, Tuple
 
 import numpy as np
 import pandas as pd
@@ -26,18 +26,32 @@ class PoSCountError(ValueError):
 @dataclass
 class TokenCountMixIn:
 
-    token_counts: dict = field(init=False, default_factory=list)
+    token_counts: dict = field(init=False, default_factory=dict)
+    enable_counts: bool = True
 
     def register_token_count(self, document_name: str, n_tokens: int) -> None:
-        self.token_counts.append((document_name, n_tokens))
+        if self.enable_counts:
+            self.token_counts[document_name] = (document_name, n_tokens)
 
     def flush_token_counts(self, document_index: pd.DataFrame) -> None:
-        update_document_index_by_dicts_or_tuples(
-            document_index, data=self.token_counts, columns=['n_tokens'], dtype=np.int32, default=0
-        )
+        if self.enable_counts:
+            update_document_index_by_dicts_or_tuples(
+                document_index,
+                data=list(self.token_counts.values()),
+                columns=['document_name', 'n_tokens'],
+                dtype=np.int32,
+                default=0,
+            )
 
     def exit_hook(self):
-        self.flush_token_counts(self.document_index)
+        if self.enable_counts:
+            self.flush_token_counts(self.document_index)
+            """Only count once if iterated more than once"""
+            self.enable_counts = False
+
+            # setattr(self, 'register_token_count', MethodType(noop, self))
+            # setattr(self, 'flush_token_counts', MethodType(noop, self))
+            # setattr(self, 'exit_hook', MethodType(noop, self))
 
 
 @dataclass
@@ -45,23 +59,20 @@ class PoSCountMixIn:
 
     pos_column: str = None
     document_tfs: Mapping[str, Tuple[Any, ...]] = field(init=False, default_factory=dict)
-    enable_pos_counts: bool = True
-
-    # def __post_init__(self):
-    #     self.exit_hooks.append(self.flush_pos_counts2)
+    enable_counts: bool = True
 
     def register_pos_counts2(
         self, *, document_name: str, tagged_frame: pd.DataFrame, pos_schema: PoS_Tag_Scheme
     ) -> None:
         """Computes count per PoS group. Store in local property."""
-        if not self.enable_pos_counts:
+        if not self.enable_counts:
             return
 
         if not isinstance(tagged_frame, pd.DataFrame):
             raise PoSCountError(f"{document_name}: config error (not a tagged frame)")
 
         if self.pos_column is None:
-            self.pos_column = 'pos_id' if 'pos_id' in tagged_frame.columns else 'pos'
+            self.pos_column = self.resolve_pos_column(set(tagged_frame.columns))
 
         pos_counts: dict = pos_schema.PoS_group_counts(tagged_frame[self.pos_column])
         pos_counts.update(document_name=document_name)
@@ -69,39 +80,40 @@ class PoSCountMixIn:
 
     def register_pos_counts(self, payload: interfaces.DocumentPayload) -> None:
         """Computes token counts from the tagged frame, and adds them to the document index"""
-        if not self.enable_pos_counts:
-            return
-
-        self.register_pos_counts2(
-            document_name=payload.document_name,
-            tagged_frame=payload.content,
-            pos_schema=self.pipeline.payload.pos_schema,
-        )
+        if self.enable_counts:
+            self.register_pos_counts2(
+                document_name=payload.document_name,
+                tagged_frame=payload.content,
+                pos_schema=self.pipeline.payload.pos_schema,
+            )
 
     def flush_pos_counts(self) -> None:
         """Flushes token counts to document index (inplace)."""
-        if not self.enable_pos_counts:
-            return
-
-        self.flush_pos_counts2(document_index=self.document_index)
+        if self.enable_counts:
+            self.flush_pos_counts2(document_index=self.document_index)
 
     def flush_pos_counts2(self, document_index: pd.DataFrame) -> None:
         """Flushes token counts to document index (inplace)."""
-        if not self.enable_pos_counts:
-            return
-
-        if len(self.document_tfs) > 0:
-            update_document_index_by_dicts_or_tuples(document_index=document_index, data=self.document_tfs)
+        if self.enable_counts and len(self.document_tfs) > 0:
+            update_document_index_by_dicts_or_tuples(
+                document_index=document_index, data=list(self.document_tfs.values()), dtype=np.int32, default=0
+            )
 
     def store_pos_counts(self, filename: str = 'document_tfs.json') -> None:
-        if not self.enable_pos_counts:
-            return
-
-        with contextlib.suppress(Exception):
-            write_json(filename, self.document_tfs)
+        if self.enable_counts:
+            with contextlib.suppress(Exception):
+                write_json(filename, self.document_tfs)
 
     def exit_hook(self):
-        self.flush_pos_counts()
+        if self.enable_counts:
+            self.flush_pos_counts()
+            self.enable_counts = False
+
+    def resolve_pos_column(self, columns: Set[str]) -> str:
+        for candidate in ['pos_id', 'pos', 'pos_']:
+            if candidate in columns:
+                return candidate
+        raise ValueError("config error: unable to resolve name of PoS column")
 
 
 @dataclass
