3cb3df192ccc3cf9c5241748cb1c03d8db317827
Roger Mähler <roger.mahler@hotmail.com>
2022-01-10 10:02:25 +0100

    made tidy

--- penelope/notebook/token_counts/__init__.py
+++ penelope/notebook/token_counts/__init__.py
@@ -1,5 +1,5 @@
 # type: ignore
 
 from .dtm_gui import BasicDTMGUI, ComputeOpts
-from .plot import plot_by_bokeh, plot_dataframe
 from .pipeline_gui import create_token_count_gui
+from .plot import plot_by_bokeh, plot_dataframe
