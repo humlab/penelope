06008408b116827b59ed25345e74d0da70b23003
Roger Mähler <roger.mahler@hotmail.com>
2022-01-12 07:47:34 +0100

    Renamed import alias

--- penelope/notebook/topic_modelling/model_container.py
+++ penelope/notebook/topic_modelling/model_container.py
@@ -1,7 +1,8 @@
 from functools import cached_property
 from typing import Optional, Union
 
-import penelope.topic_modelling as topic_modelling
+import penelope.topic_modelling as tm
+from loguru import logger
 
 
 class TopicModelException(Exception):
@@ -15,12 +16,12 @@ class TopicModelContainer:
 
     def __init__(
         self,
-        _trained_model: topic_modelling.InferredModel = None,
-        _inferred_topics: topic_modelling.InferredTopicsData = None,
+        _trained_model: tm.InferredModel = None,
+        _inferred_topics: tm.InferredTopicsData = None,
         _train_corpus_folder: str = None,
     ):
-        self._trained_model: topic_modelling.InferredModel = _trained_model
-        self._inferred_topics: topic_modelling.InferredTopicsData = _inferred_topics
+        self._trained_model: tm.InferredModel = _trained_model
+        self._inferred_topics: tm.InferredTopicsData = _inferred_topics
         self._train_corpus_folder: str = _train_corpus_folder
 
     @staticmethod
@@ -30,9 +31,9 @@ class TopicModelContainer:
 
     def set_data(
         self,
-        _trained_model: Optional[topic_modelling.InferredModel],
-        _inferred_topics: Optional[topic_modelling.InferredTopicsData],
-        _train_corpus_folder: Union[str, topic_modelling.TrainingCorpus] = None,
+        _trained_model: Optional[tm.InferredModel],
+        _inferred_topics: Optional[tm.InferredTopicsData],
+        _train_corpus_folder: Union[str, tm.TrainingCorpus] = None,
     ):
         if 'n_tokens' not in _inferred_topics.document_index.columns:
             raise ValueError("expected n_tokens in document_index (previous fix is removed)")
@@ -44,21 +45,21 @@ class TopicModelContainer:
         self._train_corpus_folder = _train_corpus_folder
 
     @cached_property
-    def train_corpus(self) -> topic_modelling.TrainingCorpus:
+    def train_corpus(self) -> tm.TrainingCorpus:
         if not self._train_corpus_folder:
             raise TopicModelException('Training corpus folder is not set!')
-        if isinstance(self._train_corpus_folder, topic_modelling.TrainingCorpus):
+        if isinstance(self._train_corpus_folder, tm.TrainingCorpus):
             return self._train_corpus_folder
-        return topic_modelling.TrainingCorpus.load(self._train_corpus_folder)
+        return tm.TrainingCorpus.load(self._train_corpus_folder)
 
     @property
-    def trained_model(self) -> topic_modelling.InferredModel:
+    def trained_model(self) -> tm.InferredModel:
         if self._trained_model is None:
             raise TopicModelException('Model not loaded or computed')
         return self._trained_model
 
     @property
-    def inferred_topics(self) -> topic_modelling.InferredTopicsData:
+    def inferred_topics(self) -> tm.InferredTopicsData:
         return self._inferred_topics
 
     @property
