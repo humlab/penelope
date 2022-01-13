892ba883164f094fbf24c5e9165b4d64c268a078
Roger Mähler <roger.mahler@hotmail.com>
2022-01-13 10:27:45 +0100

    Added throttle method

--- penelope/notebook/widgets_utils.py
+++ penelope/notebook/widgets_utils.py
@@ -110,3 +110,30 @@ def register_observer(ctrl: widgets.Widget, fx: Callable, value: bool, names: st
     method: str = 'observe' if value else 'unobserve'
     if hasattr(ctrl, method):
         getattr(ctrl, method)(fx, names=names)
+
+# import asyncio
+# from time import time
+
+# def throttle(wait):
+#     """ Decorator that prevents a function from being called
+#         more than once every wait period. """
+#     def decorator(fn):
+#         time_of_last_call = 0
+#         scheduled, timer = False, None
+#         new_args, new_kwargs = None, None
+#         def throttled(*args, **kwargs):
+#             nonlocal new_args, new_kwargs, time_of_last_call, scheduled, timer
+#             def call_it():
+#                 nonlocal new_args, new_kwargs, time_of_last_call, scheduled, timer
+#                 time_of_last_call = time()
+#                 fn(*new_args, **new_kwargs)
+#                 scheduled = False
+#             time_since_last_call = time() - time_of_last_call
+#             new_args, new_kwargs = args, kwargs
+#             if not scheduled:
+#                 scheduled = True
+#                 new_wait = max(0, wait - time_since_last_call)
+#                 timer = Timer(new_wait, call_it)
+#                 timer.start()
+#         return throttled
+#     return decorator
