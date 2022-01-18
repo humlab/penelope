b91987096c8bad5f0dfb464a6540f68b598e3a0e
Roger Mähler <roger.mahler@hotmail.com>
2022-01-18 21:05:46 +0100

    Added type cast

--- penelope/notebook/word_trends/displayers/utils.py
+++ penelope/notebook/word_trends/displayers/utils.py
@@ -18,7 +18,7 @@ def generate_temporal_ticks(categories: List[int], n_tick: int = 5) -> List[int]
     """Gets ticks every n_tick years if category is year
     Returns all categories if all values are either, lustrum and decade"""
 
-    if all(x % 5 in (0, 5) for x in categories):
+    if all(int(x) % 5 in (0, 5) for x in categories):
         return categories
 
     return list(range(low_bound(categories, n_tick), high_bound(categories, n_tick) + 1, n_tick))
