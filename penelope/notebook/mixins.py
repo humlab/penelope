67be2789f18acb69d99f9b11e2e093a465698d73
Roger Mähler <roger.mahler@hotmail.com>
2022-01-27 20:52:41 +0100

    Simplified code

--- penelope/notebook/mixins.py
+++ penelope/notebook/mixins.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import contextlib
 from collections import defaultdict
-from typing import Any, List, Mapping, Protocol, Set, Tuple, Union
+from typing import Any, Callable, List, Mapping, Set, Tuple, Union
 
 import pandas as pd
 from IPython.display import display as ipydisplay
-from ipywidgets import Button, HBox, Layout, Output, SelectMultiple, ToggleButton
+from ipywidgets import Button, Layout, Output, SelectMultiple, ToggleButton
 
 from penelope import utility as pu
 
@@ -34,55 +34,14 @@ class DownloadMixIn:
                     ipydisplay(js_download)
 
 
-# pylint: disable=no-member
-class IPivotKeysMixIn(Protocol):
-
-    pivot_keys: pu.PivotKeys
-    autoselect_key_values: bool
-
-    def setup(self, **kwargs) -> "IPivotKeysMixIn":
-        ...
-
-    def reset(self) -> "IPivotKeysMixIn":
-        ...
-
-    @property
-    def pivot_keys_text_names(self) -> List[str]:
-        ...
-
-    @property
-    def pivot_keys_id_names(self) -> List[str]:
-        ...
-
-    @property
-    def pivot_keys_filter_values(self) -> pu.PropertyValueMaskingOpts:
-        ...
-
-    @property
-    def unstack_tabular(self) -> bool:
-        ...
-
-    def pivot_key_handler(self, change: dict, *_):
-        ...
-
-    def layout(self) -> HBox:
-        ...
-
-    def observe(self, value: bool) -> None:
-        ...
-
-    def _display_handler(self, *_) -> None:
-        ...
-
-
 class PivotKeysMixIn:
-    """Defines controls and event logics for pivot keys and filters for pivot key values.
-    super() must implement: __init__() setup() reset()  observe()
-    Sibling class must implement: _display_handler(), temporal_key: str
-    """
+    """Defines controls and event logics for pivot keys and filters for pivot key values."""
 
-    def __init__(self: IPivotKeysMixIn, pivot_key_specs: PivotKeySpecArg = None, **kwargs):
+    def __init__(self, pivot_key_specs: PivotKeySpecArg = None, **kwargs):
+
         super().__init__(**kwargs)
+
+        self._display_event_handler: Callable[[Any], None] = None
 
         self.pivot_keys: pu.PivotKeys = (
             pivot_key_specs if isinstance(pivot_key_specs, pu.PivotKeys) else pu.PivotKeys(pivot_key_specs)
@@ -102,43 +61,59 @@ class PivotKeysMixIn:
         self.autoselect_key_values: bool = False
         self.prevent_event: bool = False
 
-    def setup(self: IPivotKeysMixIn, **kwargs) -> "IPivotKeysMixIn":
-        if self.pivot_keys.has_pivot_keys:
-            self._unstack_tabular.observe(self._display_handler, 'value')
-        super().setup(**kwargs)
+    @property
+    def filter_key_values(self) -> List[str]:
+        """Avaliable filter key values"""
+        return self._filter_keys.options
+
+    @property
+    def filter_key_selected_values(self) -> List[str]:
+        """Avaliable filter key values"""
+        return self._filter_keys.value
+
+    def setup(self, **kwargs) -> "PivotKeysMixIn":
+        if hasattr(super(), 'setup'):
+            getattr(super(), 'setup')(**kwargs)
+        register_observer(self._pivot_keys_text_names, handler=self.pivot_key_handler, value=True)
         return self
 
-    def reset(self: IPivotKeysMixIn | PivotKeysMixIn) -> "IPivotKeysMixIn":
-        self.observe(False)
+    def reset(self) -> "PivotKeysMixIn":
+
+        self.observe(handler=self._display_event_handler, value=False)
         self._pivot_keys_text_names.value = ['None']
         self._filter_keys.value = []
         self._unstack_tabular.value = False
-        self.observe(True)
-        super().reset()
+        self.observe(handler=self._display_event_handler, value=False)
+
+        if hasattr(super(), 'reset'):
+            super().reset()
+
         return self
 
     @property
-    def pivot_keys_text_names(self: IPivotKeysMixIn) -> List[str]:
+    def pivot_keys_text_names(self) -> List[str]:
         """Return column names for selected the pivot keys"""
         return [x for x in self._pivot_keys_text_names.value if x != 'None']
 
     @property
-    def pivot_keys_id_names(self: IPivotKeysMixIn) -> List[str]:
+    def pivot_keys_id_names(self) -> List[str]:
         """Return ID column names for selected pivot key"""
         return [self.pivot_keys.key_name2key_id.get(x) for x in self.pivot_keys_text_names]
 
     @property
-    def pivot_keys_filter_values(self: IPivotKeysMixIn) -> pu.PropertyValueMaskingOpts:
+    def filter_opts(self) -> pu.PropertyValueMaskingOpts:
         """Returns user's filter selections as a name-to-values mapping."""
         key_values = defaultdict(list)
         value_tuples: Tuple[str, str] = [x.split(': ') for x in self._filter_keys.value]
         for k, v in value_tuples:
             key_values[k].append(v)
         filter_opts = self.pivot_keys.create_filter_key_values_dict(key_values, decode=True)
+        if hasattr(super(), 'filter_opts'):
+            filter_opts.update(super().filter_opts)
         return filter_opts
 
     @property
-    def unstack_tabular(self: IPivotKeysMixIn) -> bool:
+    def unstack_tabular(self) -> bool:
         if len(self.pivot_keys_text_names) > 0:
             return self._unstack_tabular.value
         return False
@@ -146,8 +121,8 @@ class PivotKeysMixIn:
     def decode_pivot_keys(self, df: pd.DataFrame, drop: bool = True) -> pd.DataFrame:
         return self.pivot_keys.decode_pivot_keys(df, drop)
 
-    def pivot_key_handler(self: IPivotKeysMixIn, change: dict, *_):
-
+    def pivot_key_handler(self, change: dict, *_):
+        """Pivot key selection has changed"""
         try:
 
             if self.prevent_event:
@@ -180,14 +155,21 @@ class PivotKeysMixIn:
         finally:
             self.prevent_event = False
 
-    def observe(self: IPivotKeysMixIn, value: bool) -> None:
-        register_observer(self._pivot_keys_text_names, handler=self.pivot_key_handler, value=value)
+    def observe(self, *, handler: Callable[[Any], None], value: bool) -> None:
+
+        if handler is None:
+            return
+
+        self._display_event_handler = handler
+
         display_trigger_ctrls: List[Any] = (
-            [self._unstack_tabular, self._filter_keys]
+            [self._unstack_tabular, self._filter_keys, self._unstack_tabular]
             if self.pivot_keys.has_pivot_keys
             else [self._pivot_keys_text_names]
         )
 
         for ctrl in display_trigger_ctrls:
-            register_observer(ctrl, handler=self._display_handler, value=value)
-        super().observe(value=value)
+            register_observer(ctrl, handler=handler, value=value)
+
+        if hasattr(super(), "observe"):
+            super().observe(value=value)
