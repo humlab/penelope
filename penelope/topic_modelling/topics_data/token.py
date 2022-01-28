b8ae502a32a8ab477658135875316eaff2de39ab
Roger Mähler <roger.mahler@hotmail.com>
2022-01-28 10:39:50 +0100

    TTW without token

--- penelope/topic_modelling/topics_data/token.py
+++ penelope/topic_modelling/topics_data/token.py
@@ -19,7 +19,7 @@ def get_topic_titles(
     if 'token' not in topic_token_weights.columns:
 
         if id2token is None:
-            raise TypeError("get_topic_titles: either ttw must contain `token` or `id2token` must be supplied")
+            raise TypeError("get_topic_titles: either TTW must contain `token` or `id2token` must be supplied")
 
         fg = id2token.get
         topic_titles: pd.DataFrame = (
@@ -57,17 +57,17 @@ def get_topic_title2(
     return f'ID {topic_id}: {tokens}'
 
 
-def get_topic_top_tokens(topic_token_weights: pd.DataFrame, topic_id: int = None, n_tokens: int = 100) -> pd.DataFrame:
+def _get_topic_top_tokens(
+    ttw: pd.DataFrame, topic_id: int, n_tokens: int = 100, id2token: dict[int, str] = None
+) -> pd.DataFrame:
     """Returns most probable tokens for given topic sorted by probability descending"""
-    weights: pd.DataFrame = (
-        topic_token_weights if topic_id is None else topic_token_weights[(topic_token_weights.topic_id == topic_id)]
-    )
-    top_tokens: pd.DataFrame = weights.sort_values('weight', ascending=False)[:n_tokens]
+    top_tokens: pd.DataFrame = ttw[(ttw.topic_id == topic_id)].sort_values('weight', ascending=False)[:n_tokens]
+    top_tokens['token'] = top_tokens['token_id'].apply(id2token.get)
     return top_tokens
 
 
 def top_topic_token_weights(topic_token_weights: pd.DataFrame, id2term: dict, n_top: int) -> pd.DataFrame:
-    """Find top `n_top` tokens for each topic. Return data frame."""
+    """Find top `n_top` tokens for each topic and their position."""
     data: pd.DataFrame = (
         topic_token_weights.groupby(['topic_id'])[['topic_id', 'token_id', 'weight']]
         .apply(lambda x: x.nlargest(n_top, columns=['weight']))
@@ -110,20 +110,28 @@ class TopicTokensMixIn:
         super().__init__(**kwargs)
 
     def top_topic_token_weights(self: IMixIn, n_top: int) -> pd.DataFrame:
-        return top_topic_token_weights(self.topic_token_weights, self.id2term, n_top=n_top)
+        return top_topic_token_weights(self.topic_token_weights, id2term=self.id2term, n_top=n_top)
 
     def get_topic_titles(self: IMixIn, topic_id: int = None, n_tokens: int = 100) -> pd.Series:
         """Return strings of `n_tokens` most probable words per topic."""
-        return get_topic_titles(self.topic_token_weights, topic_id, n_tokens=n_tokens)
+        return get_topic_titles(self.topic_token_weights, topic_id, n_tokens=n_tokens, id2token=self.id2term)
 
     def get_topic_title(self: IMixIn, topic_id: int, n_tokens: int = 100) -> str:
         """Return string of `n_tokens` most probable words per topic"""
-        return get_topic_title(self.topic_token_weights, topic_id, n_tokens=n_tokens)
+        return get_topic_title(self.topic_token_weights, topic_id, n_tokens=n_tokens, id2token=self.id2term)
 
     def get_topic_title2(self: IMixIn, topic_id: int, n_tokens: int = 100) -> str:
         """Return string of `n_tokens` most probable words per topic"""
-        return get_topic_title2(self.topic_token_weights, topic_id, n_tokens=n_tokens)
+        return get_topic_title2(self.topic_token_weights, topic_id, n_tokens=n_tokens, id2token=self.id2term)
 
-    def get_topic_top_tokens(self: IMixIn, topic_id: int = None, n_tokens: int = 100) -> pd.DataFrame:
+    def get_topic_top_tokens(self: IMixIn, topic_id: int, n_tokens: int = 100) -> pd.DataFrame:
         """Return most probable tokens for given topic sorted by probability descending"""
-        return get_topic_top_tokens(self.topic_token_weights, topic_id, n_tokens=n_tokens)
+        return _get_topic_top_tokens(
+            self.topic_token_weights, topic_id=topic_id, n_tokens=n_tokens, id2token=self.id2term
+        )
+
+    def get_topic_tokens(self: IMixIn, topic_id: int) -> pd.DataFrame:
+        topic_ttw: pd.DataFrame = self.topic_token_weights.loc[self.topic_token_weights.topic_id == topic_id]
+        if 'token' not in topic_ttw:
+            topic_ttw['token'] = topic_ttw['token_id'].apply(self.id2term.get)
+        return topic_ttw
