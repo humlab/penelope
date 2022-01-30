dbf79e0744d0d4f1f9c77f074d429c6debbe1705
Roger Mähler <roger.mahler@hotmail.com>
2022-01-30 09:22:45 +0100

    Updated wordcloud UI

--- penelope/notebook/topic_modelling/topic_wordcloud_gui.py
+++ penelope/notebook/topic_modelling/topic_wordcloud_gui.py
@@ -1,24 +1,19 @@
+import ipywidgets as w
 import pandas as pd
 from IPython.display import display
-from ipywidgets import HTML, Button, Dropdown, HBox, IntProgress, IntSlider, Output, VBox
 
 import penelope.plot as plot_utility
 import penelope.utility as utility
 from penelope import topic_modelling as tm
-from penelope.notebook.topic_modelling.mixins import TopicsStateGui  # type: ignore
 
-from .. import widgets_utils
+from . import mixins as mx
 from .model_container import TopicModelContainer
-
-pd.set_option("display.max_rows", None)
-pd.set_option('max_colwidth', 200)
+from .utility import table_widget
 
 PLOT_OPTS = {'max_font_size': 100, 'background_color': 'white', 'width': 1200, 'height': 600}
-
-TEXT_ID: str = 'tx02'
 OUTPUT_OPTIONS = ['Wordcloud', 'Table', 'CSV', 'XLSX', 'Clipboard']
 CLEAR_OUTPUT: bool = True
-view = Output()
+view = w.Output()
 
 
 @view.capture(clear_output=CLEAR_OUTPUT)
@@ -27,97 +22,75 @@ def display_wordcloud(
     topic_id: int = 0,
     n_words: int = 100,
     output_format: str = 'Wordcloud',
-    gui: "WordcloudGUI" = None,
 ):
 
-    gui.tick(1)
+    tokens = inferred_topics.get_topic_title(topic_id, n_tokens=n_words)
 
-    try:
-        tokens = inferred_topics.get_topic_title(topic_id, n_tokens=n_words)
+    if len(tokens) == 0:
+        raise ValueError("No data! Please change selection.")
 
-        if len(tokens) == 0:
-            print("No data! Please change selection.")
-            return
-
-        gui.text.value = 'ID {}: {}'.format(topic_id, tokens)
-
-        gui.tick()
-
-        if output_format == 'Wordcloud':
-            topic_ttw: pd.DataFrame = inferred_topics.get_topic_tokens(topic_id)
-            plot_utility.plot_wordcloud(topic_ttw, token='token', weight='weight', max_words=n_words, **PLOT_OPTS)
-        else:
-            top_tokens: pd.DataFrame = inferred_topics.get_topic_top_tokens(topic_id=topic_id, n_tokens=n_words)
-            if output_format == 'Table':
-                display(top_tokens)
-            elif output_format.lower() in ('xlsx', 'csv', 'clipboard'):
-                utility.ts_store(data=top_tokens, extension=output_format.lower(), basename='topic_top_tokens')
-
-    except IndexError:
-        print('No data for topic')
-    gui.tick(0)
+    if output_format == 'Wordcloud':
+        plot_utility.plot_wordcloud(
+            inferred_topics.get_topic_tokens(topic_id), token='token', weight='weight', max_words=n_words, **PLOT_OPTS
+        )
+    else:
+        top_tokens: pd.DataFrame = inferred_topics.get_topic_top_tokens(topic_id=topic_id, n_tokens=n_words)
+        g = table_widget(top_tokens)
+        display(g)
+        if output_format.lower() in ('xlsx', 'csv', 'clipboard'):
+            utility.ts_store(data=top_tokens, extension=output_format.lower(), basename='topic_top_tokens')
 
 
-class WordcloudGUI(TopicsStateGui):
+class WordcloudGUI(mx.NextPrevTopicMixIn, mx.AlertMixIn, mx.TopicsStateGui):
     def __init__(self, state: TopicModelContainer):
 
         super().__init__(state=state)
 
         self.n_topics: int = self.inferred_n_topics
 
-        self.text_id: str = TEXT_ID
-        self.text: HTML = HTML(value=f"<span class='{TEXT_ID}'></span>", placeholder='', description='')
-        self.topic_id: IntSlider = IntSlider(
-            description='Topic ID', min=0, max=self.n_topics - 1, step=1, value=0, continuous_update=False
-        )
-        self.word_count: IntSlider = IntSlider(
+        self._text: w.HTML = w.HTML()
+        self._word_count: w.IntSlider = w.IntSlider(
             description='#Words', min=5, max=250, step=1, value=75, continuous_update=False
         )
-        self.output_format: Dropdown = Dropdown(
+        self._output_format: w.Dropdown = w.Dropdown(
             description='Format', options=OUTPUT_OPTIONS, value=OUTPUT_OPTIONS[0], layout=dict(width="200px")
         )
-        self.progress: IntProgress = IntProgress(min=0, max=4, step=1, value=0, layout=dict(width="95%"))
-        # self.output: Output = Output()
-        self.prev_topic_id: Button = None
-        self.next_topic_id: Button = None
 
-    def setup(self) -> "WordcloudGUI":
-
-        self.prev_topic_id = widgets_utils.button_with_previous_callback(self, 'topic_id', self.n_topics)
-        self.next_topic_id = widgets_utils.button_with_next_callback(self, 'topic_id', self.n_topics)
-
-        self.topic_id.observe(self.update_handler, 'value')
-        self.word_count.observe(self.update_handler, 'value')
-        self.output_format.observe(self.update_handler, 'value')
-
+    def setup(self, **kwargs) -> "WordcloudGUI":
+        super().setup(**kwargs)
+        self._topic_id.observe(self.update_handler, 'value')
+        self._word_count.observe(self.update_handler, 'value')
+        self._output_format.observe(self.update_handler, 'value')
         return self
-
-    def tick(self, n=None):
-        self.progress.value = (self.progress.value + 1) if n is None else n
 
     def update_handler(self, *_):
 
+        self._topic_id.unobserve(self.update_handler, 'value')
+
         if self.n_topics != self.inferred_n_topics:
             self.n_topics = self.inferred_n_topics
-            self.topic_id.value = 0
-            self.topic_id.max = self.inferred_n_topics - 1
+            self.topic_id = (0, self.inferred_n_topics - 1)
 
-        display_wordcloud(
-            inferred_topics=self.inferred_topics,
-            topic_id=self.topic_id.value,
-            n_words=self.word_count.value,
-            output_format=self.output_format.value,
-            gui=self,
-        )
-
+        try:
+            self.alert("⌛Computing...")
+            display_wordcloud(
+                inferred_topics=self.inferred_topics,
+                topic_id=self.topic_id,
+                n_words=self._word_count.value,
+                output_format=self._output_format.value,
+            )
+            self.alert("✅ Done!")
+        except Exception as ex:
+            self.warn(f"😡 {ex}")
+        finally:
+            self._topic_id.observe(self.update_handler, 'value')
         return self
 
-    def layout(self) -> VBox:
-        return VBox(
+    def layout(self) -> w.VBox:
+        return w.VBox(
             [
-                self.text,
-                HBox([self.prev_topic_id, self.next_topic_id, self.topic_id, self.word_count, self.output_format]),
-                self.progress,
+                self._text,
+                w.HBox([self._next_prev_layout, self._word_count, self._output_format, self._alert]),
                 view,
             ]
         )
