d06cb5092fb8cf3b79db8d9f60d35b7008013c88
Roger Mähler <roger.mahler@hotmail.com>
2022-01-31 06:45:39 +0100

    Renamed class

--- tests/notebook/topic_modelling/topic_trends_overview_gui_test.py
+++ tests/notebook/topic_modelling/topic_trends_overview_gui_test.py
@@ -1,10 +1,9 @@
 from penelope.notebook.topic_modelling import TopicModelContainer
-from penelope.notebook.topic_modelling.topic_trends_overview_gui import TopicOverviewGUI
-from penelope.topic_modelling.topics_data.prevelance import AverageTopicPrevalenceOverTimeCalculator
+from penelope.notebook.topic_modelling.topic_trends_overview_gui import TopicTrendsOverviewGUI
 
 
 def test_create_gui(state: TopicModelContainer):
-    gui: TopicOverviewGUI = TopicOverviewGUI(state=state, calculator=AverageTopicPrevalenceOverTimeCalculator())
+    gui: TopicTrendsOverviewGUI = TopicTrendsOverviewGUI(state=state)
     assert gui is not None
 
     gui = gui.setup()
