97cde743fd9678af5834bfc87747c8f9bfcf89e6
Roger Mähler <roger.mahler@hotmail.com>
2022-03-19 23:18:40 +0100

    Silenced lint

--- penelope/network/networkx/networkx_api.py
+++ penelope/network/networkx/networkx_api.py
@@ -1,5 +1,6 @@
 # type: ignore
 # flake8: noqa
+# pylint: disable=unused-argument
 
 from __future__ import annotations
 
@@ -9,18 +10,18 @@ from penelope.utility.utils import DummyClass
 
 class nx(pu.DummyClass):
     class Graph(DummyClass):
-        add_nodes_from = pu.DummyFunction
-        add_edges_from = pu.DummyFunction
-        get_edge_attributes = pu.DummyFunction
-        pydot_layout = pu.DummyFunction
+        add_nodes_from = lambda *args, **kwargs: {}
+        add_edges_from = lambda *args, **kwargs: {}
+        get_edge_attributes = lambda *args, **kwargs: {}
+        pydot_layout = lambda *args, **kwargs: {}
 
     class nx_pydot(pu.DummyClass):
         pydot_layout = pu.DummyClass
 
-    spring_layout = pu.DummyFunction
-    shell_layout = pu.DummyFunction
-    circular_layout = pu.DummyFunction
-    spectral_layout = pu.DummyFunction
+    spring_layout = lambda *args, **kwargs: {}
+    shell_layout = lambda *args, **kwargs: {}
+    circular_layout = lambda *args, **kwargs: {}
+    spectral_layout = lambda *args, **kwargs: {}
 
 
 try:
