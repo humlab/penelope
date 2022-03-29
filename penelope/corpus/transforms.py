69c8af1f0bbdedbbad62f1f84805f6dfeda8a8a0
Roger Mähler <roger.mahler@hotmail.com>
2022-03-29 09:30:22 +0200

    Added normalize_characters

--- penelope/corpus/transforms.py
+++ penelope/corpus/transforms.py
@@ -10,6 +10,8 @@ import nltk
 import penelope.vendor.nltk as nltk_utility
 from penelope.vendor.textacy_api import normalize_whitespace
 
+# pylint: disable=W0601,E0602
+
 ALPHABETIC_LOWER_CHARS = string.ascii_lowercase + "åäöéàáâãäåæèéêëîïñôöùûÿ"
 ALPHABETIC_CHARS = set(ALPHABETIC_LOWER_CHARS + ALPHABETIC_LOWER_CHARS.upper())
 SYMBOLS_CHARS = set("'\\¢£¥§©®°±øæç•›€™").union(set('!"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~'))
@@ -23,8 +25,57 @@ RE_HYPHEN_REGEXP: re.Pattern = re.compile(DEFAULT_HYPHEN_REGEXP, re.UNICODE)
 CURRENCY_SYMBOLS = ''.join(chr(i) for i in range(0xFFFF) if unicodedata.category(chr(i)) == 'Sc')
 RE_CURRENCY_SYMBOLS: re.Pattern = re.compile(rf"[{CURRENCY_SYMBOLS}]")
 
+SPECIAL_CHARS = {
+    'hyphens': '-‐‑⁃‒–—―',
+    'minuses': '-−－⁻',
+    'pluses': '+＋⁺',
+    'slashes': '/⁄∕',
+    'tildes': '~˜⁓∼∽∿〜～',
+    'apostrophes': "'’՚Ꞌꞌ＇",
+    'single_quotes': "'‘’‚‛",
+    'double_quotes': '"“”„‟',
+    'accents': '`´',
+    'primes': '′″‴‵‶‷⁗',
+}
 
-# pylint: disable=W0601,E0602
+# SPECIAL_CHARS_ESCAPED = {
+#     'hyphens': '-\u2010\u2011\u2043\u2012\u2013\u2014\u2015',
+#     'minuses': '-\u2212\uff0d\u207b',
+#     'pluses': '+\uff0b\u207a',
+#     'slashes': '/\u2044\u2215',
+#     'tildes': '~\u02dc\u2053\u223c\u223d\u223f\u301c\uff5e',
+#     'apostrophes': "'\u2019\u055a\ua78b\ua78c\uff07",
+#     'single_quotes': "'\u2018\u2019\u201a\u201b",
+#     'double_quotes': '"\u201c\u201d\u201e\u201f',
+#     'accents': '`\xb4',
+#     'primes': '\u2032\u2033\u2034\u2035\u2036\u2037\u2057',
+# }
+
+# ALL_IN_ONE_TRANSLATION = str.maketrans(
+#     *list(map(''.join, zip(*[(v[1:], v[0] * (len(v) - 1)) for _, v in SPECIAL_CHARS.items()])))
+# )
+
+"""Cretae translations that maps characters to first character in each string"""
+SPECIAL_CHARS_GROUP_TRANSLATIONS = {k: str.maketrans(v[1:], v[0] * (len(v) - 1)) for k, v in SPECIAL_CHARS.items()}
+
+ALL_IN_ONE_TRANSLATION = str.maketrans(
+    *[
+        '‐‑⁃‒–—―−－⁻＋⁺⁄∕˜⁓∼∽∿〜～’՚Ꞌꞌ＇‘’‚‛“”„‟´″‴‵‶‷⁗',
+        '----------++//~~~~~~~\'\'\'\'\'\'\'\'\'""""`′′′′′′',
+    ]
+)
+
+
+def normalize_characters(text: str, groups: str = None) -> str:
+
+    if groups is None:
+        return text.translate(ALL_IN_ONE_TRANSLATION)
+
+    for group in groups.split(","):
+        text = text.translate(SPECIAL_CHARS_GROUP_TRANSLATIONS[group])
+
+    return text
+
 
 TokensTransformerFunction = Callable[[Iterable[str]], Iterable[str]]
 
