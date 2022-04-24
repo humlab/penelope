a308e897a4f9cc52d69ccd8e8992f8bcee0043f1
Roger Mähler <roger.mahler@hotmail.com>
2022-04-24 13:04:23 +0200

    Store config

--- penelope/workflows/tm/train_legacy.py
+++ penelope/workflows/tm/train_legacy.py
@@ -1,7 +1,10 @@
+import contextlib
 from os.path import join as jj
 
 import penelope.topic_modelling as tm
-from penelope.corpus import TextReaderOpts, TextTransformOpts, TokenizedCorpus, TokensTransformOpts
+from penelope import corpus as pc
+from penelope import pipeline as pp
+from penelope import utility as pp
 from penelope.corpus.readers import TextTokenizer
 
 # pylint: disable=unused-argument, too-many-arguments
@@ -12,9 +15,9 @@ def compute(
     target_name: str = None,
     corpus_source: str = None,
     target_folder: str = None,
-    reader_opts: TextReaderOpts = None,
-    text_transform_opts: TextTransformOpts = None,
-    transform_opts: TokensTransformOpts = None,
+    reader_opts: pc.TextReaderOpts = None,
+    text_transform_opts: pc.TextTransformOpts = None,
+    transform_opts: pc.TokensTransformOpts = None,
     engine: str = "gensim_lda-multicore",
     engine_args: dict = None,
     store_corpus: bool = False,
@@ -30,7 +33,7 @@ def compute(
         reader_opts=reader_opts,
     )
 
-    corpus: TokenizedCorpus = TokenizedCorpus(reader=tokens_reader, transform_opts=transform_opts)
+    corpus: pc.TokenizedCorpus = pc.TokenizedCorpus(reader=tokens_reader, transform_opts=transform_opts)
 
     train_corpus: tm.TrainingCorpus = tm.TrainingCorpus(
         corpus=corpus,
@@ -65,5 +68,19 @@ def compute(
     )
 
     inferred_topics.store(target_folder)
+
+    """Store a reconstructed corpus config in target folder"""
+    with contextlib.suppress(Exception):
+        pp.CorpusConfig(
+            corpus_name=target_name,
+            corpus_type=pp.CorpusType.Text,
+            corpus_pattern=None,
+            checkpoint_opts=None,
+            text_reader_opts=reader_opts,
+            text_transform_opts=transform_opts,
+            pipelines=None,
+            pipeline_payload=pp.PipelinePayload(source=corpus_source),
+            language=transform_opts.language,
+        ).dump(jj(target_folder, "corpus.yml"))
 
     return dict(folder=target_folder, tag=target_name)
