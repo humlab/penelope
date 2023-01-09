08e949155abe00fbfca6528ceca66f564fa856df
Roger Mähler <roger.mahler@hotmail.com>
2023-01-09 16:01:00 +0100

    Fixed test

--- tests/version_test.py
+++ tests/version_test.py
@@ -8,7 +8,7 @@ import penelope
 def test_versions_are_in_sync():
     """Checks if the pyproject.toml and package.__init__.py __version__ are in sync."""
 
-    with open(str(Path(__file__).resolve().parents[2] / "pyproject.toml")) as fp:
+    with open(str(Path(__file__).resolve().parents[1] / "pyproject.toml")) as fp:
         pyproject: str = toml.loads(fp.read())
 
     pyproject_version: str = pyproject["tool"]["poetry"]["version"]
