72eb67d1e3b9800d1fa5d06c902a2eaf53e5fabc
Roger Mähler <roger.mahler@hotmail.com>
2023-01-10 13:42:32 +0100

    made tidy

--- penelope/plot/__init__.py
+++ penelope/plot/__init__.py
@@ -1,10 +1,5 @@
 # type: ignore
 
 from .colors import get_color_palette
-from .utility import (
-    pchip_interpolate_frame,
-    plot_multiline,
-    plot_multiple_value_series,
-    plot_stacked_bar,
-)
+from .utility import pchip_interpolate_frame, plot_multiline, plot_multiple_value_series, plot_stacked_bar
 from .wordcloud_utility import plot_wordcloud
