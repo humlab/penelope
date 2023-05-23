39fd5f3930e50b3d3221b53b4566ca319a925c29
roger-mahler <roger.mahler@hotmail.com>
2023-05-23 12:38:12 +0200

    Added docstrings

--- penelope/workflows/vectorize/dtm.py
+++ penelope/workflows/vectorize/dtm.py
@@ -14,6 +14,8 @@ def compute(
     corpus_config: CorpusConfig,
     tagged_frame_pipeline: pipeline.CorpusPipeline = None,
 ) -> VectorizedCorpus:
+    """Computes DTM from from stream reseived from a tagged frame pipeline.
+    Uses tagged_frame_pipeline argument or pipeline specified in corpus config. """
     try:
         assert args.is_satisfied()
 
