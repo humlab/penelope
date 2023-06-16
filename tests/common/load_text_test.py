cb76880b34f89ccaab4dcd257ca44fed0a849669
roger-mahler <roger.mahler@hotmail.com>
2023-06-16 16:18:05 +0200

    Fixed test

--- tests/common/load_text_test.py
+++ tests/common/load_text_test.py
@@ -1,16 +1,7 @@
-from os.path import join
-
-import pandas as pd
-
-from penelope import corpus as corpora
-from penelope import pipeline, utility
-from penelope.common.render_text import LemmaCorpusLoader, TextCorpusLoader, TokenizedCorpusLoader, ZipLoader
-from penelope.corpus import load_document_index
+from penelope.common.render_text import LemmaCorpusLoader, TaggedCorpusLoader, ZipLoader, ZippedTextCorpusLoader
 from penelope.corpus.transforms import dehyphen
-from penelope.pipeline import tasks
 
-# FIXME: Create a test fixture for this
-TEST_CORPUS_FILENAME: str = '/data/inidun/courier/corpus/v0.2.0/article_corpus.zip'
+# pylint: disable=protected-access
 
 
 def test_remove_hyphens():
@@ -39,106 +30,29 @@ and training."""
     )
 
 
-def test_load():
-    text_transform_opts: corpora.TextTransformOpts = corpora.TextTransformOpts(
-        transforms="normalize-whitespaces,dehyphen,strip-accents"
-    )
-    config: pipeline.CorpusConfig = pipeline.CorpusConfig.create(
-        corpus_name='courier',
-        corpus_type=pipeline.CorpusType.Text,
-        corpus_pattern='*.zip',
-        checkpoint_opts=pipeline.CheckpointOpts(),
-        text_reader_opts=corpora.TextReaderOpts(),
-        pipelines={},
-        pipeline_payload=pipeline.PipelinePayload(),
-        language='english',
-        text_transform_opts=text_transform_opts,
-    )
-
-    text: str = utility.read_textfile('./tests/test_data/courier/1955_069029_026.txt')
-
-    # config.pipeline_payload.source = [('apa.txt', text)]
-
-    task = tasks.LoadText(
-        source=[('apa.txt', text)],
-        reader_opts=config.text_reader_opts,
-        transform_opts=text_transform_opts,
-    )
-
-    p = pipeline.CorpusPipeline(config=config).add(task).setup()
-
-    processed_payload: pipeline.DocumentPayload = p.single()
-
-    # payload: pipeline.DocumentPayload = pipeline.DocumentPayload(
-    #     content_type=pipeline.ContentType.TEXT, filename='APA.txt', content=text
-    # )
-    # processed_payload: pipeline.DocumentPayload = task.process_payload(payload)
-
-    assert processed_payload is not None
-
-
-TEST_DOCUMENT_INFO = {
-    'document_id': 1774,
-    'courier_id': 78179,
-    'year': 1961,
-    'record_number': 63805,
-    'pages': '[32]',
-    'catalogue_title': "They call their telescope 'Bima Sakti'",
-    'authors': 'Blanco, Victor M.',
-    'filename': '1961_078179_63805.txt',
-    'document_name': '1961_078179_63805',
+TEST_DOCUMENT_INFO: dict = {
+    'filename': 'tran_2019_03_test.csv',
+    'year': 2019,
+    'number': 3,
+    'document_id': 2,
+    'document_name': 'tran_2019_03_test',
+    'text': 'Det finns mitt i skogen en oväntad glänta',
 }
 
 
-def test_document_index():
-    source: str = TEST_CORPUS_FILENAME
-
-    di: pd.DataFrame = load_document_index(join(source, 'document_index.csv'), sep="\t")
-
-    assert di is not None
-
-    assert set(di.columns) == set(TEST_DOCUMENT_INFO.keys())
-
-    assert di.loc["1961_078179_63805"].todict() == TEST_DOCUMENT_INFO
-
-
-def test_load_article_document_text():
-    info: dict = dict(TEST_DOCUMENT_INFO)
-
-    # article_name: str = "1961_078179_63805"
-    article_name: str = f'{info.get("year")}_{int(info.get("courier_id",0)):06}_{info.get("record_number")}'
-
-    source: str = TEST_CORPUS_FILENAME
-
-    text = ZipLoader(source).load(article_name)
-
-    assert text is not None
-
-
-def test_load_article_document_using_loader():
-    source: str = TEST_CORPUS_FILENAME
-
-    di: pd.DataFrame = ZipLoader(source).load_document_index()
-
-    assert di is not None
-
-
 def test_load_tagged_article_document_text():
-    info: dict = dict(TEST_DOCUMENT_INFO)
+    document_name: str = 'tran_2019_03_test'
 
-    # article_name: str = "1961_078179_63805"
-    article_name: str = f'{info.get("year")}_{int(info.get("courier_id",0)):06}_{info.get("record_number")}'
+    source: str = 'tests/test_data/tranströmer_corpus.zip'
 
-    source: str = TEST_CORPUS_FILENAME
+    text = ZipLoader(source).load(f'{document_name}.txt')
+    assert (text or '').startswith('Det finns mitt i skogen en oväntad glänta')
 
-    text = ZipLoader(source).load(f'{article_name}.txt')
-    assert (text or '').startswith('They call their telescope\nJava')
+    text = ZippedTextCorpusLoader(source).load(document_name)
+    assert (text or '').startswith('Det finns mitt i skogen en oväntad glänta')
 
-    text = TextCorpusLoader(source).load(article_name)
-    assert (text or '').startswith('They call their telescope\nJava')
+    text = TaggedCorpusLoader(source).load(document_name)
+    assert (text or '').startswith("# text Det finns mitt i skogen en oväntad glänta")
 
-    text = TokenizedCorpusLoader(source).load(article_name)
-    assert (text or '').startswith("They call their telescope Java 's")
-
-    text = LemmaCorpusLoader(source).load(article_name)
-    assert (text or '').startswith("they call their telescope Java 's")
+    text = LemmaCorpusLoader(source).load(document_name)
+    assert (text or '').startswith('|den|en| |finna|finnas| |mitt|mitt i| |mitt i:3| |skog| |en|')
--- tests/common/render_text_test.py
+++ tests/common/render_text_test.py
@@ -9,7 +9,7 @@ LINKS_REGISTRY = {'PDF': lambda _: 'PDF', 'MD': lambda _: 'MD'}
 
 
 def test_text_repository():
-    document_index: pd.DataFrame = load_document_index('tests/test_data/tranströmer_corpus.csv', sep=";")
+    document_index: pd.DataFrame = load_document_index('tests/test_data/tranströmer_corpus.csv', sep="\t")
     repository: TextRepository = TextRepository(
         source='tests/test_data/tranströmer_corpus.zip',
         document_index=document_index,
@@ -22,8 +22,8 @@ def test_text_repository():
     assert len(filenames) == 5
     assert len(filenames) == len(document_index)
 
-    filename: str = 'CONSTITUTION_0201_015244_1945_london.txt'
-    expected_text: str = 'Constitution of the United Nations Educational, Sc'
+    filename: str = 'tran_2019_03_test.txt'
+    expected_text: str = 'Det finns mitt i skogen en oväntad glänta'
     document_name: str = splitext(filename)[0]
 
     text: str = repository.get_text(filename)
@@ -56,14 +56,15 @@ def test_text_repository():
 
 def test_text_repository_trender_document():
     template = DEFAULT_TEMPLATE
-    renderer: RenderService = RenderService(template, links_registry=LINKS_REGISTRY.items())
+    renderer: RenderService = RenderService(template, links_registry=LINKS_REGISTRY)
 
     document_info: dict = {
-        'document_id': 1,
-        'document_name': 'CONSTITUTION_0201_015244_1945_london',
-        'text': 'Constitution of the United Nations Educational, Sc',
-        'num_tokens': 100,
-        'num_types': 50,
+        'filename': 'tran_2019_03_test.csv',
+        'year': 2019,
+        'number': 3,
+        'document_id': 2,
+        'document_name': 'tran_2019_03_test',
+        'text': 'Det finns mitt i skogen en oväntad glänta',
     }
     text: str = renderer.render(document_info=document_info, kind='text')
     assert text == document_info['text']
@@ -71,4 +72,4 @@ def test_text_repository_trender_document():
     html: str = renderer.render(document_info=document_info, kind='html')
 
     assert document_info['text'] in html
-    assert '<b>Document:</b> CONSTITUTION_0201_015244_1945_london (1) <br/>' in html
+    assert '<b>Document:</b> tran_2019_03_test (2) <br/>' in html
