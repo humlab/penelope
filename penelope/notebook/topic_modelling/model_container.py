13f0d235bd8a32e319141f376b602780e978639e
roger-mahler <roger.mahler@hotmail.com>
2023-06-17 18:03:10 +0200

    Removed deprecated code

--- penelope/notebook/topic_modelling/model_container.py
+++ penelope/notebook/topic_modelling/model_container.py
@@ -20,7 +20,6 @@ class TopicModelContainer:
     def __getitem__(
         self, key: Literal['topic_model', 'trained_model', 'topics_data', 'inferred_topics']
     ) -> Union[tm.InferredModel, tm.InferredModel]:
-        """make it dict-like to simplify testing"""
         if key == 'trained_model':
             return self.trained_model
         if key == 'topic_model':
@@ -44,11 +43,6 @@ class TopicModelContainer:
         inferred_topics: Optional[tm.InferredTopicsData] = None,
         train_corpus_folder: Union[str, tm.TrainingCorpus] = None,
     ) -> "TopicModelContainer":
-        # if inferred_topics is not None:
-        #     if 'n_tokens' not in inferred_topics.document_index.columns:
-        #         raise ValueError("expected n_tokens in document_index (previous fix is removed)")
-        #         # assert _trained_model.train_corpus is not None
-        #         # _inferred_topics.document_index['n_tokens'] = _trained_model.train_corpus.n_tokens
         self._trained_model = trained_model
         self._inferred_topics = inferred_topics
         self._train_corpus_folder = train_corpus_folder
@@ -72,12 +66,3 @@ class TopicModelContainer:
     @property
     def train_corpus_folder(self) -> str:
         return self._train_corpus_folder
-
-    # @pu.deprecated
-    # @cached_property
-    # def train_corpus(self) -> tm.TrainingCorpus:
-    #     if not self._train_corpus_folder:
-    #         raise TopicModelException('Training corpus folder is not set!')
-    #     if isinstance(self._train_corpus_folder, tm.TrainingCorpus):
-    #         return self._train_corpus_folder
-    #     return tm.TrainingCorpus.load(self._train_corpus_folder)
