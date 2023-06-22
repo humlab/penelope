79e81b9844b5922969c986e9061ad91ecc76a247
roger-mahler <roger.mahler@hotmail.com>
2023-06-22 12:29:37 +0200

    Move load to container

--- penelope/notebook/topic_modelling/load_topic_model_gui.py
+++ penelope/notebook/topic_modelling/load_topic_model_gui.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
-from contextlib import suppress
 from typing import Any
 
 import ipywidgets as w
-import pandas as pd
 from IPython.display import display
 from loguru import logger
 
@@ -26,15 +24,12 @@ def _get_filename_fields(folder: str) -> Any:
     return None
 
 
-def _load_tm_handler(*, state: TopicModelContainer, model_info: tm.ModelFolder, slim: bool = False, n_tokens: int = 500):
-    trained_model: tm.InferredModel = tm.InferredModel.load(model_info.folder, lazy=True)
-    inferred_topics: tm.InferredTopicsData = tm.InferredTopicsData.load(
-        folder=model_info.folder, filename_fields=_get_filename_fields(model_info.folder), slim=slim
-    )
+def _load_tm_handler(
+    *, state: TopicModelContainer, model_info: tm.ModelFolder, slim: bool = False, n_tokens: int = 500
+):
+    state.load(folder=model_info.folder, slim=slim)
 
-    state.update(trained_model=trained_model, inferred_topics=inferred_topics, train_corpus_folder=model_info.folder)
-
-    topics = inferred_topics.get_topics_overview_with_score(n_tokens=n_tokens)
+    topics = state.inferred_topics.get_topics_overview_with_score(n_tokens=n_tokens)
 
     display(tt_ui.PandasTopicTitlesGUI(topics, n_tokens=n_tokens).setup().layout())
 
