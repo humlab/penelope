a5bac300f4b4ab4be6681645e7c384c255d149c6
roger-mahler <roger.mahler@hotmail.com>
2023-07-06 14:00:24 +0200

    made tidy

--- penelope/notebook/mdw/main_gui.py
+++ penelope/notebook/mdw/main_gui.py
@@ -18,12 +18,12 @@ def display_mdw(corpus: dtm.VectorizedCorpus, df_mdw):  # pylint: disable=unused
 
 @view_gui.capture(clear_output=True)
 def default_loaded_callback(
-    corpus: dtm.VectorizedCorpus, folder: str, done_callback=display_mdw
-):  # pylint: disable=unused-argument
+    corpus: dtm.VectorizedCorpus, folder: str, done_callback=display_mdw  # pylint: disable=unused-argument
+):
     mdw_gui = create_mdw_gui(corpus, done_callback=done_callback)
     ipy_display.display(mdw_gui.layout())
 
 
 def create_main_gui(corpus_folder: str, loaded_callback=default_loaded_callback) -> VBox:
-    gui = load_dtm_gui.create_load_gui(corpus_folder=corpus_folder, loaded_callback=loaded_callback)
+    gui = load_dtm_gui.LoadGUI(folder=corpus_folder, done_callback=loaded_callback).setup()
     return VBox([gui.layout(), view_gui, view_display])
