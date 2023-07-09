8ad8b6deab70c8e47c11c7841a56d1c35dfa88e8
roger-mahler <roger.mahler@hotmail.com>
2023-07-09 22:56:14 +0200

    Prevent browser open call

--- tests/notebook/topic_modelling/topic_trends_overview_gui_test.py
+++ tests/notebook/topic_modelling/topic_trends_overview_gui_test.py
@@ -1,7 +1,11 @@
+from unittest import mock
+
 from penelope.notebook.topic_modelling import TopicModelContainer
 from penelope.notebook.topic_modelling.topic_trends_overview_gui import TopicTrendsOverviewGUI
 
 
+@mock.patch('bokeh.plotting.show', lambda *_, **__: None)
+@mock.patch('bokeh.io.push_notebook', lambda *_, **__: None)
 def test_create_gui(state: TopicModelContainer):
     gui: TopicTrendsOverviewGUI = TopicTrendsOverviewGUI(state=state)
     assert gui is not None
