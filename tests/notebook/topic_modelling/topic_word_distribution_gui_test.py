865a02ca02a4ac01dd700342317c0450c11feb93
roger-mahler <roger.mahler@hotmail.com>
2023-07-13 08:42:04 +0200

    Silenced test runs

--- tests/notebook/topic_modelling/topic_word_distribution_gui_test.py
+++ tests/notebook/topic_modelling/topic_word_distribution_gui_test.py
@@ -1,7 +1,11 @@
+from unittest import mock
+
 from penelope.notebook.topic_modelling import TopicModelContainer
 from penelope.notebook.topic_modelling.topic_word_distribution_gui import TopicWordDistributionGUI
 
 
+@mock.patch('bokeh.plotting.show', lambda *_, **__: None)
+@mock.patch('bokeh.io.push_notebook', lambda *_, **__: None)
 def test_create_gui(state: TopicModelContainer):
     gui = TopicWordDistributionGUI(state)
     assert gui is not None
